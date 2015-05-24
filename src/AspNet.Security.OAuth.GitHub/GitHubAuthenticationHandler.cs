2d72efc147d911ad646698a9491125715b2b4c68
Kévin Chalet <kevinchalet@gmail.com>
2015-05-25 00:38:03 +0200

    Add Kestrel to project.json

--- src/AspNet.Security.OAuth.GitHub/GitHubAuthenticationHandler.cs
+++ src/AspNet.Security.OAuth.GitHub/GitHubAuthenticationHandler.cs
@@ -17,68 +17,66 @@ namespace AspNet.Security.OAuth.GitHub {
 
         protected override async Task<AuthenticationTicket> GetUserInformationAsync(
             [NotNull] AuthenticationProperties properties, [NotNull] TokenResponse tokens) {
-            using (var request = new HttpRequestMessage(HttpMethod.Get, Options.UserInformationEndpoint)) {
-                request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", tokens.AccessToken);
-                request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            var request = new HttpRequestMessage(HttpMethod.Get, Options.UserInformationEndpoint);
+            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", tokens.AccessToken);
+            request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 
-                using (var response = await Backchannel.SendAsync(request, Context.RequestAborted)) {
-                    response.EnsureSuccessStatusCode();
+            var response = await Backchannel.SendAsync(request, Context.RequestAborted);
+            response.EnsureSuccessStatusCode();
 
-                    var payload = JObject.Parse(await response.Content.ReadAsStringAsync());
+            var payload = JObject.Parse(await response.Content.ReadAsStringAsync());
 
-                    var identity = new ClaimsIdentity(Options.AuthenticationScheme);
-                    var notification = new GitHubAuthenticatedNotification(Context, Options, payload, tokens);
+            var identity = new ClaimsIdentity(Options.AuthenticationScheme);
+            var notification = new GitHubAuthenticatedNotification(Context, Options, payload, tokens);
                     
-                    if (!string.IsNullOrEmpty(notification.Identifier)) {
-                        identity.AddClaim(new Claim(ClaimTypes.NameIdentifier, notification.Identifier,
-                                                    ClaimValueTypes.String, Options.ClaimsIssuer));
-                    }
+            if (!string.IsNullOrEmpty(notification.Identifier)) {
+                identity.AddClaim(new Claim(ClaimTypes.NameIdentifier, notification.Identifier,
+                                            ClaimValueTypes.String, Options.ClaimsIssuer));
+            }
                     
-                    if (!string.IsNullOrEmpty(notification.Login)) {
-                        identity.AddClaim(new Claim(ClaimTypes.Name, notification.Login,
-                                                    ClaimValueTypes.String, Options.ClaimsIssuer));
-                    }
+            if (!string.IsNullOrEmpty(notification.Login)) {
+                identity.AddClaim(new Claim(ClaimTypes.Name, notification.Login,
+                                            ClaimValueTypes.String, Options.ClaimsIssuer));
+            }
                     
-                    if (!string.IsNullOrEmpty(notification.Name)) {
-                        identity.AddClaim(new Claim("urn:github:name", notification.Name,
-                                                    ClaimValueTypes.String, Options.ClaimsIssuer));
-                    }
+            if (!string.IsNullOrEmpty(notification.Name)) {
+                identity.AddClaim(new Claim("urn:github:name", notification.Name,
+                                            ClaimValueTypes.String, Options.ClaimsIssuer));
+            }
                     
-                    if (!string.IsNullOrEmpty(notification.Link)) {
-                        identity.AddClaim(new Claim("urn:github:url", notification.Link,
-                                                    ClaimValueTypes.String, Options.ClaimsIssuer));
-                    }
+            if (!string.IsNullOrEmpty(notification.Link)) {
+                identity.AddClaim(new Claim("urn:github:url", notification.Link,
+                                            ClaimValueTypes.String, Options.ClaimsIssuer));
+            }
 
-                    if (Options.SaveTokens) {
-                        identity.AddClaim(new Claim("access_token", notification.AccessToken,
-                                                    ClaimValueTypes.String, Options.ClaimsIssuer));
+            if (Options.SaveTokens) {
+                identity.AddClaim(new Claim("access_token", notification.AccessToken,
+                                            ClaimValueTypes.String, Options.ClaimsIssuer));
 
-                        if (!string.IsNullOrEmpty(notification.RefreshToken)) {
-                            identity.AddClaim(new Claim("refresh_token", notification.RefreshToken,
-                                                        ClaimValueTypes.String, Options.ClaimsIssuer));
-                        }
+                if (!string.IsNullOrEmpty(notification.RefreshToken)) {
+                    identity.AddClaim(new Claim("refresh_token", notification.RefreshToken,
+                                                ClaimValueTypes.String, Options.ClaimsIssuer));
+                }
 
-                        if (!string.IsNullOrEmpty(notification.TokenType)) {
-                            identity.AddClaim(new Claim("token_type", notification.TokenType,
-                                                        ClaimValueTypes.String, Options.ClaimsIssuer));
-                        }
+                if (!string.IsNullOrEmpty(notification.TokenType)) {
+                    identity.AddClaim(new Claim("token_type", notification.TokenType,
+                                                ClaimValueTypes.String, Options.ClaimsIssuer));
+                }
 
-                        if (notification.ExpiresIn.HasValue) {
-                            identity.AddClaim(new Claim("expires_in", notification.ExpiresIn.Value.TotalSeconds.ToString(CultureInfo.InvariantCulture),
-                                                        ClaimValueTypes.String, Options.ClaimsIssuer));
-                        }
-                    }
-
-                    notification.Properties = properties;
-                    notification.Principal = new ClaimsPrincipal(identity);
-
-                    await Options.Notifications.Authenticated(notification);
-                    
-                    return new AuthenticationTicket(
-                        notification.Principal, notification.Properties,
-                        notification.Options.AuthenticationScheme);
+                if (notification.ExpiresIn.HasValue) {
+                    identity.AddClaim(new Claim("expires_in", notification.ExpiresIn.Value.TotalSeconds.ToString(CultureInfo.InvariantCulture),
+                                                ClaimValueTypes.String, Options.ClaimsIssuer));
                 }
             }
+
+            notification.Properties = properties;
+            notification.Principal = new ClaimsPrincipal(identity);
+
+            await Options.Notifications.Authenticated(notification);
+                    
+            return new AuthenticationTicket(
+                notification.Principal, notification.Properties,
+                notification.Options.AuthenticationScheme);
         }
     }
 }
