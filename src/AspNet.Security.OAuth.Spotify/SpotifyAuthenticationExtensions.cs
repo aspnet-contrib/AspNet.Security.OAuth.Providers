f26ef0b0ad0a6955f577818a6062e6228bca6e41
Adam Reisinger <reisinger.adam@gmail.com>
2015-09-10 19:04:11 +0200

    Add a new provider for Spotify

---
+++ src/AspNet.Security.OAuth.Spotify/SpotifyAuthenticationExtensions.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * Licensed under the Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
+ * See https://github.com/aspnet-contrib/AspNet.Security.OAuth.Providers
+ * for more information concerning the license and the contributors participating to this project.
+ */
+
+using System;
+using AspNet.Security.OAuth.Spotify;
+using Microsoft.Framework.DependencyInjection;
+using Microsoft.Framework.Internal;
+using Microsoft.Framework.OptionsModel;
+
+namespace Microsoft.AspNet.Builder {
+    public static class SpotifyAuthenticationExtensions {
+        public static IServiceCollection ConfigureSpotifyAuthentication(
+            [NotNull] this IServiceCollection services,
+            [NotNull] Action<SpotifyAuthenticationOptions> configuration) {
+            return services.Configure(configuration);
+        }
+
+        public static IApplicationBuilder UseSpotifyAuthentication([NotNull] this IApplicationBuilder app) {
+            return app.UseMiddleware<SpotifyAuthenticationMiddleware>();
+        }
+
+        public static IApplicationBuilder UseSpotifyAuthentication(
+            [NotNull] this IApplicationBuilder app,
+            [NotNull] Action<SpotifyAuthenticationOptions> configuration) {
+            return app.UseMiddleware<SpotifyAuthenticationMiddleware>(
+                new ConfigureOptions<SpotifyAuthenticationOptions>(configuration));
+        }
+    }
+}
---
+++ src/AspNet.Security.OAuth.Spotify/SpotifyAuthenticationOptions.cs
@@ -0,0 +1,29 @@
+﻿/*
+ * Licensed under the Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
+ * See https://github.com/aspnet-contrib/AspNet.Security.OAuth.Providers
+ * for more information concerning the license and the contributors participating to this project.
+ */
+
+using Microsoft.AspNet.Authentication.OAuth;
+using Microsoft.AspNet.Http;
+
+namespace AspNet.Security.OAuth.Spotify {
+    /// <summary>
+    /// Defines a set of options used by <see cref="SpotifyAuthenticationHandler"/>.
+    /// </summary>
+    public class SpotifyAuthenticationOptions : OAuthAuthenticationOptions {
+        public SpotifyAuthenticationOptions() {
+            AuthenticationScheme = SpotifyAuthenticationDefaults.AuthenticationScheme;
+            Caption = SpotifyAuthenticationDefaults.Caption;
+            ClaimsIssuer = SpotifyAuthenticationDefaults.Issuer;
+
+            CallbackPath = new PathString(SpotifyAuthenticationDefaults.CallbackPath);
+
+            AuthorizationEndpoint = SpotifyAuthenticationDefaults.AuthorizationEndpoint;
+            TokenEndpoint = SpotifyAuthenticationDefaults.TokenEndpoint;
+            UserInformationEndpoint = SpotifyAuthenticationDefaults.UserInformationEndpoint;
+
+            SaveTokensAsClaims = false;
+        }
+    }
+}
