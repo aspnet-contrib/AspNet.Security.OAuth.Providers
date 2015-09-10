c241e55e71f6c07f1f1b2c0bdf728c14cf9b66c4
Gekctek <Gekctek@Gmail.com>
2015-09-09 21:42:48 -0700

    Add a new provider for Battle.net

---
+++ src/AspNet.Security.OAuth.BattleNet/BattleNetAuthenticationRegion.cs
@@ -0,0 +1,13 @@
+﻿namespace AspNet.Security.OAuth.BattleNet {
+    /// <summary>
+    /// Defines a list of regions used to determine the appropriate
+    /// API endpoints when communicating with BattleNet.
+    /// </summary>
+    public enum BattleNetAuthenticationRegion {
+        America = 0,
+        China = 1,
+        Europe = 2,
+        Korea = 3,
+        Taiwan = 4
+    }
+}
