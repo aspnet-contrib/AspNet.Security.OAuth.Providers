37d32d1af5a127f1dd89d4ef6b3cf4697d4f0866
Tommy Parnell <tparnell8@gmail.com>
2015-09-15 15:51:13 -0400

    Add a new provider for Reddit

--- src/AspNet.Security.OAuth.Reddit/RedditAuthenticationHandler.cs
+++ src/AspNet.Security.OAuth.Reddit/RedditAuthenticationHandler.cs
@@ -15,12 +15,13 @@ using AspNet.Security.OAuth.Extensions;
 using Microsoft.AspNet.Authentication;
 using Microsoft.AspNet.Authentication.OAuth;
 using Microsoft.AspNet.Http.Authentication;
+using Microsoft.AspNet.WebUtilities;
 using Microsoft.Framework.Internal;
 using Newtonsoft.Json.Linq;
 
-namespace AspNet.Security.OAuth.Yahoo {
-    public class YahooAuthenticationHandler : OAuthHandler<YahooAuthenticationOptions> {
-        public YahooAuthenticationHandler([NotNull] HttpClient client)
+namespace AspNet.Security.OAuth.Reddit {
+    public class RedditAuthenticationHandler : OAuthHandler<RedditAuthenticationOptions> {
+        public RedditAuthenticationHandler([NotNull] HttpClient client)
             : base(client) {
         }
 
@@ -28,19 +29,16 @@ namespace AspNet.Security.OAuth.Yahoo {
             [NotNull] AuthenticationProperties properties, [NotNull] OAuthTokenResponse tokens) {
             var request = new HttpRequestMessage(HttpMethod.Get, Options.UserInformationEndpoint);
             request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", tokens.AccessToken);
+            request.Headers.Authorization = new AuthenticationHeaderValue("bearer", tokens.AccessToken);
 
             var response = await Backchannel.SendAsync(request, HttpCompletionOption.ResponseHeadersRead, Context.RequestAborted);
             response.EnsureSuccessStatusCode();
 
             var payload = JObject.Parse(await response.Content.ReadAsStringAsync());
 
-            identity.AddOptionalClaim(ClaimTypes.NameIdentifier, YahooAuthenticationHelper.GetIdentifier(payload), Options.ClaimsIssuer)
-                    .AddOptionalClaim(ClaimTypes.Name, YahooAuthenticationHelper.GetNickname(payload), Options.ClaimsIssuer)
-                    .AddOptionalClaim("urn:yahoo:familyname", YahooAuthenticationHelper.GetFamilyName(payload), Options.ClaimsIssuer)
-                    .AddOptionalClaim("urn:yahoo:givenname", YahooAuthenticationHelper.GetGivenName(payload), Options.ClaimsIssuer)
-                    .AddOptionalClaim("urn:yahoo:profile", YahooAuthenticationHelper.GetProfileUrl(payload), Options.ClaimsIssuer)
-                    .AddOptionalClaim("urn:yahoo:profileimage", YahooAuthenticationHelper.GetProfileImageUrl(payload), Options.ClaimsIssuer);
+            identity.AddOptionalClaim(ClaimTypes.NameIdentifier, RedditAuthenticationHelper.GetIdentifier(payload), Options.ClaimsIssuer)
+                    .AddOptionalClaim(ClaimTypes.Name, RedditAuthenticationHelper.GetName(payload), Options.ClaimsIssuer)
+                    .AddOptionalClaim("urn:reddit:over18", RedditAuthenticationHelper.GetOver18(payload), Options.ClaimsIssuer);
 
             var context = new OAuthAuthenticatedContext(Context, Options, Backchannel, tokens, payload) {
                 Principal = new ClaimsPrincipal(identity),
@@ -54,6 +52,21 @@ namespace AspNet.Security.OAuth.Yahoo {
             }
 
             return new AuthenticationTicket(context.Principal, context.Properties, Options.AuthenticationScheme);
+        }
+
+        protected override string BuildChallengeUrl(AuthenticationProperties properties, string redirectUri) {
+            var address = base.BuildChallengeUrl(properties, redirectUri);
+
+            // Add duration=permanent to the authorization request to get an access token that doesn't expire after 1 hour.
+            // See https://github.com/reddit/reddit/wiki/OAuth2#authorization.
+            return QueryHelpers.AddQueryString(address, name: "duration", value: "permanent");
+        }
+
+        protected override string FormatScope() {
+            // Note: Reddit requires a non-standard comma-separated scope.
+            // See https://github.com/reddit/reddit/wiki/OAuth2#authorization
+            // and http://tools.ietf.org/html/rfc6749#section-3.3.
+            return string.Join(",", Options.Scope);
         }
 
         protected override async Task<OAuthTokenResponse> ExchangeCodeAsync([NotNull] string code, [NotNull] string redirectUri) {
