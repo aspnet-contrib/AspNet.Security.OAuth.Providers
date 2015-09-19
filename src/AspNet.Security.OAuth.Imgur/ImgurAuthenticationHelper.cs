11e98a0f993fcdccb5cef9227f04a928daaacea5
Albireo <kappa7194@hotmail.it>
2015-09-19 12:13:07 +0200

    Add XML docs for ImgurAuthenticationHelper

--- src/AspNet.Security.OAuth.Imgur/ImgurAuthenticationHelper.cs
+++ src/AspNet.Security.OAuth.Imgur/ImgurAuthenticationHelper.cs
@@ -15,16 +15,37 @@ namespace AspNet.Security.OAuth.Imgur {
     /// instance retrieved from Imgur after a successful authentication process.
     /// </summary>
     public static class ImgurAuthenticationHelper {
+        /// <summary>Get the id of the authenticated user.</summary>
+        /// <param name="user">The authenticated user.</param>
+        /// <returns>The id of the authenticated user.</returns>
         public static string GetId([NotNull] JObject user) => GetString(user, "id");
 
+        /// <summary>Get the url (name) of the authenticated user.</summary>
+        /// <param name="user">The authenticated user.</param>
+        /// <returns>The url (name) of the authenticated user.</returns>
         public static string GetUrl([NotNull] JObject user) => GetString(user, "url");
 
+        /// <summary>Get the bio of the authenticated user.</summary>
+        /// <param name="user">The authenticated user.</param>
+        /// <returns>The bio of the authenticated user.</returns>
         public static string GetBio([NotNull] JObject user) => GetString(user, "bio");
 
+        /// <summary>Get the reputation of the authenticated user.</summary>
+        /// <param name="user">The authenticated user.</param>
+        /// <returns>The reputation of the authenticated user.</returns>
+        /// <remarks></remarks>
         public static string GetReputation([NotNull] JObject user) => GetString(user, "reputation");
 
+        /// <summary>Get the epoch time of account creation of the authenticated user.</summary>
+        /// <param name="user">The authenticated user.</param>
+        /// <returns>The epoch time of account creation of the authenticated user.</returns>
+        /// <remarks></remarks>
         public static string GetCreated([NotNull] JObject user) => GetString(user, "created");
 
+        /// <summary>Get the epoch time of the expiration of the pro account of the authenticated user.</summary>
+        /// <param name="user">The authenticated user.</param>
+        /// <returns>The the epoch time of the expiration of the pro account of the authenticated user.</returns>
+        /// <remarks></remarks>
         public static string GetProExpiration([NotNull] JObject user) => GetString(user, "pro_expiration");
 
         private static string GetString([NotNull] JToken token, string key) {
