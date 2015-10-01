cbe593c39063c006ebd25ac1e21e850a24cea103
Sam <sinan.hana@live.ca>
2015-09-30 21:33:20 -0400

    Add a new provider for Dropbox

---
+++ src/AspNet.Security.OAuth.Dropbox/DropboxAuthenticationOptions.cs
@@ -0,0 +1,29 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
+ * See https://github.com/aspnet-contrib/AspNet.Security.OAuth.Providers
+ * for more information concerning the license and the contributors participating to this project.
+ */
+
+using Microsoft.AspNet.Authentication.OAuth;
+using Microsoft.AspNet.Http;
+
+namespace AspNet.Security.OAuth.Dropbox {
+    /// <summary>
+    /// Defines a set of options used by <see cref="DropboxAuthenticationHandler"/>.
+    /// </summary>
+    public class DropboxAuthenticationOptions : OAuthOptions {
+        public DropboxAuthenticationOptions() {
+            AuthenticationScheme = DropboxAuthenticationDefaults.AuthenticationScheme;
+            DisplayName = DropboxAuthenticationDefaults.DisplayName;
+            ClaimsIssuer = DropboxAuthenticationDefaults.Issuer;
+
+            CallbackPath = new PathString(DropboxAuthenticationDefaults.CallbackPath);
+
+            AuthorizationEndpoint = DropboxAuthenticationDefaults.AuthorizationEndpoint;
+            TokenEndpoint = DropboxAuthenticationDefaults.TokenEndpoint;
+            UserInformationEndpoint = DropboxAuthenticationDefaults.UserInformationEndpoint;
+
+            SaveTokensAsClaims = false;
+        }
+    }
+}
