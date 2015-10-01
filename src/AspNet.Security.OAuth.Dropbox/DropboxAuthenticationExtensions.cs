d3a0814fa7a5f943550a9f17d0f070e296a7b9a4
Sam <sinan.hana@live.ca>
2015-09-30 21:33:20 -0400

    Drop Box provider

---
+++ src/AspNet.Security.OAuth.Dropbox/DropboxAuthenticationExtensions.cs
@@ -0,0 +1,28 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
+ * See https://github.com/aspnet-contrib/AspNet.Security.OAuth.Providers
+ * for more information concerning the license and the contributors participating to this project.
+ */
+
+using System;
+using AspNet.Security.OAuth.Dropbox;
+using Microsoft.Framework.Internal;
+
+namespace Microsoft.AspNet.Builder {
+    public static class DropboxAuthenticationExtensions {
+        public static IApplicationBuilder UseDropboxAuthentication(
+            [NotNull] this IApplicationBuilder app,
+            [NotNull] DropboxAuthenticationOptions options) {
+            return app.UseMiddleware<DropboxAuthenticationMiddleware>(options);
+        }
+
+        public static IApplicationBuilder UseDropboxAuthentication(
+            [NotNull] this IApplicationBuilder app,
+            [NotNull] Action<DropboxAuthenticationOptions> configuration) {
+            var options = new DropboxAuthenticationOptions();
+            configuration(options);
+
+            return app.UseMiddleware<DropboxAuthenticationMiddleware>(options);
+        }
+    }
+}
