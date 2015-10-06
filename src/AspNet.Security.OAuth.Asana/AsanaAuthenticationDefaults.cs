e89b88bfbdf8d59904fb3dd7ae458cc9351e958e
Adam Reisinger <reisinger.adam@gmail.com>
2015-10-06 14:14:55 +0200

    Add a new provider for Asana

---
+++ src/AspNet.Security.OAuth.Asana/AsanaAuthenticationDefaults.cs
@@ -0,0 +1,50 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
+ * See https://github.com/aspnet-contrib/AspNet.Security.OAuth.Providers
+ * for more information concerning the license and the contributors participating to this project.
+ */
+
+using Microsoft.AspNet.Authentication;
+using Microsoft.AspNet.Authentication.OAuth;
+
+namespace AspNet.Security.OAuth.Asana {
+    /// <summary>
+    /// Default values used by the Asana authentication middleware.
+    /// </summary>
+    public static class AsanaAuthenticationDefaults {
+        /// <summary>
+        /// Default value for <see cref="AuthenticationOptions.AuthenticationScheme"/>.
+        /// </summary>
+        public const string AuthenticationScheme = "Asana";
+
+        /// <summary>
+        /// Default value for <see cref="OAuthOptions.DisplayName"/>.
+        /// </summary>
+        public const string DisplayName = "Asana";
+
+        /// <summary>
+        /// Default value for <see cref="OAuthOptions.ClaimsIssuer"/>.
+        /// </summary>
+        public const string Issuer = "Asana";
+
+        /// <summary>
+        /// Default value for <see cref="OAuthOptions.CallbackPath"/>.
+        /// </summary>
+        public const string CallbackPath = "/signin-asana";
+
+        /// <summary>
+        /// Default value for <see cref="OAuthOptions.AuthorizationEndpoint"/>.
+        /// </summary>
+        public const string AuthorizationEndpoint = "https://app.asana.com/-/oauth_authorize";
+
+        /// <summary>
+        /// Default value for <see cref="OAuthOptions.TokenEndpoint"/>.
+        /// </summary>
+        public const string TokenEndpoint = "https://app.asana.com/-/oauth_token";
+
+        /// <summary>
+        /// Default value for <see cref="OAuthOptions.UserInformationEndpoint"/>.
+        /// </summary>
+        public const string UserInformationEndpoint = "https://app.asana.com/api/1.0/users/me";
+    }
+}
