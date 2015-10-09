422e419fae507157cee4dc2dea92390e215d627e
Gekctek <Gekctek@Gmail.com>
2015-10-08 19:44:10 -0700

    aspnet-contrib/AspNet.Security.OAuth.Providers#2 Adding helper setter for setting server region for BattleNet

--- src/AspNet.Security.OAuth.BattleNet/BattleNetAuthenticationOptions.cs
+++ src/AspNet.Security.OAuth.BattleNet/BattleNetAuthenticationOptions.cs
@@ -23,12 +23,64 @@ namespace AspNet.Security.OAuth.BattleNet {
             CallbackPath = new PathString(BattleNetAuthenticationDefaults.CallbackPath);
 
             //Default to US server endpoints
-            AuthorizationEndpoint = BattleNetAuthenticationDefaults.US.AuthorizationEndpoint;
-            TokenEndpoint = BattleNetAuthenticationDefaults.US.TokenEndpoint;
-
-            UserInformationEndpoint = BattleNetAuthenticationDefaults.US.UserInformationEndpoint;
+            this.ServerRegion = BattleNetServerRegion.US;
 
             SaveTokensAsClaims = false;
         }
+        
+        /// <summary>
+        /// Sets the oauth endpoints for the specified region
+        /// </summary>
+        public BattleNetServerRegion ServerRegion
+        {
+            set
+            {
+                string authEndpoint;
+                string tokenEndpoint;
+                string userEndpoint;
+                switch (value)
+                {
+                    case BattleNetServerRegion.US:
+                        authEndpoint = BattleNetAuthenticationDefaults.US.AuthorizationEndpoint;
+                        tokenEndpoint = BattleNetAuthenticationDefaults.US.TokenEndpoint;
+                        userEndpoint = BattleNetAuthenticationDefaults.US.UserInformationEndpoint;
+                        break;
+                    case BattleNetServerRegion.CN:
+                        authEndpoint = BattleNetAuthenticationDefaults.CN.AuthorizationEndpoint;
+                        tokenEndpoint = BattleNetAuthenticationDefaults.CN.TokenEndpoint;
+                        userEndpoint = BattleNetAuthenticationDefaults.CN.UserInformationEndpoint;
+                        break;
+                    case BattleNetServerRegion.EU:
+                        authEndpoint = BattleNetAuthenticationDefaults.EU.AuthorizationEndpoint;
+                        tokenEndpoint = BattleNetAuthenticationDefaults.EU.TokenEndpoint;
+                        userEndpoint = BattleNetAuthenticationDefaults.EU.UserInformationEndpoint;
+                        break;
+                    case BattleNetServerRegion.KR:
+                        authEndpoint = BattleNetAuthenticationDefaults.KR.AuthorizationEndpoint;
+                        tokenEndpoint = BattleNetAuthenticationDefaults.KR.TokenEndpoint;
+                        userEndpoint = BattleNetAuthenticationDefaults.KR.UserInformationEndpoint;
+                        break;
+                    case BattleNetServerRegion.TW:
+                        authEndpoint = BattleNetAuthenticationDefaults.TW.AuthorizationEndpoint;
+                        tokenEndpoint = BattleNetAuthenticationDefaults.TW.TokenEndpoint;
+                        userEndpoint = BattleNetAuthenticationDefaults.TW.UserInformationEndpoint;
+                        break;
+                    default:
+                        throw new NotImplementedException();
+                }
+                AuthorizationEndpoint = authEndpoint;
+                TokenEndpoint = tokenEndpoint;
+                UserInformationEndpoint = userEndpoint;
+            }
+        }
+    }
+
+    public enum BattleNetServerRegion
+    {
+        US,
+        CN,
+        EU,
+        KR,
+        TW
     }
 }
