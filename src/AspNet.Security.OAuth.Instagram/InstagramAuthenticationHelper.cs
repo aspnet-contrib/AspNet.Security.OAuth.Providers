f1c748c23ff226c424b58e7bcdf8d228106c8a3b
yannicsmeets <yannic.smeets@me.com>
2015-11-16 22:30:04 +0100

    Added Instagram Provider

---
+++ src/AspNet.Security.OAuth.Instagram/InstagramAuthenticationHelper.cs
@@ -0,0 +1,26 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
+ * See https://github.com/aspnet-contrib/AspNet.Security.OAuth.Providers
+ * for more information concerning the license and the contributors participating to this project.
+ */
+
+using Microsoft.Extensions.Internal;
+using Newtonsoft.Json.Linq;
+
+namespace AspNet.Security.OAuth.Instagram {
+    /// <summary>
+    /// Contains static methods that allow to extract user's information from a <see cref="JObject"/>
+    /// instance retrieved from Instagram after a successful authentication process.
+    /// </summary>
+    public static class InstagramAuthenticationHelper {
+        /// <summary>
+        /// Gets the identifier corresponding to the authenticated user.
+        /// </summary>
+        public static string GetIdentifier([NotNull] JObject user) => user.Value<string>("id");
+
+        /// <summary>
+        /// Gets the full name corresponding to the authenticated user.
+        /// </summary>
+        public static string GetFullName([NotNull] JObject user) => user.Value<string>("full_name");
+    }
+}
