a04c9a917ac419b09ee772bdf6de83f699f72409
Kévin Chalet <kevinchalet@gmail.com>
2016-03-24 15:12:55 +0100

    Update project.json to use an explicit alpha marker and add missing metadata

--- samples/Mvc.Client/Extensions/HttpContextExtensions.cs
+++ samples/Mvc.Client/Extensions/HttpContextExtensions.cs
@@ -9,17 +9,24 @@ using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Http.Authentication;
-using JetBrains.Annotations;
 
 namespace Mvc.Client.Extensions {
     public static class HttpContextExtensions {
-        public static IEnumerable<AuthenticationDescription> GetExternalProviders([NotNull] this HttpContext context) {
+        public static IEnumerable<AuthenticationDescription> GetExternalProviders(this HttpContext context) {
+            if (context == null) {
+                throw new ArgumentNullException(nameof(context));
+            }
+
             return from description in context.Authentication.GetAuthenticationSchemes()
                    where !string.IsNullOrWhiteSpace(description.DisplayName)
                    select description;
         }
 
-        public static bool IsProviderSupported([NotNull] this HttpContext context, [NotNull] string provider) {
+        public static bool IsProviderSupported(this HttpContext context, string provider) {
+            if (context == null) {
+                throw new ArgumentNullException(nameof(context));
+            }
+
             return (from description in context.GetExternalProviders()
                     where string.Equals(description.AuthenticationScheme, provider, StringComparison.OrdinalIgnoreCase)
                     select description).Any();
