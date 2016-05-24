084ddac9afc9166180b158a9ab357414b72d0d42
Maxime Roussin-Bélanger <maxime.roussinbelanger@gmail.com>
2016-05-24 19:15:20 -0400

    Add gitter provider

---
+++ src/AspNet.Security.OAuth.Gitter/GitterAuthenticationMiddleware.cs
@@ -0,0 +1,30 @@
+﻿using JetBrains.Annotations;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.OAuth;
+using Microsoft.AspNetCore.DataProtection;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System.Text.Encodings.Web;
+
+namespace AspNet.Security.OAuth.Gitter
+{
+    public class GitterAuthenticationMiddleware : OAuthMiddleware<GitterAuthenticationOptions>
+    {
+        public GitterAuthenticationMiddleware(
+            [NotNull] RequestDelegate next,
+            [NotNull] IDataProtectionProvider dataProtectionProvider,
+            [NotNull] ILoggerFactory loggerFactory,
+            [NotNull] UrlEncoder encoder,
+            [NotNull] IOptions<SharedAuthenticationOptions> sharedOptions,
+            [NotNull] IOptions<GitterAuthenticationOptions> options)
+            : base(next, dataProtectionProvider, loggerFactory, encoder, sharedOptions, options)
+        {
+        }
+
+        protected override AuthenticationHandler<GitterAuthenticationOptions> CreateHandler()
+        {
+            return new GitterAuthenticationHandler(Backchannel);
+        }
+    }
+}
---
+++ src/AspNet.Security.OAuth.Gitter/GitterAuthenticationOptions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+namespace AspNet.Security.OAuth.Gitter
+{
+    public class GitterAuthenticationOptions : OAuthOptions
+    {
+        public GitterAuthenticationOptions()
+        {
+            AuthenticationScheme = GitterAuthenticationDefaults.AuthenticationScheme;
+            DisplayName = GitterAuthenticationDefaults.DisplayName;
+            ClaimsIssuer = GitterAuthenticationDefaults.Issuer;
+
+            CallbackPath = new PathString(GitterAuthenticationDefaults.CallbackPath);
+
+            AuthorizationEndpoint = GitterAuthenticationDefaults.AuthorizationEndpoint;
+            TokenEndpoint = GitterAuthenticationDefaults.TokenEndpoint;
+            UserInformationEndpoint = GitterAuthenticationDefaults.UserInformationEndpoint;
+        }
+    }
+}
