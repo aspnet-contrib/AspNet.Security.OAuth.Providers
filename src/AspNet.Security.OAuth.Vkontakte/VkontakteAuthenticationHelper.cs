0c55394b3a9edc45554c123350cd707e36c32ad3
Albert Zakiev <albert.zakiev@gmail.com>
2016-06-14 21:43:45 +0300

    Added license headers Change access modifier for VkontakteAuthenticationHelper to public

--- src/AspNet.Security.OAuth.Vkontakte/VkontakteAuthenticationHelper.cs
+++ src/AspNet.Security.OAuth.Vkontakte/VkontakteAuthenticationHelper.cs
@@ -1,4 +1,10 @@
-﻿using JetBrains.Annotations;
+﻿/*
+ * Licensed under the Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
+ * See https://github.com/aspnet-contrib/AspNet.Security.OAuth.Providers
+ * for more information concerning the license and the contributors participating to this project.
+ */
+
+using JetBrains.Annotations;
 using Newtonsoft.Json.Linq;
 
 namespace AspNet.Security.OAuth.Vkontakte {
@@ -6,7 +12,7 @@ namespace AspNet.Security.OAuth.Vkontakte {
     /// Contains static methods that allow to extract user's information from a <see cref="JObject"/>
     /// instance retrieved from Facebook after a successful authentication process.
     /// </summary>
-    internal static class VkontakteAuthenticationHelper {
+    public static class VkontakteAuthenticationHelper {
         /// <summary>
         /// Gets the user ID.
         /// </summary>
--- src/AspNet.Security.OAuth.Vkontakte/VkontakteAuthenticationOptions.cs
+++ src/AspNet.Security.OAuth.Vkontakte/VkontakteAuthenticationOptions.cs
@@ -1,4 +1,10 @@
-﻿using System.Collections.Generic;
+﻿/*
+ * Licensed under the Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
+ * See https://github.com/aspnet-contrib/AspNet.Security.OAuth.Providers
+ * for more information concerning the license and the contributors participating to this project.
+ */
+
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 
