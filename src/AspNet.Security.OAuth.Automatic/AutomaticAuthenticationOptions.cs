b232e644da5dc1bd08cb07eac025bf7ff533f1b1
Jesse Mandel <jesse.mandel@gmail.com>
2016-06-30 22:32:31 -0700

    Add an Automatic provider

---
+++ src/AspNet.Security.OAuth.Automatic/AutomaticAuthenticationOptions.cs
@@ -0,0 +1,27 @@
+﻿/*
+ * Licensed under the Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
+ * See https://github.com/aspnet-contrib/AspNet.Security.OAuth.Providers
+ * for more information concerning the license and the contributors participating to this project.
+ */
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+namespace AspNet.Security.OAuth.Automatic {
+    /// <summary>
+    /// Defines a set of options used by <see cref="AutomaticAuthenticationHandler"/>.
+    /// </summary>
+    public class AutomaticAuthenticationOptions : OAuthOptions {
+        public AutomaticAuthenticationOptions() {
+            AuthenticationScheme = AutomaticAuthenticationDefaults.AuthenticationScheme;
+            DisplayName = AuthenticationScheme;
+            ClaimsIssuer = AutomaticAuthenticationDefaults.Issuer;
+
+            CallbackPath = new PathString(AutomaticAuthenticationDefaults.CallbackPath);
+
+            AuthorizationEndpoint = AutomaticAuthenticationDefaults.AuthorizationEndpoint;
+            TokenEndpoint = AutomaticAuthenticationDefaults.TokenEndpoint;
+            UserInformationEndpoint = AutomaticAuthenticationDefaults.UserInformationEndpoint;
+        }
+    }
+}
