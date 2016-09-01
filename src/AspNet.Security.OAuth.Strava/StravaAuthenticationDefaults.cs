8aa6bc913142f7c403df657485ad963c4751993d
James Holcomb <james.holcomb@gmail.com>
2016-09-01 16:55:45 -0500

    Add a Strava provider

---
+++ src/AspNet.Security.OAuth.Strava/StravaAuthenticationDefaults.cs
@@ -0,0 +1,49 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
+ * See https://github.com/aspnet-contrib/AspNet.Security.OAuth.Providers
+ * for more information concerning the license and the contributors participating to this project.
+ */
+
+using Microsoft.AspNetCore.Builder;
+
+namespace AspNet.Security.OAuth.Strava {
+    /// <summary>
+    /// Default values used by the Strava authentication middleware.
+    /// </summary>
+    public static class StravaAuthenticationDefaults {
+        /// <summary>
+        /// Default value for <see cref="AuthenticationOptions.AuthenticationScheme"/>.
+        /// </summary>
+        public const string AuthenticationScheme = "Strava";
+
+        /// <summary>
+        /// Default value for <see cref="RemoteAuthenticationOptions.DisplayName"/>.
+        /// </summary>
+        public const string DisplayName = "Strava";
+
+        /// <summary>
+        /// Default value for <see cref="AuthenticationOptions.ClaimsIssuer"/>.
+        /// </summary>
+        public const string Issuer = "Strava";
+
+        /// <summary>
+        /// Default value for <see cref="RemoteAuthenticationOptions.CallbackPath"/>.
+        /// </summary>
+        public const string CallbackPath = "/signin-strava";
+
+        /// <summary>
+        /// Default value for <see cref="OAuthOptions.AuthorizationEndpoint"/>.
+        /// </summary>
+        public const string AuthorizationEndpoint = "https://www.strava.com/oauth/authorize";
+
+        /// <summary>
+        /// Default value for <see cref="OAuthOptions.TokenEndpoint"/>.
+        /// </summary>
+        public const string TokenEndpoint = "https://www.strava.com/oauth/token";
+
+        /// <summary>
+        /// Default value for <see cref="OAuthOptions.UserInformationEndpoint"/>.
+        /// </summary>
+        public const string UserInformationEndpoint = "https://www.strava.com/api/v3/athlete";
+    }
+}
---
+++ src/AspNet.Security.OAuth.Strava/StravaAuthenticationOptions.cs
@@ -0,0 +1,29 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
+ * See https://github.com/aspnet-contrib/AspNet.Security.OAuth.Providers
+ * for more information concerning the license and the contributors participating to this project.
+ */
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+namespace AspNet.Security.OAuth.Strava {
+    /// <summary>
+    /// Defines a set of options used by <see cref="StravaAuthenticationHandler"/>.
+    /// </summary>
+    public class StravaAuthenticationOptions : OAuthOptions {
+        public StravaAuthenticationOptions() {
+            AuthenticationScheme = StravaAuthenticationDefaults.AuthenticationScheme;
+            DisplayName = StravaAuthenticationDefaults.DisplayName;
+            ClaimsIssuer = StravaAuthenticationDefaults.Issuer;
+
+            CallbackPath = new PathString(StravaAuthenticationDefaults.CallbackPath);
+
+            AuthorizationEndpoint = StravaAuthenticationDefaults.AuthorizationEndpoint;
+            TokenEndpoint = StravaAuthenticationDefaults.TokenEndpoint;
+            UserInformationEndpoint = StravaAuthenticationDefaults.UserInformationEndpoint;
+
+            Scope.Add("public");
+        }
+    }
+}
