2eac3acba9f9945f802e50c9f841b7b2512af2ec
Albert Zakiev <albert.zakiev@gmail.com>
2016-10-05 12:23:53 +0300

    Untappd provider

---
+++ src/AspNet.Security.OAuth.Untappd/UntappdAuthenticationOptions.cs
@@ -0,0 +1,27 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
+ * See https://github.com/aspnet-contrib/AspNet.Security.OAuth.Providers
+ * for more information concerning the license and the contributors participating to this project.
+ */
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+namespace AspNet.Security.OAuth.Untappd {
+    /// <summary>
+    /// Defines a set of options used by <see cref="UntappdAuthenticationHandler"/>.
+    /// </summary>
+    public class UntappdAuthenticationOptions : OAuthOptions {
+        public UntappdAuthenticationOptions() {
+            AuthenticationScheme = UntappdAuthenticationDefaults.AuthenticationScheme;
+            DisplayName = UntappdAuthenticationDefaults.DisplayName;
+            ClaimsIssuer = UntappdAuthenticationDefaults.Issuer;
+
+            CallbackPath = new PathString(UntappdAuthenticationDefaults.CallbackPath);
+
+            AuthorizationEndpoint = UntappdAuthenticationDefaults.AuthorizationEndpoint;
+            TokenEndpoint = UntappdAuthenticationDefaults.TokenEndpoint;
+            UserInformationEndpoint = UntappdAuthenticationDefaults.UserInformationEndpoint;
+        }
+    }
+}
