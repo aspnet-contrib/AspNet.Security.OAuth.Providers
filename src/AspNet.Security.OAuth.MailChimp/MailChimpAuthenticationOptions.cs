c3c8dea9984aa1d5477be7f2e4f302fe9b8faa1a
igors <igor.simovic@hunchinsights.com>
2016-11-04 15:01:46 +0100

    MailChimp OAuth support

---
+++ src/AspNet.Security.OAuth.MailChimp/MailChimpAuthenticationOptions.cs
@@ -0,0 +1,27 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
+ * See https://github.com/aspnet-contrib/AspNet.Security.OAuth.Providers
+ * for more information concerning the license and the contributors participating to this project.
+ */
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+namespace AspNet.Security.OAuth.MailChimp {
+    /// <summary>
+    /// Defines a set of options used by <see cref="MailChimpAuthenticationHandler"/>.
+    /// </summary>
+    public class MailChimpAuthenticationOptions : OAuthOptions {
+        public MailChimpAuthenticationOptions() {
+            AuthenticationScheme = MailChimpAuthenticationDefaults.AuthenticationScheme;
+            DisplayName = MailChimpAuthenticationDefaults.DisplayName;
+            ClaimsIssuer = MailChimpAuthenticationDefaults.Issuer;
+
+            CallbackPath = new PathString(MailChimpAuthenticationDefaults.CallbackPath);
+
+            AuthorizationEndpoint = MailChimpAuthenticationDefaults.AuthorizationEndpoint;
+            TokenEndpoint = MailChimpAuthenticationDefaults.TokenEndpoint;
+            UserInformationEndpoint = MailChimpAuthenticationDefaults.UserInformationEndpoint;
+        }
+    }
+}
