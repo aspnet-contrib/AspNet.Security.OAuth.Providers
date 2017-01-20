4d5a766b06e4b3a7c02c42ebdf2a164439ea0fec
Jeff Hotchkiss <jeff.hotchkiss@autodesk.com>
2017-01-20 15:12:49 -0600

    Add an Autodesk provider

---
+++ src/AspNet.Security.OAuth.Autodesk/AutodeskAuthenticationOptions.cs
@@ -0,0 +1,32 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
+ * See https://github.com/aspnet-contrib/AspNet.Security.OAuth.Providers
+ * for more information concerning the license and the contributors participating to this project.
+ */
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+namespace AspNet.Security.OAuth.Autodesk
+{
+    /// <summary>
+    /// Defines a set of options used by <see cref="AutodeskAuthenticationHandler"/>.
+    /// </summary>
+    public class AutodeskAuthenticationOptions : OAuthOptions
+    {
+        public AutodeskAuthenticationOptions()
+        {
+            AuthenticationScheme = AutodeskAuthenticationDefaults.AuthenticationScheme;
+            DisplayName = AutodeskAuthenticationDefaults.DisplayName;
+            ClaimsIssuer = AutodeskAuthenticationDefaults.Issuer;
+
+            CallbackPath = new PathString(AutodeskAuthenticationDefaults.CallbackPath);
+
+            AuthorizationEndpoint = AutodeskAuthenticationDefaults.AuthorizationEndpoint;
+            TokenEndpoint = AutodeskAuthenticationDefaults.TokenEndpoint;
+            UserInformationEndpoint = AutodeskAuthenticationDefaults.UserInformationEndpoint;
+
+            Scope.Add(AutodeskAuthenticationConstants.Scopes.UserProfileRead);
+        }
+    }
+}
--- src/AspNet.Security.OAuth.Extensions/ClaimsExtensions.cs
+++ src/AspNet.Security.OAuth.Extensions/ClaimsExtensions.cs
@@ -11,7 +11,12 @@ namespace AspNet.Security.OAuth.Extensions
 {
     public static class ClaimsExtensions
     {
-        public static ClaimsIdentity AddOptionalClaim(this ClaimsIdentity identity, string type, string value, string issuer)
+        public static ClaimsIdentity AddOptionalClaim(this ClaimsIdentity identity, string name, string value, string issuer)
+        {
+            return AddOptionalClaim(identity, name, value, ClaimValueTypes.String, issuer);
+        }
+
+        public static ClaimsIdentity AddOptionalClaim(this ClaimsIdentity identity, string name, string value, string type, string issuer)
         {
             if (identity == null)
             {
@@ -19,12 +24,12 @@ namespace AspNet.Security.OAuth.Extensions
             }
 
             // Don't update the identity if the claim cannot be safely added.
-            if (string.IsNullOrEmpty(type) || string.IsNullOrEmpty(value))
+            if (string.IsNullOrEmpty(name) || string.IsNullOrEmpty(value))
             {
                 return identity;
             }
 
-            identity.AddClaim(new Claim(type, value, ClaimValueTypes.String, issuer ?? ClaimsIdentity.DefaultIssuer));
+            identity.AddClaim(new Claim(name, value, type ?? ClaimValueTypes.String, issuer ?? ClaimsIdentity.DefaultIssuer));
             return identity;
         }
     }
