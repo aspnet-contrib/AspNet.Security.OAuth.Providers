891a40ec6991be972faabc1c8e21d730bf6497ce
Dmitry Popov <dmitry@parsec-club.ru>
2017-03-06 00:37:41 +0300

    Yandex (yandex.ru) provider added

---
+++ src/AspNet.Security.OAuth.Yandex/YandexAuthenticationDefaults.cs
@@ -0,0 +1,51 @@
+﻿/*
+ * Licensed under the Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
+ * See https://github.com/aspnet-contrib/AspNet.Security.OAuth.Providers
+ * for more information concerning the license and the contributors participating to this project.
+ */
+
+using Microsoft.AspNetCore.Builder;
+
+namespace AspNet.Security.OAuth.Yandex
+{
+    /// <summary>
+    /// Default values used by the Yandex authentication middleware.
+    /// </summary>
+    public static class YandexAuthenticationDefaults
+    {
+        /// <summary>
+        /// Default value for <see cref="AuthenticationOptions.AuthenticationScheme"/>.
+        /// </summary>
+        public const string AuthenticationScheme = "Yandex";
+
+        /// <summary>
+        /// Default value for <see cref="RemoteAuthenticationOptions.DisplayName"/>.
+        /// </summary>
+        public const string DisplayName = "Yandex";
+
+        /// <summary>
+        /// Default value for <see cref="AuthenticationOptions.ClaimsIssuer"/>.
+        /// </summary>
+        public const string Issuer = "Yandex";
+
+        /// <summary>
+        /// Default value for <see cref="RemoteAuthenticationOptions.CallbackPath"/>.
+        /// </summary>
+        public const string CallbackPath = "/signin-yandex";
+
+        /// <summary>
+        /// Default value for <see cref="OAuthOptions.AuthorizationEndpoint"/>.
+        /// </summary>
+        public const string AuthorizationEndpoint = "https://oauth.yandex.ru/authorize";
+
+        /// <summary>
+        /// Default value for <see cref="OAuthOptions.TokenEndpoint"/>.
+        /// </summary>
+        public const string TokenEndpoint = "https://oauth.yandex.ru/token";
+
+        /// <summary>
+        /// Default value for <see cref="OAuthOptions.UserInformationEndpoint"/>.
+        /// </summary>
+        public const string UserInformationEndpoint = "https://login.yandex.ru/info";
+    }
+}
---
+++ src/AspNet.Security.OAuth.Yandex/YandexAuthenticationHelper.cs
@@ -0,0 +1,43 @@
+﻿/*
+ * Licensed under the Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
+ * See https://github.com/aspnet-contrib/AspNet.Security.OAuth.Providers
+ * for more information concerning the license and the contributors participating to this project.
+ */
+
+using JetBrains.Annotations;
+using Newtonsoft.Json.Linq;
+
+namespace AspNet.Security.OAuth.Yandex
+{
+    /// <summary>
+    /// Contains static methods that allow to extract user's information from a <see cref="JObject"/>
+    /// instance retrieved from Yandex after a successful authentication process.
+    /// </summary>
+    public class YandexAuthenticationHelper
+    {
+        /// <summary>
+        /// Gets the family name corresponding to the authenticated user.
+        /// </summary>
+        public static string GetFamilyName([NotNull] JObject payload) => payload.Value<string>("last_name");
+
+        /// <summary>
+        /// Gets the given name corresponding to the authenticated user.
+        /// </summary>
+        public static string GetGivenName([NotNull] JObject payload) => payload.Value<string>("first_name");
+
+        /// <summary>
+        /// Gets the identifier corresponding to the authenticated user.
+        /// </summary>
+        public static string GetIdentifier([NotNull] JObject payload) => payload.Value<string>("id");
+
+        /// <summary>
+        /// Gets the nickname corresponding to the authenticated user.
+        /// </summary>
+        public static string GetNickname([NotNull] JObject payload) => payload.Value<string>("login");
+
+        /// <summary>
+        /// Gets the email corresponding to the authenticated user.
+        /// </summary>
+        public static string GetEmail([NotNull] JObject payload) => payload.Value<JArray>("emails")?[0]?.Value<string>();
+    }
+}
