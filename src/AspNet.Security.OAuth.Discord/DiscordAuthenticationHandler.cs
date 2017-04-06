4f743dde57aaa8026c1136bea269bba72a9feec9
ablankholm <bo.bojsen@gmail.com>
2017-04-06 15:54:18 +0100

    Add a Discord Provider

---
+++ src/AspNet.Security.OAuth.Discord/DiscordAuthenticationHandler.cs
@@ -0,0 +1,53 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
+ * See https://github.com/aspnet-contrib/AspNet.Security.OAuth.Providers
+ * for more information concerning the license and the contributors participating to this project.
+ */
+
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using AspNet.Security.OAuth.Extensions;
+using JetBrains.Annotations;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.OAuth;
+using Microsoft.AspNetCore.Http.Authentication;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json.Linq;
+
+namespace AspNet.Security.OAuth.Discord
+{
+    public class DiscordAuthenticationHandler : OAuthHandler<DiscordAuthenticationOptions>
+    {
+        public DiscordAuthenticationHandler([NotNull] HttpClient client)
+            : base(client)
+        {
+        }
+
+        protected override async Task<AuthenticationTicket> CreateTicketAsync([NotNull] ClaimsIdentity identity,
+            [NotNull] AuthenticationProperties properties, [NotNull] OAuthTokenResponse tokens)
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get, Options.UserInformationEndpoint);
+            request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", tokens.AccessToken);
+
+            var response = await Backchannel.SendAsync(request, HttpCompletionOption.ResponseHeadersRead, Context.RequestAborted);
+            response.EnsureSuccessStatusCode();
+
+            var payload = JObject.Parse(await response.Content.ReadAsStringAsync());
+
+            identity.AddOptionalClaim(ClaimTypes.NameIdentifier, DiscordAuthenticationHelper.GetIdentifier(payload), Options.ClaimsIssuer)
+                    .AddOptionalClaim(ClaimTypes.Email, DiscordAuthenticationHelper.GetEmail(payload), Options.ClaimsIssuer)
+                    .AddOptionalClaim(ClaimTypes.Name, DiscordAuthenticationHelper.GetUsername(payload), Options.ClaimsIssuer);
+
+            var principal = new ClaimsPrincipal(identity);
+            var ticket = new AuthenticationTicket(principal, properties, Options.AuthenticationScheme);
+
+            var context = new OAuthCreatingTicketContext(ticket, Context, Options, Backchannel, tokens, payload);
+            await Options.Events.CreatingTicket(context);
+
+            return context.Ticket;
+        }
+    }
+}
---
+++ src/AspNet.Security.OAuth.Discord/DiscordAuthenticationOptions.cs
@@ -0,0 +1,30 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
+ * See https://github.com/aspnet-contrib/AspNet.Security.OAuth.Providers
+ * for more information concerning the license and the contributors participating to this project.
+ */
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+namespace AspNet.Security.OAuth.Discord
+{
+    /// <summary>
+    /// Defines a set of options used by <see cref="DiscordAuthenticationHandler"/>.
+    /// </summary>
+    public class DiscordAuthenticationOptions : OAuthOptions
+    {
+        public DiscordAuthenticationOptions()
+        {
+            AuthenticationScheme = DiscordAuthenticationDefaults.AuthenticationScheme;
+            DisplayName = DiscordAuthenticationDefaults.DisplayName;
+            ClaimsIssuer = DiscordAuthenticationDefaults.Issuer;
+
+            CallbackPath = new PathString(DiscordAuthenticationDefaults.CallbackPath);
+
+            AuthorizationEndpoint = DiscordAuthenticationDefaults.AuthorizationEndpoint;
+            TokenEndpoint = DiscordAuthenticationDefaults.TokenEndpoint;
+            UserInformationEndpoint = DiscordAuthenticationDefaults.UserInformationEndpoint;
+        }
+    }
+}
