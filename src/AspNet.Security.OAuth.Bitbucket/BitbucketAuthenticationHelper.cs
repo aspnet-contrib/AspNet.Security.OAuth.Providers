aa8401ee1ac631c5ba0773dfbf115c70e663e13a
swenz <github@wenz.to>
2017-04-07 11:20:12 +0200

    Add a Bitbucket provider

---
+++ src/AspNet.Security.OAuth.Bitbucket/BitbucketAuthenticationHelper.cs
@@ -0,0 +1,49 @@
+﻿/*
+ * Licensed under the Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
+ * See https://github.com/aspnet-contrib/AspNet.Security.OAuth.Providers
+ * for more information concerning the license and the contributors participating to this project.
+ */
+
+using System.Linq;
+using JetBrains.Annotations;
+using Newtonsoft.Json.Linq;
+
+namespace AspNet.Security.OAuth.Bitbucket
+{
+    /// <summary>
+    /// Contains static methods that allow to extract user's information from a <see cref="JObject"/>
+    /// instance retrieved from Bitbucket after a successful authentication process.
+    /// </summary>
+    public static class BitbucketAuthenticationHelper
+    {
+        /// <summary>
+        /// Gets the identifier corresponding to the authenticated user.
+        /// </summary>
+        public static string GetIdentifier([NotNull] JObject user) => user.Value<string>("account_id");
+
+        /// <summary>
+        /// Gets the login corresponding to the authenticated user.
+        /// </summary>
+        public static string GetLogin([NotNull] JObject user) => user.Value<string>("username");
+
+        /// <summary>
+        /// Gets the email address corresponding to the authenticated user.
+        /// </summary>
+        public static string GetEmail([NotNull] JObject payload)
+        {
+            return (from address in payload.Value<JArray>("values")
+                    where address.Value<bool>("is_primary")
+                    select address.Value<string>("email")).FirstOrDefault();
+        }
+
+        /// <summary>
+        /// Gets the name corresponding to the authenticated user.
+        /// </summary>
+        public static string GetName([NotNull] JObject user) => user.Value<string>("display_name");
+
+        /// <summary>
+        /// Gets the URL corresponding to the authenticated user.
+        /// </summary>
+        public static string GetLink([NotNull] JObject user) => user.Value<string>("website");
+    }
+}
