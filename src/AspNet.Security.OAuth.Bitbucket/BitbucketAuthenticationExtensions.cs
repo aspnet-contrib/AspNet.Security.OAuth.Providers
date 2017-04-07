8c41fbd1c1d2eeb55063565a4ac380a6998e4bba
Steffen Wenz <steffen@wenz.io>
2017-04-07 11:20:12 +0200

    Implemented support for Bitbucket

---
+++ src/AspNet.Security.OAuth.Bitbucket/BitbucketAuthenticationExtensions.cs
@@ -0,0 +1,70 @@
+﻿/*
+ * Licensed under the Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
+ * See https://github.com/aspnet-contrib/AspNet.Security.OAuth.Providers
+ * for more information concerning the license and the contributors participating to this project.
+ */
+
+using System;
+using AspNet.Security.OAuth.Bitbucket;
+using JetBrains.Annotations;
+using Microsoft.Extensions.Options;
+
+namespace Microsoft.AspNetCore.Builder
+{
+    /// <summary>
+    /// Extension methods to add Bitbucket authentication capabilities to an HTTP application pipeline.
+    /// </summary>
+    public static class BitbucketAuthenticationExtensions
+    {
+        /// <summary>
+        /// Adds the <see cref="BitbucketAuthenticationMiddleware"/> middleware to the specified
+        /// <see cref="IApplicationBuilder"/>, which enables Bitbucket authentication capabilities.
+        /// </summary>
+        /// <param name="app">The <see cref="IApplicationBuilder"/> to add the middleware to.</param>
+        /// <param name="options">A <see cref="BitbucketAuthenticationOptions"/> that specifies options for the middleware.</param>
+        /// <returns>A reference to this instance after the operation has completed.</returns>
+        public static IApplicationBuilder UseBitbucketAuthentication(
+            [NotNull] this IApplicationBuilder app,
+            [NotNull]BitbucketAuthenticationOptions options)
+        {
+            if (app == null)
+            {
+                throw new ArgumentNullException(nameof(app));
+            }
+
+            if (options == null)
+            {
+                throw new ArgumentNullException(nameof(options));
+            }
+
+            return app.UseMiddleware<BitbucketAuthenticationMiddleware>(Options.Create(options));
+        }
+
+        /// <summary>
+        /// Adds the <see cref="BitbucketAuthenticationMiddleware"/> middleware to the specified
+        /// <see cref="IApplicationBuilder"/>, which enables Bitbucket authentication capabilities.
+        /// </summary>
+        /// <param name="app">The <see cref="IApplicationBuilder"/> to add the middleware to.</param>
+        /// <param name="configuration">An action delegate to configure the provided <see cref="BitbucketAuthenticationOptions"/>.</param>
+        /// <returns>A reference to this instance after the operation has completed.</returns>
+        public static IApplicationBuilder UseBitbucketAuthentication(
+            [NotNull] this IApplicationBuilder app,
+            [NotNull] Action<BitbucketAuthenticationOptions> configuration)
+        {
+            if (app == null)
+            {
+                throw new ArgumentNullException(nameof(app));
+            }
+
+            if (configuration == null)
+            {
+                throw new ArgumentNullException(nameof(configuration));
+            }
+
+            var options = new BitbucketAuthenticationOptions();
+            configuration(options);
+
+            return app.UseMiddleware<BitbucketAuthenticationMiddleware>(Options.Create(options));
+        }
+    }
+}
---
+++ src/AspNet.Security.OAuth.Bitbucket/BitbucketAuthenticationOptions.cs
@@ -0,0 +1,36 @@
+﻿/*
+ * Licensed under the Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
+ * See https://github.com/aspnet-contrib/AspNet.Security.OAuth.Providers
+ * for more information concerning the license and the contributors participating to this project.
+ */
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+namespace AspNet.Security.OAuth.Bitbucket
+{
+    /// <summary>
+    /// Defines a set of options used by <see cref="BitbucketAuthenticationHandler"/>.
+    /// </summary>
+    public class BitbucketAuthenticationOptions : OAuthOptions
+    {
+        public BitbucketAuthenticationOptions()
+        {
+            AuthenticationScheme = BitbucketAuthenticationDefaults.AuthenticationScheme;
+            DisplayName = BitbucketAuthenticationDefaults.DisplayName;
+            ClaimsIssuer = BitbucketAuthenticationDefaults.Issuer;
+
+            CallbackPath = new PathString(BitbucketAuthenticationDefaults.CallbackPath);
+
+            AuthorizationEndpoint = BitbucketAuthenticationDefaults.AuthorizationEndpoint;
+            TokenEndpoint = BitbucketAuthenticationDefaults.TokenEndpoint;
+            UserInformationEndpoint = BitbucketAuthenticationDefaults.UserInformationEndpoint;
+        }
+
+        /// <summary>
+        /// Gets or sets the address of the endpoint exposing
+        /// the email addresses associated with the logged in user.
+        /// </summary>
+        public string UserEmailsEndpoint { get; set; } = BitbucketAuthenticationDefaults.UserEmailsEndpoint;
+    }
+}
