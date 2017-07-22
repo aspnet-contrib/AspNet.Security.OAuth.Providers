c8ae943ac333c9070076f0e3f69300145ea78ebc
Antoine Bichon <antbichon+github@gmail.com>
2017-07-22 20:00:18 +0200

    Add a Patreon provider

---
+++ src/AspNet.Security.OAuth.Patreon/PatreonAuthenticationHelper.cs
@@ -0,0 +1,71 @@
+﻿/*
+ * Licensed under the Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
+ * See https://github.com/aspnet-contrib/AspNet.Security.OAuth.Providers
+ * for more information concerning the license and the contributors participating to this project.
+ */
+
+using System;
+using JetBrains.Annotations;
+using Newtonsoft.Json.Linq;
+
+namespace AspNet.Security.OAuth.Patreon
+{
+    /// <summary>
+    /// Contains static methods that allow to extract user's information from a <see cref="JObject"/>
+    /// instance retrieved from Patreon after a successful authentication process.
+    /// </summary>
+    public static class PatreonAuthenticationHelper
+    {
+        /// <summary>
+        /// Gets the identifier corresponding to the authenticated user.
+        /// </summary>
+        public static string GetUserIdentifier([NotNull] JObject user)
+        {
+            if (user == null)
+            {
+                throw new ArgumentNullException(nameof(user));
+            }
+
+            return user["data"]?.Value<string>("id");
+        }
+
+        /// <summary>
+        /// Gets the name corresponding to the authenticated user.
+        /// </summary>
+        public static string GetUserName([NotNull] JObject user)
+        {
+            if (user == null)
+            {
+                throw new ArgumentNullException(nameof(user));
+            }
+
+            return user["data"]?["attributes"]?.Value<string>("full_name");
+        }
+
+        /// <summary>
+        /// Gets the page URL corresponding to the authenticated user.
+        /// </summary>
+        public static string GetLink([NotNull] JObject user)
+        {
+            if (user == null)
+            {
+                throw new ArgumentNullException(nameof(user));
+            }
+
+            return user["data"]?["attributes"]?.Value<string>("url");
+        }
+
+        /// <summary>
+        /// Gets the avatar URL corresponding to the authenticated user.
+        /// </summary>
+        public static string GetAvatarLink([NotNull] JObject user)
+        {
+            if (user == null)
+            {
+                throw new ArgumentNullException(nameof(user));
+            }
+
+            return user["data"]?["attributes"]?.Value<string>("thumb_url");
+        }
+    }
+}
