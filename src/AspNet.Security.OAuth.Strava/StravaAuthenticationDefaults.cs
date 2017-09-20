50eb197a1396ee81321a539ba61602aa3434c889
Jack Thorpe <jackthorpe1@hotmail.com>
2017-09-20 20:48:12 +0100

    Migrate the Strava provider to ASP.NET Core 2.0

--- src/AspNet.Security.OAuth.Strava/StravaAuthenticationDefaults.cs
+++ src/AspNet.Security.OAuth.Strava/StravaAuthenticationDefaults.cs
@@ -4,7 +4,8 @@
  * for more information concerning the license and the contributors participating to this project.
  */
 
-using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.OAuth;
 
 namespace AspNet.Security.OAuth.Strava
 {
@@ -14,17 +15,17 @@ namespace AspNet.Security.OAuth.Strava
     public static class StravaAuthenticationDefaults
     {
         /// <summary>
-        /// Default value for <see cref="AuthenticationOptions.AuthenticationScheme"/>.
+        /// Default value for <see cref="AuthenticationScheme.Name"/>.
         /// </summary>
         public const string AuthenticationScheme = "Strava";
 
         /// <summary>
-        /// Default value for <see cref="RemoteAuthenticationOptions.DisplayName"/>.
+        /// Default value for <see cref="AuthenticationScheme.DisplayName"/>.
         /// </summary>
         public const string DisplayName = "Strava";
 
         /// <summary>
-        /// Default value for <see cref="AuthenticationOptions.ClaimsIssuer"/>.
+        /// Default value for <see cref="AuthenticationSchemeOptions.ClaimsIssuer"/>.
         /// </summary>
         public const string Issuer = "Strava";
 
