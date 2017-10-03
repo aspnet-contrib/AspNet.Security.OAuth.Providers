5147582fca49554d276c1b9add0b1bf9054f1a75
7IN0SAN9 <me@7in0.me>
2017-10-04 02:00:14 +0800

    Migrate the Yahoo provider to ASP.NET Core 2.0

--- src/AspNet.Security.OAuth.Yahoo/YahooAuthenticationHandler.cs
+++ src/AspNet.Security.OAuth.Yahoo/YahooAuthenticationHandler.cs
@@ -10,21 +10,25 @@ using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Security.Claims;
 using System.Text;
+using System.Text.Encodings.Web;
 using System.Threading.Tasks;
-using AspNet.Security.OAuth.Extensions;
 using JetBrains.Annotations;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.OAuth;
-using Microsoft.AspNetCore.Http.Authentication;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using Newtonsoft.Json.Linq;
 
 namespace AspNet.Security.OAuth.Yahoo
 {
     public class YahooAuthenticationHandler : OAuthHandler<YahooAuthenticationOptions>
     {
-        public YahooAuthenticationHandler([NotNull] HttpClient client)
-            : base(client)
+        public YahooAuthenticationHandler(
+            [NotNull] IOptionsMonitor<YahooAuthenticationOptions> options,
+            [NotNull] ILoggerFactory logger,
+            [NotNull] UrlEncoder encoder,
+            [NotNull] ISystemClock clock)
+            : base(options, logger, encoder, clock)
         {
         }
 
@@ -49,21 +53,12 @@ namespace AspNet.Security.OAuth.Yahoo
 
             var payload = JObject.Parse(await response.Content.ReadAsStringAsync());
 
-            identity.AddOptionalClaim(ClaimTypes.NameIdentifier, YahooAuthenticationHelper.GetIdentifier(payload), Options.ClaimsIssuer)
-                    .AddOptionalClaim(ClaimTypes.Name, YahooAuthenticationHelper.GetNickname(payload), Options.ClaimsIssuer)
-                    .AddOptionalClaim("urn:yahoo:familyname", YahooAuthenticationHelper.GetFamilyName(payload), Options.ClaimsIssuer)
-                    .AddOptionalClaim("urn:yahoo:givenname", YahooAuthenticationHelper.GetGivenName(payload), Options.ClaimsIssuer)
-                    .AddOptionalClaim("urn:yahoo:profile", YahooAuthenticationHelper.GetProfileUrl(payload), Options.ClaimsIssuer)
-                    .AddOptionalClaim("urn:yahoo:profileimage", YahooAuthenticationHelper.GetProfileImageUrl(payload), Options.ClaimsIssuer);
-
             var principal = new ClaimsPrincipal(identity);
-            var ticket = new AuthenticationTicket(principal, properties, Options.AuthenticationScheme);
+            var context = new OAuthCreatingTicketContext(principal, properties, Context, Scheme, Options, Backchannel, tokens, payload);
+            context.RunClaimActions(payload.Value<JObject>("profile"));
 
-            var context = new OAuthCreatingTicketContext(ticket, Context, Options, Backchannel, tokens, payload);
             await Options.Events.CreatingTicket(context);
-
-            return context.Ticket;
-        }
+            return new AuthenticationTicket(context.Principal, context.Properties, Scheme.Name);        }
 
         protected override async Task<OAuthTokenResponse> ExchangeCodeAsync([NotNull] string code, [NotNull] string redirectUri)
         {
