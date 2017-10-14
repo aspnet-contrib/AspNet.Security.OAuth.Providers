9a8bc5d50c2a70b911a82f8a05dcabc500532ebb
Семёновых Алексей <ds.lsnk@gmail.com>
2017-10-14 23:15:32 +0300

    Migrate the Vkontakte provider to ASP.NET Core 2.0

--- src/AspNet.Security.OAuth.Vkontakte/VkontakteAuthenticationDefaults.cs
+++ src/AspNet.Security.OAuth.Vkontakte/VkontakteAuthenticationDefaults.cs
@@ -4,7 +4,8 @@
  * for more information concerning the license and the contributors participating to this project.
  */
 
-using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.OAuth;
 
 namespace AspNet.Security.OAuth.Vkontakte
 {
@@ -14,17 +15,17 @@ namespace AspNet.Security.OAuth.Vkontakte
     public static class VkontakteAuthenticationDefaults
     {
         /// <summary>
-        /// Default value for <see cref="AuthenticationOptions.AuthenticationScheme"/>.
+        /// Default value for <see cref="AuthenticationScheme.Name"/>.
         /// </summary>
         public const string AuthenticationScheme = "Vkontakte";
 
         /// <summary>
-        /// Default value for <see cref="RemoteAuthenticationOptions.DisplayName"/>.
+        /// Default value for <see cref="AuthenticationScheme.DisplayName"/>.
         /// </summary>
         public const string DisplayName = "Vkontakte";
 
         /// <summary>
-        /// Default value for <see cref="AuthenticationOptions.ClaimsIssuer"/>.
+        /// Default value for <see cref="AuthenticationSchemeOptions.ClaimsIssuer"/>.
         /// </summary>
         public const string Issuer = "Vkontakte";
 
