d99395f25dd2835039853e06048225c43bf9af3c
Chino Chang <chino@joymoe.com>
2017-11-03 02:17:09 +0800

    Migrate the EVEOnline provider to ASP.NET Core 2.0

--- src/AspNet.Security.OAuth.EVEOnline/EVEOnlineAuthenticationOptions.cs
+++ src/AspNet.Security.OAuth.EVEOnline/EVEOnlineAuthenticationOptions.cs
@@ -5,7 +5,9 @@
  */
 
 using System;
-using Microsoft.AspNetCore.Builder;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.OAuth;
 using Microsoft.AspNetCore.Http;
 
 namespace AspNet.Security.OAuth.EVEOnline
@@ -17,13 +19,15 @@ namespace AspNet.Security.OAuth.EVEOnline
     {
         public EVEOnlineAuthenticationOptions()
         {
-            AuthenticationScheme = EVEOnlineAuthenticationDefaults.AuthenticationScheme;
-            DisplayName = EVEOnlineAuthenticationDefaults.DisplayName;
             ClaimsIssuer = EVEOnlineAuthenticationDefaults.Issuer;
-
             CallbackPath = new PathString(EVEOnlineAuthenticationDefaults.CallbackPath);
 
             Server = EVEOnlineAuthenticationServer.Tranquility;
+
+            ClaimActions.MapJsonKey(ClaimTypes.NameIdentifier, "CharacterID");
+            ClaimActions.MapJsonKey(ClaimTypes.Name, "CharacterName");
+            ClaimActions.MapJsonKey(ClaimTypes.Expiration, "ExpiresOn");
+            ClaimActions.MapJsonKey("urn:eveonline:scopes", "Scopes");
         }
 
         /// <summary>
