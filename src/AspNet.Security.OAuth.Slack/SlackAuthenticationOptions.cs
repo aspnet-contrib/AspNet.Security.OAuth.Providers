73da8b6a8317f66ec34ad3503005890920b55bff
Chino Chang <chino@joymoe.com>
2017-11-03 12:41:22 +0800

    Migrate the Slack provider to ASP.NET Core 2.0

--- src/AspNet.Security.OAuth.Slack/SlackAuthenticationOptions.cs
+++ src/AspNet.Security.OAuth.Slack/SlackAuthenticationOptions.cs
@@ -4,7 +4,9 @@
  * for more information concerning the license and the contributors participating to this project.
  */
 
-using Microsoft.AspNetCore.Builder;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.OAuth;
 using Microsoft.AspNetCore.Http;
 
 namespace AspNet.Security.OAuth.Slack
@@ -16,8 +18,6 @@ namespace AspNet.Security.OAuth.Slack
     {
         public SlackAuthenticationOptions()
         {
-            AuthenticationScheme = SlackAuthenticationDefaults.AuthenticationScheme;
-            DisplayName = SlackAuthenticationDefaults.DisplayName;
             ClaimsIssuer = SlackAuthenticationDefaults.Issuer;
 
             CallbackPath = new PathString(SlackAuthenticationDefaults.CallbackPath);
@@ -25,6 +25,17 @@ namespace AspNet.Security.OAuth.Slack
             AuthorizationEndpoint = SlackAuthenticationDefaults.AuthorizationEndpoint;
             TokenEndpoint = SlackAuthenticationDefaults.TokenEndpoint;
             UserInformationEndpoint = SlackAuthenticationDefaults.UserInformationEndpoint;
+
+            ClaimActions.MapJsonKey(ClaimTypes.NameIdentifier, "user_id");
+            ClaimActions.MapJsonKey(ClaimTypes.Name, "user");
+            ClaimActions.MapJsonKey("urn:slack:team_id", "team_id");
+            ClaimActions.MapJsonKey("urn:slack:team_name", "team");
+            ClaimActions.MapJsonKey("urn:slack:team_url", "url");
+            ClaimActions.MapCustomJson("urn:slack:bot:user_id", user => user["bot"]?.Value<string>("bot_user_id"));
+            ClaimActions.MapCustomJson("urn:slack:bot:access_token", user => user["bot"]?.Value<string>("bot_access_token"));
+            ClaimActions.MapCustomJson("urn:slack:webhook:channel", user => user["incoming_webhook"]?.Value<string>("channel"));
+            ClaimActions.MapCustomJson("urn:slack:webhook:url", user => user["incoming_webhook"]?.Value<string>("url"));
+            ClaimActions.MapCustomJson("urn:slack:webhook:configuration_url", user => user["incoming_webhook"]?.Value<string>("configuration_url"));
         }
     }
 }
