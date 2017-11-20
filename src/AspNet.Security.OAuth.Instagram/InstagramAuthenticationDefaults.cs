a8b79a2469da1619191d9fa6a6f11bc8aa6d78c4
Chino Chang <chino@joymoe.com>
2017-11-20 22:55:05 +0800

    Migrate the Instagram provider to ASP.NET Core 2.0

--- src/AspNet.Security.OAuth.Instagram/InstagramAuthenticationDefaults.cs
+++ src/AspNet.Security.OAuth.Instagram/InstagramAuthenticationDefaults.cs
@@ -4,7 +4,8 @@
  * for more information concerning the license and the contributors participating to this project.
  */
 
-using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.OAuth;
 
 namespace AspNet.Security.OAuth.Instagram
 {
@@ -14,17 +15,17 @@ namespace AspNet.Security.OAuth.Instagram
     public static class InstagramAuthenticationDefaults
     {
         /// <summary>
-        /// Default value for <see cref="AuthenticationOptions.AuthenticationScheme"/>.
+        /// Default value for <see cref="AuthenticationScheme.Name"/>.
         /// </summary>
         public const string AuthenticationScheme = "Instagram";
 
         /// <summary>
-        /// Default value for <see cref="RemoteAuthenticationOptions.DisplayName"/>.
+        /// Default value for <see cref="AuthenticationScheme.DisplayName"/>.
         /// </summary>
         public const string DisplayName = "Instagram";
 
         /// <summary>
-        /// Default value for <see cref="AuthenticationOptions.ClaimsIssuer"/>.
+        /// Default value for <see cref="AuthenticationSchemeOptions.ClaimsIssuer"/>.
         /// </summary>
         public const string Issuer = "Instagram";
 
