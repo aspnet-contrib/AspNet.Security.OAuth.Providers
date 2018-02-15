d68257822cc2d593758eb4fbda1d257b0e60307b
Kristian Hellang <kristian@hellang.com>
2018-02-16 00:34:10 +0100

    Add WeiboClaimTypes

--- src/AspNet.Security.OAuth.Weibo/WeiboAuthenticationOptions.cs
+++ src/AspNet.Security.OAuth.Weibo/WeiboAuthenticationOptions.cs
@@ -30,12 +30,12 @@ namespace AspNet.Security.OAuth.Weibo
             ClaimActions.MapJsonKey(ClaimTypes.NameIdentifier, "id");
             ClaimActions.MapJsonKey(ClaimTypes.Name, "name");
             ClaimActions.MapJsonKey(ClaimTypes.Gender, "gender");
-            ClaimActions.MapJsonKey("urn:weibo:screen_name", "screen_name");
-            ClaimActions.MapJsonKey("urn:weibo:profile_image_url", "profile_image_url");
-            ClaimActions.MapJsonKey("urn:weibo:avatar_large", "avatar_large");
-            ClaimActions.MapJsonKey("urn:weibo:avatar_hd", "avatar_hd");
-            ClaimActions.MapJsonKey("urn:weibo:cover_image_phone", "cover_image_phone");
-            ClaimActions.MapJsonKey("urn:weibo:location", "location");
+            ClaimActions.MapJsonKey(WeiboClaimTypes.ScreenName, "screen_name");
+            ClaimActions.MapJsonKey(WeiboClaimTypes.ProfileImageUrl, "profile_image_url");
+            ClaimActions.MapJsonKey(WeiboClaimTypes.AvatarLarge, "avatar_large");
+            ClaimActions.MapJsonKey(WeiboClaimTypes.AvatarHd, "avatar_hd");
+            ClaimActions.MapJsonKey(WeiboClaimTypes.CoverImagePhone, "cover_image_phone");
+            ClaimActions.MapJsonKey(WeiboClaimTypes.Location, "location");
         }
     }
 }
