cb903bef0d4c4bed8e1d78467401d40ec4d1ce8e
dance2die <dontbothermewithspam@gmail.com>
2018-02-22 21:45:26 -0500

    First working version of Meetup provider

---
+++ src/AspNet.Security.OAuth.Meetup/MeetupAuthenticationDefaults.cs
@@ -0,0 +1,52 @@
+﻿/*
+ * Licensed under the Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
+ * See https://github.com/aspnet-contrib/AspNet.Security.OAuth.Providers
+ * for more information concerning the license and the contributors participating to this project.
+ */
+
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.OAuth;
+
+namespace AspNet.Security.OAuth.Meetup
+{
+    /// <summary>
+    /// Default values used by the Meetup authentication middleware.
+    /// </summary>
+    public static class MeetupAuthenticationDefaults
+    {
+        /// <summary>
+        /// Default value for <see cref="AuthenticationScheme.Name"/>.
+        /// </summary>
+        public const string AuthenticationScheme = "Meetup";
+
+        /// <summary>
+        /// Default value for <see cref="AuthenticationScheme.DisplayName"/>.
+        /// </summary>
+        public const string DisplayName = "Meetup";
+
+        /// <summary>
+        /// Default value for <see cref="AuthenticationSchemeOptions.ClaimsIssuer"/>.
+        /// </summary>
+        public const string Issuer = "Meetup";
+
+        /// <summary>
+        /// Default value for <see cref="RemoteAuthenticationOptions.CallbackPath"/>.
+        /// </summary>
+        public const string CallbackPath = "/signin-meetup";
+
+        /// <summary>
+        /// Default value for <see cref="OAuthOptions.AuthorizationEndpoint"/>.
+        /// </summary>
+        public const string AuthorizationEndpoint = "https://secure.meetup.com/oauth2/authorize";
+
+        /// <summary>
+        /// Default value for <see cref="OAuthOptions.TokenEndpoint"/>.
+        /// </summary>
+        public const string TokenEndpoint = "https://secure.meetup.com/oauth2/access";
+
+        /// <summary>
+        /// Default value for <see cref="OAuthOptions.UserInformationEndpoint"/>.
+        /// </summary>
+        public const string UserInformationEndpoint = "https://api.meetup.com/members/self";
+    }
+}
---
+++ src/AspNet.Security.OAuth.Meetup/MeetupAuthenticationOptions.cs
@@ -0,0 +1,36 @@
+﻿/*
+ * Licensed under the Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
+ * See https://github.com/aspnet-contrib/AspNet.Security.OAuth.Providers
+ * for more information concerning the license and the contributors participating to this project.
+ */
+
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.OAuth;
+using Microsoft.AspNetCore.Http;
+
+namespace AspNet.Security.OAuth.Meetup
+{
+    /// <summary>
+    /// Defines a set of options used by <see cref="MeetupAuthenticationHandler"/>.
+    /// </summary>
+    public class MeetupAuthenticationOptions : OAuthOptions
+    {
+        public MeetupAuthenticationOptions()
+        {
+            ClaimsIssuer = MeetupAuthenticationDefaults.Issuer;
+
+            CallbackPath = new PathString(MeetupAuthenticationDefaults.CallbackPath);
+
+            AuthorizationEndpoint = MeetupAuthenticationDefaults.AuthorizationEndpoint;
+            TokenEndpoint = MeetupAuthenticationDefaults.TokenEndpoint;
+            UserInformationEndpoint = MeetupAuthenticationDefaults.UserInformationEndpoint;
+
+            ClaimActions.MapJsonKey(ClaimTypes.NameIdentifier, "id");
+            ClaimActions.MapJsonKey(ClaimTypes.Name, "login");
+            ClaimActions.MapJsonKey(ClaimTypes.Email, "email");
+            ClaimActions.MapJsonKey("urn:meetup:name", "name");
+            ClaimActions.MapJsonKey("urn:meetup:url", "url");
+        }
+    }
+}
