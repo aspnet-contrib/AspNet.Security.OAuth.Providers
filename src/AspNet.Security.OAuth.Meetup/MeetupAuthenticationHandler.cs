8f6c909fd19ecaf329c820146959ea2f14693967
dance2die <dontbothermewithspam@gmail.com>
2018-02-23 21:33:40 -0500

    Fixed indentation as suggested by kinosang on #220

--- src/AspNet.Security.OAuth.Meetup/MeetupAuthenticationHandler.cs
+++ src/AspNet.Security.OAuth.Meetup/MeetupAuthenticationHandler.cs
@@ -18,45 +18,45 @@ using Newtonsoft.Json.Linq;
 
 namespace AspNet.Security.OAuth.Meetup
 {
-	public class MeetupAuthenticationHandler : OAuthHandler<MeetupAuthenticationOptions>
-	{
-		public MeetupAuthenticationHandler(
-			[NotNull] IOptionsMonitor<MeetupAuthenticationOptions> options,
-			[NotNull] ILoggerFactory logger,
-			[NotNull] UrlEncoder encoder,
-			[NotNull] ISystemClock clock)
-			: base(options, logger, encoder, clock)
-		{
-		}
+    public class MeetupAuthenticationHandler : OAuthHandler<MeetupAuthenticationOptions>
+    {
+        public MeetupAuthenticationHandler(
+            [NotNull] IOptionsMonitor<MeetupAuthenticationOptions> options,
+            [NotNull] ILoggerFactory logger,
+            [NotNull] UrlEncoder encoder,
+            [NotNull] ISystemClock clock)
+            : base(options, logger, encoder, clock)
+        {
+        }
 
-		protected override async Task<AuthenticationTicket> CreateTicketAsync([NotNull] ClaimsIdentity identity,
-			[NotNull] AuthenticationProperties properties, [NotNull] OAuthTokenResponse tokens)
-		{
-			var request = new HttpRequestMessage(HttpMethod.Get, Options.UserInformationEndpoint);
-			request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-			request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", tokens.AccessToken);
+        protected override async Task<AuthenticationTicket> CreateTicketAsync([NotNull] ClaimsIdentity identity,
+            [NotNull] AuthenticationProperties properties, [NotNull] OAuthTokenResponse tokens)
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get, Options.UserInformationEndpoint);
+            request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", tokens.AccessToken);
 
-			var response = await Backchannel.SendAsync(request, HttpCompletionOption.ResponseHeadersRead, Context.RequestAborted);
-			if (!response.IsSuccessStatusCode)
-			{
-				Logger.LogError("An error occurred while retrieving the user profile: the remote server " +
-								"returned a {Status} response with the following payload: {Headers} {Body}.",
-								/* Status: */ response.StatusCode,
-								/* Headers: */ response.Headers.ToString(),
-								/* Body: */ await response.Content.ReadAsStringAsync());
+            var response = await Backchannel.SendAsync(request, HttpCompletionOption.ResponseHeadersRead, Context.RequestAborted);
+            if (!response.IsSuccessStatusCode)
+            {
+                Logger.LogError("An error occurred while retrieving the user profile: the remote server " +
+                                "returned a {Status} response with the following payload: {Headers} {Body}.",
+                                /* Status: */ response.StatusCode,
+                                /* Headers: */ response.Headers.ToString(),
+                                /* Body: */ await response.Content.ReadAsStringAsync());
 
-				throw new HttpRequestException("An error occurred while retrieving the user profile.");
-			}
+                throw new HttpRequestException("An error occurred while retrieving the user profile.");
+            }
 
-			var payload = JObject.Parse(await response.Content.ReadAsStringAsync());
+            var payload = JObject.Parse(await response.Content.ReadAsStringAsync());
 
-			var principal = new ClaimsPrincipal(identity);
-			var context = new OAuthCreatingTicketContext(principal, properties, Context, Scheme, Options, Backchannel, tokens, payload);
-			context.RunClaimActions(payload);
+            var principal = new ClaimsPrincipal(identity);
+            var context = new OAuthCreatingTicketContext(principal, properties, Context, Scheme, Options, Backchannel, tokens, payload);
+            context.RunClaimActions(payload);
 
-			await Options.Events.CreatingTicket(context);
+            await Options.Events.CreatingTicket(context);
 
-			return new AuthenticationTicket(context.Principal, context.Properties, Scheme.Name);
-		}
-	}
+            return new AuthenticationTicket(context.Principal, context.Properties, Scheme.Name);
+        }
+    }
 }
