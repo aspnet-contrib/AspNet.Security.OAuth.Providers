4dbdd668bbd10b19b4ad3a8475e7e9c071e6ae82
martincostello <martin@martincostello.com>
2019-05-12 17:12:16 +0100

    Fix Travis CI test
    Fix test by using 2019 instead of 2099 so that Travis CI doesn't decide that the year is 1999 instead.

--- test/AspNet.Security.OAuth.Providers.Tests/EVEOnline/EVEOnlineTests.cs
+++ test/AspNet.Security.OAuth.Providers.Tests/EVEOnline/EVEOnlineTests.cs
@@ -30,7 +30,7 @@ namespace AspNet.Security.OAuth.EVEOnline
         [Theory]
         [InlineData(ClaimTypes.NameIdentifier, "my-id")]
         [InlineData(ClaimTypes.Name, "John Smith")]
-        [InlineData(ClaimTypes.Expiration, "2099-12-31T23:59:59+00:00")]
+        [InlineData(ClaimTypes.Expiration, "2019-12-31T23:59:59+00:00")]
         [InlineData("urn:eveonline:scopes", "my-scopes")]
         public async Task Can_Sign_In_Using_EVE_Online(string claimType, string claimValue)
         {
