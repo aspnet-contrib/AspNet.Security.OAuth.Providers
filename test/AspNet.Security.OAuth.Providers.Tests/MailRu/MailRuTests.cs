55a6e7276111bbdc3b41e69066a2cfe92ebfa806
Dmitry Vostryakov <Dmitry.Vostryakov@sakhalinenergy.ru>
2019-05-31 08:49:48 +1100

    MailRu provider: add line to readme with provider information, clean code, new test for gender

--- test/AspNet.Security.OAuth.Providers.Tests/MailRu/MailRuTests.cs
+++ test/AspNet.Security.OAuth.Providers.Tests/MailRu/MailRuTests.cs
@@ -36,6 +36,7 @@ namespace AspNet.Security.OAuth.MailRu
         [InlineData(ClaimTypes.Email, "vasya@mail.ru")]
         [InlineData(ClaimTypes.GivenName, "Vasiliy")]
         [InlineData(ClaimTypes.Surname, "Ivanov")]
+        [InlineData(ClaimTypes.Gender, "m")]
         [InlineData("urn:mailru:profileimage", "https://filin.mail.ru/pic?d=idofpic&width=180&height=180")]
         public async Task Can_Sign_In_Using_MailRu(string claimType, string claimValue)
         {
