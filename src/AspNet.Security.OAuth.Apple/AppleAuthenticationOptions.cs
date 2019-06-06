cffa39572d09db3b06650d2d8f7bfb7799b2a736
martincostello <martin@martincostello.com>
2019-06-06 22:52:41 +0100

    Basic Apple provider
    Add a very basic Sign In with Apple provider based on currently available information.

---
+++ src/AspNet.Security.OAuth.Apple/AppleAuthenticationOptions.cs
@@ -0,0 +1,35 @@
+﻿/*
+ * Licensed under the Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
+ * See https://github.com/aspnet-contrib/AspNet.Security.OAuth.Providers
+ * for more information concerning the license and the contributors participating to this project.
+ */
+
+using Microsoft.AspNetCore.Authentication.OAuth;
+
+namespace AspNet.Security.OAuth.Apple
+{
+    /// <summary>
+    /// Defines a set of options used by <see cref="AppleAuthenticationHandler"/>.
+    /// </summary>
+    public class AppleAuthenticationOptions : OAuthOptions
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AppleAuthenticationOptions"/> class.
+        /// </summary>
+        public AppleAuthenticationOptions()
+        {
+            ClaimsIssuer = AppleAuthenticationDefaults.Issuer;
+            CallbackPath = AppleAuthenticationDefaults.CallbackPath;
+
+            AuthorizationEndpoint = AppleAuthenticationDefaults.AuthorizationEndpoint;
+            TokenEndpoint = AppleAuthenticationDefaults.TokenEndpoint;
+            UserInformationEndpoint = AppleAuthenticationDefaults.UserInformationEndpoint;
+
+            // TODO Add any required scopes
+            // Scope.Add("?");
+
+            // TODO Map any claims
+            // ClaimActions.MapJsonKey(ClaimTypes.Email, "?");
+        }
+    }
+}
