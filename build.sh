16f21211dfce8b3aa2f915e40f8e5a01194ae3fe
martincostello <martin@martincostello.com>
2019-06-22 12:25:13 +0100

    Remove KoreBuild
    Remove usage of KoreBuild as it is obsolete and no longer supports .NET Core 3.0 after preview 5.
    Add code coverage to test project.

--- build.sh
+++ build.sh
@@ -5,4 +5,4 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Call "sync" between "chmod" and execution to prevent "text file busy" error in Docker (aufs)
 chmod +x "$DIR/run.sh"; sync
-"$DIR/run.sh" default-build "$@"
+"$DIR/run.sh" "$@"
