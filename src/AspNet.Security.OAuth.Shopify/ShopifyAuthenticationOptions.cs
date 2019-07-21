a3e4fe5ad05a9cb4a8d85f178bc6da29927b753c
Sébastien BIAUDET <sebastien.biaudet@gmail.com>
2019-07-21 14:38:35 +0200

    Add shopify provider (#326)
    * Add shopify provider
    
    * Revert sln changes
    
    * Apply code review
    
    * Fix second core review
    
    * Fix ChallentAsync param comment
    
    * Fix scope summary
    
    * Done pull request review #3
    
    * Revert whitespace
    
    * Update ShopifyLoopbackRedirectHandler.cs
    
    * Update ShopifyAuthenticationExtensions.cs
    
    * Update ShopifyAuthenticationHandler.cs
    
    * Update ShopifyAuthenticationOptions.cs
    
    * Update ShopifyTests.cs
    
    * Update README.md
    
    * Update README.md

---
+++ src/AspNet.Security.OAuth.Shopify/ShopifyAuthenticationOptions.cs
@@ -0,0 +1,43 @@
+﻿/*
+ * Licensed under the Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
+ * See https://github.com/aspnet-contrib/AspNet.Security.OAuth.Providers
+ * for more information concerning the license and the contributors participating to this project.
+ */
+
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.OAuth;
+using Microsoft.AspNetCore.Http;
+
+namespace AspNet.Security.OAuth.Shopify
+{
+    /// <summary>
+    /// Defines a set of options used by <see cref="ShopifyAuthenticationHandler"/>.
+    /// </summary>
+    public class ShopifyAuthenticationOptions : OAuthOptions
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ShopifyAuthenticationOptions"/> class.
+        /// </summary>
+        public ShopifyAuthenticationOptions()
+        {
+            ClaimsIssuer = ShopifyAuthenticationDefaults.Issuer;
+            CallbackPath = ShopifyAuthenticationDefaults.CallbackPath;
+            AuthorizationEndpoint = ShopifyAuthenticationDefaults.AuthorizationEndpointFormat;
+            TokenEndpoint = ShopifyAuthenticationDefaults.TokenEndpointFormat;
+            UserInformationEndpoint = ShopifyAuthenticationDefaults.UserInformationEndpointFormat;
+            
+            ClaimActions.MapJsonSubKey(ClaimTypes.NameIdentifier, "shop", "myshopify_domain");
+            ClaimActions.MapJsonSubKey(ClaimTypes.Name, "shop", "name");
+            ClaimActions.MapJsonSubKey(ClaimTypes.Webpage, "shop", "domain");
+            ClaimActions.MapJsonSubKey(ClaimTypes.Email, "shop", "email");
+            ClaimActions.MapJsonSubKey(ClaimTypes.Country, "shop", "country_code");
+            ClaimActions.MapJsonSubKey(ClaimTypes.StateOrProvince, "shop", "province_code");
+            ClaimActions.MapJsonSubKey(ClaimTypes.PostalCode, "shop", "zip");
+            ClaimActions.MapJsonSubKey(ClaimTypes.Locality, "shop", "primary_locale");
+            ClaimActions.MapJsonSubKey(ShopifyAuthenticationDefaults.ShopifyPlanNameClaimType, "shop", "plan_name");
+            ClaimActions.MapJsonSubKey(ShopifyAuthenticationDefaults.ShopifyEligibleForPaymentsClaimType, "shop", "eligible_for_payments", ClaimValueTypes.Boolean);
+            ClaimActions.MapJsonSubKey(ShopifyAuthenticationDefaults.ShopifyTimezoneClaimType, "shop", "timezone");
+        }
+    }
+}
--- test/AspNet.Security.OAuth.Providers.Tests/OAuthTests`1.cs
+++ test/AspNet.Security.OAuth.Providers.Tests/OAuthTests`1.cs
@@ -19,6 +19,7 @@ using MartinCostello.Logging.XUnit;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.OAuth;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.Extensions.DependencyInjection;
 using Shouldly;
@@ -38,6 +39,8 @@ namespace AspNet.Security.OAuth
             Interceptor = new HttpClientInterceptorOptions()
                 .ThrowsOnMissingRegistration()
                 .RegisterBundle(Path.Combine(GetType().Name.Replace("Tests", string.Empty), "bundle.json"));
+
+            LoopbackRedirectHandler = new LoopbackRedirectHandler { RedirectUri = RedirectUri };
         }
 
         /// <summary>
@@ -106,6 +109,14 @@ namespace AspNet.Security.OAuth
         protected HttpClient CreateBackchannel(AuthenticationBuilder builder)
             => builder.Services.BuildServiceProvider().GetRequiredService<IHttpClientFactory>().CreateClient();
 
+        public DelegatingHandler LoopbackRedirectHandler { get; set; }
+
+        /// <summary>
+        /// Run the ChannelAsync for authentication 
+        /// </summary>
+        /// <param name="context">The HTTP context</param>
+        protected internal virtual Task ChallengeAsync(HttpContext context) => context.ChallengeAsync();
+
         /// <summary>
         /// Asynchronously authenticates the user and returns the claims associated with the authenticated user.
         /// </summary>
@@ -119,7 +130,7 @@ namespace AspNet.Security.OAuth
 
             // Arrange - Force a request chain that challenges the request to the authentication
             // handler and returns an authentication cookie to log the user in to the application.
-            using (var client = server.CreateDefaultClient(new LoopbackRedirectHandler() { RedirectUri = RedirectUri }))
+            using (var client = server.CreateDefaultClient(LoopbackRedirectHandler))
             {
                 // Act
                 using (var result = await client.GetAsync("/me"))
