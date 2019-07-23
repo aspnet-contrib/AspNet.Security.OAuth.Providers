90a58ccf9ef6ff53463be744186a88371e234883
twsl <45483159+twsI@users.noreply.github.com>
2019-07-23 20:01:31 +0200

    Gitlab (#329)
    * Added gitlab oauth
    
    * Fixed some minor typos & restored wildcard include
    
    * Applied suggested changes from code review
    
    * Added missing version var to tests
    
    * Added missing release build config
    
    * Added Gitlab back to solution
    
    * Fix VS auto resolving the references
    
    * Revert weird bundle exclude

---
+++ src/AspNet.Security.OAuth.GitLab/GitLabAuthenticationConstants.cs
@@ -0,0 +1,21 @@
+﻿/*
+ * Licensed under the Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
+ * See https://github.com/aspnet-contrib/AspNet.Security.OAuth.Providers
+ * for more information concerning the license and the contributors participating to this project.
+ */
+
+namespace AspNet.Security.OAuth.GitLab
+{
+    /// <summary>
+    /// Contains constants specific to the <see cref="GitLabAuthenticationHandler"/>.
+    /// </summary>
+    public static class GitLabAuthenticationConstants
+    {
+        public static class Claims
+        {
+            public const string Name = "urn:gitlab:name";
+            public const string Avatar = "urn:gitlab:avatar";
+            public const string Url = "urn:gitlab:url";
+        }
+    }
+}
