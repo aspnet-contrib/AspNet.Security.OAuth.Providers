22e56f60de4c076802cff7955b7214eaa1dcc58c
Dennis E <zaflu@justgameplay.de>
2019-09-21 12:42:37 +0200

    Add Nextcloud provider (#325)
    * Add Nextcloud provider and unit test
    
    * updated according to suggestions
    
    * Change ClaimTypes.Name to Claims.Username

---
+++ src/AspNet.Security.OAuth.Nextcloud/NextcloudAuthenticationConstants.cs
@@ -0,0 +1,24 @@
+﻿/*
+ * Licensed under the Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
+ * See https://github.com/aspnet-contrib/AspNet.Security.OAuth.Providers
+ * for more information concerning the license and the contributors participating to this project.
+ */
+
+namespace AspNet.Security.OAuth.Nextcloud
+{
+    /// <summary>
+    /// Contains constants specific to the <see cref="NextcloudAuthenticationHandler"/>.
+    /// </summary>
+    public static class NextcloudAuthenticationConstants
+    {
+        public static class Claims
+        {
+            public const string Groups = "urn:nextcloud:groups";
+            public const string Username = "urn:nextcloud:username";
+            public const string DisplayName = "urn:nextcloud:displayname";
+            public const string IsEnabled = "urn:nextcloud:enabled";
+            public const string Language = "urn:nextcloud:language";
+            public const string Locale = "urn:nextcloud:locale";
+        }
+    }
+}
