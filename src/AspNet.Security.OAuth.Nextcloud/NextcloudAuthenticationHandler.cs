0428f5160bf848066ceefeb6fc14b074ad17f920
martincostello <martin@martincostello.com>
2019-09-21 12:22:59 +0100

    Update Nextcloud for ASP.NET Core 3.0
    Update the Nextcloud provider to support ASP.NET Core 3.0.

--- src/AspNet.Security.OAuth.Nextcloud/NextcloudAuthenticationHandler.cs
+++ src/AspNet.Security.OAuth.Nextcloud/NextcloudAuthenticationHandler.cs
@@ -9,13 +9,13 @@ using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Security.Claims;
 using System.Text.Encodings.Web;
+using System.Text.Json;
 using System.Threading.Tasks;
 using JetBrains.Annotations;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.OAuth;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
-using Newtonsoft.Json.Linq;
 
 namespace AspNet.Security.OAuth.Nextcloud
 {
@@ -30,18 +30,20 @@ namespace AspNet.Security.OAuth.Nextcloud
         {
         }
 
-        protected override async Task<AuthenticationTicket> CreateTicketAsync([NotNull] ClaimsIdentity identity,
-            [NotNull] AuthenticationProperties properties, [NotNull] OAuthTokenResponse tokens)
+        protected override async Task<AuthenticationTicket> CreateTicketAsync(
+            [NotNull] ClaimsIdentity identity,
+            [NotNull] AuthenticationProperties properties,
+            [NotNull] OAuthTokenResponse tokens)
         {
-            string userId = tokens.Response.Value<string>("user_id");
+            string userId = tokens.Response.RootElement.GetString("user_id");
             string userEndpoint = Options.UserInformationEndpoint.TrimEnd('/');
             userEndpoint += $"/{Uri.EscapeUriString(userId)}";
 
-            var request = new HttpRequestMessage(HttpMethod.Get, userEndpoint);
+            using var request = new HttpRequestMessage(HttpMethod.Get, userEndpoint);
             request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
             request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", tokens.AccessToken);
 
-            var response = await Backchannel.SendAsync(request, HttpCompletionOption.ResponseHeadersRead, Context.RequestAborted);
+            using var response = await Backchannel.SendAsync(request, HttpCompletionOption.ResponseHeadersRead, Context.RequestAborted);
             if (!response.IsSuccessStatusCode)
             {
                 Logger.LogError("An error occurred while retrieving the user profile: the remote server " +
@@ -53,11 +55,11 @@ namespace AspNet.Security.OAuth.Nextcloud
                 throw new HttpRequestException("An error occurred while retrieving the user profile.");
             }
 
-            var payload = JObject.Parse(await response.Content.ReadAsStringAsync());
+            using var payload = JsonDocument.Parse(await response.Content.ReadAsStringAsync());
 
             var principal = new ClaimsPrincipal(identity);
-            var context = new OAuthCreatingTicketContext(principal, properties, Context, Scheme, Options, Backchannel, tokens, payload);
-            context.RunClaimActions(payload);
+            var context = new OAuthCreatingTicketContext(principal, properties, Context, Scheme, Options, Backchannel, tokens, payload.RootElement);
+            context.RunClaimActions();
 
             await Options.Events.CreatingTicket(context);
             return new AuthenticationTicket(context.Principal, context.Properties, Scheme.Name);
