68b74bc07410712030c57d5b81154eddc6bd4100
Chiva Chen <q932104843@outlook.com>
2020-02-05 12:16:45 +0800

    Add Gitee support

---
+++ src/AspNet.Security.OAuth.Gitee/GiteeAuthenticationOptions.cs
@@ -0,0 +1,18 @@
+﻿/*
+ * Licensed under the Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
+ * See https://github.com/aspnet-contrib/AspNet.Security.OAuth.Providers
+ * for more information concerning the license and the contributors participating to this project.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Authentication.OAuth;
+
+namespace AspNet.Security.OAuth.Gitee
+{
+    public class GiteeAuthenticationOptions : OAuthOptions
+    {
+        public string UserEmailsEndpoint { get; set; } = GiteeAuthenticationDefaults.UserEmailsEndpoint;
+    }
+}
