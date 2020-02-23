1a7ee804badfe1d51a950afd16fecc9b4aa72003
Chiva Chen <q932104843@outlook.com>
2020-02-23 14:27:25 +0800

    Add NetEase Tests

---
+++ test/AspNet.Security.OAuth.Providers.Tests/NetEase/NetEaseTests.cs
@@ -0,0 +1,46 @@
+﻿/*
+ * Licensed under the Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
+ * See https://github.com/aspnet-contrib/AspNet.Security.OAuth.Providers
+ * for more information concerning the license and the contributors participating to this project.
+ */
+
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace AspNet.Security.OAuth.NetEase
+{
+    public class NetEaseTests : OAuthTests<NetEaseAuthenticationOptions>
+    {
+        public NetEaseTests(ITestOutputHelper outputHelper)
+        {
+            OutputHelper = outputHelper;
+        }
+
+        public override string DefaultScheme => NetEaseAuthenticationDefaults.AuthenticationScheme;
+
+        protected internal override void RegisterAuthentication(AuthenticationBuilder builder)
+        {
+            builder.AddNetEase(options => ConfigureDefaults(builder, options));
+        }
+
+        [Theory]
+        [InlineData(ClaimTypes.NameIdentifier, "820014421")]
+        [InlineData(ClaimTypes.Name, "urstest_mreg")]
+        [InlineData(ClaimTypes.Email, "urstest_mreg")]
+        public async Task Can_Sign_In_Using_NetEase(string claimType, string claimValue)
+        {
+            // Arrange
+            using var server = CreateTestServer();
+
+            // Act
+            var claims = await AuthenticateUserAsync(server);
+
+            // Assert
+            AssertClaim(claims, claimType, claimValue);
+        }
+    }
+}
