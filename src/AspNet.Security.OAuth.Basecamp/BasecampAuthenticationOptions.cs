b44e276b13904cf4c314f987b3305d2b63a79f33
Luke Fulliton <63476273+lukefulliton@users.noreply.github.com>
2020-04-12 09:31:05 -0500

    Add Basecamp provider (#406)
    * Adding Luke Fulliton as a contributor.
    
    * Added new provider, Basecamp.
    
    * Moved the basecamp provider project location to the same location that the other providers reside.
    
    * Added Basecamp Tests.  Basecamp Tests pass successfully.
    
    * Updated the README.md file
    
    * Removed unnecessary JSON property check in the BasecampAuthenticationHandler.

---
+++ src/AspNet.Security.OAuth.Basecamp/BasecampAuthenticationOptions.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * Licensed under the Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
+ * See https://github.com/aspnet-contrib/AspNet.Security.OAuth.Providers
+ * for more information concerning the license and the contributors participating to this project.
+ */
+
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.OAuth;
+
+namespace AspNet.Security.OAuth.Basecamp
+{
+    /// <summary>
+    /// Defines a set of options used by <see cref="BasecampAuthenticationHandler"/>.
+    /// </summary>
+    public class BasecampAuthenticationOptions : OAuthOptions
+    {
+        public BasecampAuthenticationOptions()
+        {
+            ClaimsIssuer = BasecampAuthenticationDefaults.Issuer;
+            CallbackPath = BasecampAuthenticationDefaults.CallbackPath;
+            AuthorizationEndpoint = BasecampAuthenticationDefaults.AuthorizationEndpoint;
+            TokenEndpoint = BasecampAuthenticationDefaults.TokenEndpoint;
+            UserInformationEndpoint = BasecampAuthenticationDefaults.UserInformationEndpoint;
+
+            ClaimActions.MapJsonSubKey(ClaimTypes.NameIdentifier, "identity", "id");
+            ClaimActions.MapJsonSubKey(ClaimTypes.GivenName, "identity", "first_name");
+            ClaimActions.MapJsonSubKey(ClaimTypes.Surname, "identity", "last_name");
+            ClaimActions.MapJsonSubKey(ClaimTypes.Email, "identity", "email_address");
+        }
+    }
+}
