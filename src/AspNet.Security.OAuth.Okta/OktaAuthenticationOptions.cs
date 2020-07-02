ffb27859abdb79b75165e1e04b0397e915f98984
martincostello <martin@martincostello.com>
2020-07-02 18:43:15 +0100

    Add Okta provider
    Add a provider for Okta to resolve #403.

---
+++ src/AspNet.Security.OAuth.Okta/OktaAuthenticationOptions.cs
@@ -0,0 +1,69 @@
+﻿/*
+ * Licensed under the Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
+ * See https://github.com/aspnet-contrib/AspNet.Security.OAuth.Providers
+ * for more information concerning the license and the contributors participating to this project.
+ */
+
+using System;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.OAuth;
+
+namespace AspNet.Security.OAuth.Okta
+{
+    /// <summary>
+    /// Defines a set of options used by <see cref="OktaAuthenticationHandler"/>.
+    /// </summary>
+    public class OktaAuthenticationOptions : OAuthOptions
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="OktaAuthenticationOptions"/> class.
+        /// </summary>
+        public OktaAuthenticationOptions()
+        {
+            ClaimsIssuer = OktaAuthenticationDefaults.Issuer;
+            CallbackPath = OktaAuthenticationDefaults.CallbackPath;
+
+            AuthorizationEndpoint = OktaAuthenticationDefaults.AuthorizationEndpointFormat;
+            TokenEndpoint = OktaAuthenticationDefaults.TokenEndpointFormat;
+            UserInformationEndpoint = OktaAuthenticationDefaults.UserInformationEndpointFormat;
+
+            Scope.Add("openid");
+            Scope.Add("profile");
+            Scope.Add("email");
+
+            ClaimActions.MapJsonKey(ClaimTypes.Email, "email");
+            ClaimActions.MapJsonKey(ClaimTypes.GivenName, "given_name");
+            ClaimActions.MapJsonKey(ClaimTypes.Name, "name");
+            ClaimActions.MapJsonKey(ClaimTypes.NameIdentifier, "sub");
+            ClaimActions.MapJsonKey(ClaimTypes.Surname, "family_name");
+        }
+
+        /// <inheritdoc/>
+        public override void Validate()
+        {
+            if (!Uri.TryCreate(AuthorizationEndpoint, UriKind.Absolute, out var _))
+            {
+                throw new ArgumentException(
+                    $"The '{nameof(AuthorizationEndpoint)}' option must be set to a valid URI.",
+                    nameof(AuthorizationEndpoint));
+            }
+
+            if (!Uri.TryCreate(TokenEndpoint, UriKind.Absolute, out var _))
+            {
+                throw new ArgumentException(
+                    $"The '{nameof(TokenEndpoint)}' option must be set to a valid URI.",
+                    nameof(TokenEndpoint));
+            }
+
+            if (!Uri.TryCreate(UserInformationEndpoint, UriKind.Absolute, out var _))
+            {
+                throw new ArgumentException(
+                    $"The '{nameof(UserInformationEndpoint)}' option must be set to a valid URI.",
+                    nameof(UserInformationEndpoint));
+            }
+
+            base.Validate();
+        }
+    }
+}
---
+++ src/AspNet.Security.OAuth.Okta/OktaAuthenticationOptionsExtensions.cs
@@ -0,0 +1,45 @@
+﻿/*
+ * Licensed under the Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
+ * See https://github.com/aspnet-contrib/AspNet.Security.OAuth.Providers
+ * for more information concerning the license and the contributors participating to this project.
+ */
+
+using System;
+using System.Globalization;
+using JetBrains.Annotations;
+
+namespace AspNet.Security.OAuth.Okta
+{
+    /// <summary>
+    /// Extension methods to configure Okta authentication capabilities for an HTTP application pipeline.
+    /// </summary>
+    public static class OktaAuthenticationOptionsExtensions
+    {
+        /// <summary>
+        /// Configures the application to use a specified domain for the Okta provider.
+        /// </summary>
+        /// <param name="options">The Okta authentication options to configure.</param>
+        /// <param name="domain">The domain to use for Okta authentication.</param>
+        /// <returns>
+        /// The value of the <paramref name="options"/> argument.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="domain"/> is <see langword="null"/> or white space.
+        /// </exception>
+        public static OktaAuthenticationOptions UseDomain(
+            [NotNull] this OktaAuthenticationOptions options,
+            [NotNull] string domain)
+        {
+            if (string.IsNullOrWhiteSpace(domain))
+            {
+                throw new ArgumentException("No Okta domain name specified.", nameof(domain));
+            }
+
+            options.AuthorizationEndpoint = string.Format(CultureInfo.InvariantCulture, OktaAuthenticationDefaults.AuthorizationEndpointFormat, domain);
+            options.TokenEndpoint = string.Format(CultureInfo.InvariantCulture, OktaAuthenticationDefaults.TokenEndpointFormat, domain);
+            options.UserInformationEndpoint = string.Format(CultureInfo.InvariantCulture, OktaAuthenticationDefaults.UserInformationEndpointFormat, domain);
+
+            return options;
+        }
+    }
+}
