30b79ca60ff883186d825d46b6545fa1ea302bde
martincostello <martin@martincostello.com>
2020-07-10 12:15:33 +0100

    Fix SuperOffice tests
    Fix SuperOffice tests that were failing after the fix in a8f10dbc2f20ed5426b7b0dd111c9bf632ea79dc due to the issuer not being set.

--- test/AspNet.Security.OAuth.Providers.Tests/SuperOffice/SuperOfficeTests.cs
+++ test/AspNet.Security.OAuth.Providers.Tests/SuperOffice/SuperOfficeTests.cs
@@ -35,6 +35,7 @@ namespace AspNet.Security.OAuth.SuperOffice
 
                 options.ClientId = "gg454918d75b1b53101065c16ee51123";
                 options.TokenValidationParameters.ValidAudience = options.ClientId;
+                options.TokenValidationParameters.ValidIssuer = "https://sod.superoffice.com";
             });
         }
 
