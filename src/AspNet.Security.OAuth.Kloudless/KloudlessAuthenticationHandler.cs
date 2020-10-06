884527d1b5380da27a576faa6a5f3e7eda17dc9a
martincostello <martin@martincostello.com>
2020-10-06 18:30:42 +0100

    Update Kloudless provider to .NET 5
    Updates to the Kloudless provider to support .NET 5.

--- src/AspNet.Security.OAuth.Kloudless/KloudlessAuthenticationHandler.cs
+++ src/AspNet.Security.OAuth.Kloudless/KloudlessAuthenticationHandler.cs
@@ -71,7 +71,7 @@ namespace AspNet.Security.OAuth.Kloudless
             context.RunClaimActions(accounts[0]);
 
             await Options.Events.CreatingTicket(context);
-            return new AuthenticationTicket(context.Principal, context.Properties, Scheme.Name);
+            return new AuthenticationTicket(context.Principal!, context.Properties, Scheme.Name);
         }
     }
 }
