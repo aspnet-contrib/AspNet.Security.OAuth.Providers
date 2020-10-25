f00dab7a87cd36cf5b2f7483c0092c451997ff7e
Donghyeon Kim <wplong11@users.noreply.github.com>
2020-10-25 21:33:59 +0900

    Add KakaoTalk provider (#493)
    Add KakaoTalk provider and unit tests

---
+++ src/AspNet.Security.OAuth.KakaoTalk/KakaoTalkAuthenticationConstants.cs
@@ -0,0 +1,27 @@
+﻿/*
+ * Licensed under the Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
+ * See https://github.com/aspnet-contrib/AspNet.Security.OAuth.Providers
+ * for more information concerning the license and the contributors participating to this project.
+ */
+
+namespace AspNet.Security.OAuth.KakaoTalk
+{
+    /// <summary>
+    /// Contains constants specific to the <see cref="KakaoTalkAuthenticationHandler"/>.
+    /// </summary>
+    public static class KakaoTalkAuthenticationConstants
+    {
+        public static class Claims
+        {
+            /// <summary>
+            /// The claim for the user's age range.
+            /// </summary>
+            public const string AgeRange = "urn:kakaotalk:age_range";
+
+            /// <summary>
+            /// The claim for the user's year of birth
+            /// </summary>
+            public const string YearOfBirth = "urn:kakaotalk:year_of_birth";
+        }
+    }
+}
