bb7878a5108e96d34bb83e161d1b70c5510fb87c
LeaFrock <LeaFrock@users.noreply.github.com>
2021-03-13 23:21:35 +0800

    Add Alipay provider (#522)
    * Add Alipay provider
    
    * Add unit testing for Alipay
    
    * Update README.md
    
    * Fix test uri
    
    * Update by suggestions of review.
    
    * Use explicit type instead of var

---
+++ src/AspNet.Security.OAuth.Alipay/AlipayAuthenticationOptions.cs
@@ -0,0 +1,36 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
+ * See https://github.com/aspnet-contrib/AspNet.Security.OAuth.Providers
+ * for more information concerning the license and the contributors participating to this project.
+ */
+
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.OAuth;
+using static AspNet.Security.OAuth.Alipay.AlipayAuthenticationConstants;
+
+namespace AspNet.Security.OAuth.Alipay
+{
+    /// <summary>
+    /// Defines a set of options used by <see cref="AlipayAuthenticationHandler"/>.
+    /// </summary>
+    public class AlipayAuthenticationOptions : OAuthOptions
+    {
+        public AlipayAuthenticationOptions()
+        {
+            ClaimsIssuer = AlipayAuthenticationDefaults.Issuer;
+            CallbackPath = AlipayAuthenticationDefaults.CallbackPath;
+
+            AuthorizationEndpoint = AlipayAuthenticationDefaults.AuthorizationEndpoint;
+            TokenEndpoint = AlipayAuthenticationDefaults.TokenEndpoint;
+            UserInformationEndpoint = AlipayAuthenticationDefaults.UserInformationEndpoint;
+
+            Scope.Add("auth_user");
+
+            ClaimActions.MapJsonKey(Claims.Avatar, "avatar");
+            ClaimActions.MapJsonKey(Claims.City, "city");
+            ClaimActions.MapJsonKey(Claims.Gender, "gender");
+            ClaimActions.MapJsonKey(Claims.Nickname, "nick_name");
+            ClaimActions.MapJsonKey(Claims.Province, "province");
+        }
+    }
+}
