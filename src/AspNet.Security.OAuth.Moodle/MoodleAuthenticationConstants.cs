1d14fc44ae2d82d8edd631cb10e69adc8810347a
宿梓航 <Ferdinandsukhoi@outlook.com>
2021-03-28 15:46:52 +0800

    Add Claims for auth, country and description

--- src/AspNet.Security.OAuth.Moodle/MoodleAuthenticationConstants.cs
+++ src/AspNet.Security.OAuth.Moodle/MoodleAuthenticationConstants.cs
@@ -16,6 +16,7 @@ namespace AspNet.Security.OAuth.Moodle
             public const string IdNumber = "urn:moodle:idnumber";
             public const string MoodleId = "urn:moodle:id";
             public const string Language = "urn:moodle:lang";
+            public const string Description = "urn:moodle:desc";
         }
     }
 }
