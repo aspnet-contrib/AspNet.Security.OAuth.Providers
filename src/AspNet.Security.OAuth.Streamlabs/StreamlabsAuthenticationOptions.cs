3137c89a32a8d232b76e6d2b69058c025ad82033
SertayKabuk <sertaykabuk@gmail.com>
2021-05-31 00:11:33 +0300

    Redundant GetData removed.

--- src/AspNet.Security.OAuth.Streamlabs/StreamlabsAuthenticationOptions.cs
+++ src/AspNet.Security.OAuth.Streamlabs/StreamlabsAuthenticationOptions.cs
@@ -3,9 +3,7 @@
  * See https://github.com/aspnet-contrib/AspNet.Security.OAuth.Providers
  * for more information concerning the license and the contributors participating to this project.
  */
-using System;
 using System.Security.Claims;
-using System.Text.Json;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.OAuth;
 using static AspNet.Security.OAuth.Streamlabs.StreamlabsAuthenticationConstants;
@@ -26,52 +24,19 @@ namespace AspNet.Security.OAuth.Streamlabs
             TokenEndpoint = StreamlabsAuthenticationDefaults.TokenEndpoint;
             UserInformationEndpoint = StreamlabsAuthenticationDefaults.UserInformationEndpoint;
 
-            ClaimActions.MapCustomJson(ClaimTypes.NameIdentifier, user => GetData(user, "streamlabs.id"));
-            ClaimActions.MapCustomJson(ClaimTypes.Name, user => GetData(user, "streamlabs.username"));
-            ClaimActions.MapCustomJson(Claims.DisplayName, user => GetData(user, "streamlabs.display_name"));
-            ClaimActions.MapCustomJson(Claims.FacebookId, user => GetData(user, "facebook.id"));
-            ClaimActions.MapCustomJson(Claims.FacebookName, user => GetData(user, "facebook.name"));
-            ClaimActions.MapCustomJson(Claims.Primary, user => GetData(user, "streamlabs.primary"));
-            ClaimActions.MapCustomJson(Claims.Thumbnail, user => GetData(user, "streamlabs.thumbnail"));
-            ClaimActions.MapCustomJson(Claims.TwitchDisplayName, user => GetData(user, "twitch.display_name"));
-            ClaimActions.MapCustomJson(Claims.TwitchIconUrl, user => GetData(user, "twitch.icon_url"));
-            ClaimActions.MapCustomJson(Claims.TwitchId, user => GetData(user, "twitch.id"));
-            ClaimActions.MapCustomJson(Claims.TwitchName, user => GetData(user, "twitch.name"));
-            ClaimActions.MapCustomJson(Claims.YoutubeId, user => GetData(user, "youtube.id"));
-            ClaimActions.MapCustomJson(Claims.YoutubeTitle, user => GetData(user, "youtube.title"));
-        }
-
-        private static string? GetData(JsonElement user, string key)
-        {
-            user = GetJsonElement(user, key);
-
-            return user.ValueKind != JsonValueKind.Null && user.ValueKind != JsonValueKind.Undefined ?
-              user.ToString() :
-              default;
-        }
-
-        private static JsonElement GetJsonElement(JsonElement jsonElement, string path)
-        {
-            if (jsonElement.ValueKind == JsonValueKind.Null ||
-                jsonElement.ValueKind == JsonValueKind.Undefined)
-            {
-                return default;
-            }
-
-            string[] segments = path.Split(new[] { '.' }, StringSplitOptions.RemoveEmptyEntries);
-
-            for (int n = 0; n < segments.Length; n++)
-            {
-                jsonElement = jsonElement.TryGetProperty(segments[n], out JsonElement value) ? value : default;
-
-                if (jsonElement.ValueKind == JsonValueKind.Null ||
-                    jsonElement.ValueKind == JsonValueKind.Undefined)
-                {
-                    return default;
-                }
-            }
-
-            return jsonElement;
+            ClaimActions.MapJsonSubKey(ClaimTypes.NameIdentifier, "streamlabs", "id");
+            ClaimActions.MapJsonSubKey(ClaimTypes.Name, "streamlabs", "username");
+            ClaimActions.MapJsonSubKey(Claims.DisplayName, "streamlabs", "display_name");
+            ClaimActions.MapJsonSubKey(Claims.FacebookId, "facebook", "id");
+            ClaimActions.MapJsonSubKey(Claims.FacebookName, "facebook", "name");
+            ClaimActions.MapJsonSubKey(Claims.Primary, "streamlabs", "primary");
+            ClaimActions.MapJsonSubKey(Claims.Thumbnail, "streamlabs", "thumbnail");
+            ClaimActions.MapJsonSubKey(Claims.TwitchDisplayName, "twitch", "display_name");
+            ClaimActions.MapJsonSubKey(Claims.TwitchIconUrl, "twitch", "icon_url");
+            ClaimActions.MapJsonSubKey(Claims.TwitchId, "twitch", "id");
+            ClaimActions.MapJsonSubKey(Claims.TwitchName, "twitch", "name");
+            ClaimActions.MapJsonSubKey(Claims.YouTubeId, "youtube", "id");
+            ClaimActions.MapJsonSubKey(Claims.YouTubeTitle, "youtube", "title");
         }
     }
 }
