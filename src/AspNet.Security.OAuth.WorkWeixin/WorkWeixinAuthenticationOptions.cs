c0f7186449e4d44b3d5807d02ed12e443165df10
WangQian <30824982+w757703598@users.noreply.github.com>
2021-06-03 15:33:26 +0800

    Add WorkWeixin (WeCom) Provider (#539)
    * Add a WorkWeixin Provider
    
    * fix avator to avatar
    add Claim alias
    
    * the caption [CanBeNull]
    
    * Implemented suggestions by @martincostello
    
    * add "wecom" description
    
    * Add workweixin.md  and update README
    
    * Implemented suggestions by @LeaFrock
    
    * Implemented suggestions by @martincostello
    
    * update document

---
+++ src/AspNet.Security.OAuth.WorkWeixin/WorkWeixinAuthenticationOptions.cs
@@ -0,0 +1,49 @@
+﻿/*
+ * Licensed under the Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
+ * See https://github.com/aspnet-contrib/AspNet.Security.OAuth.Providers
+ * for more information concerning the license and the contributors participating to this project.
+ */
+
+using System.Linq;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.OAuth;
+using static AspNet.Security.OAuth.WorkWeixin.WorkWeixinAuthenticationConstants;
+
+namespace AspNet.Security.OAuth.WorkWeixin
+{
+    /// <summary>
+    /// Defines a set of options used by <see cref="WorkWeixinAuthenticationHandler"/>.
+    /// </summary>
+    public class WorkWeixinAuthenticationOptions : OAuthOptions
+    {
+        public WorkWeixinAuthenticationOptions()
+        {
+            ClaimsIssuer = WorkWeixinAuthenticationDefaults.Issuer;
+            CallbackPath = WorkWeixinAuthenticationDefaults.CallbackPath;
+
+            AuthorizationEndpoint = WorkWeixinAuthenticationDefaults.AuthorizationEndpoint;
+            TokenEndpoint = WorkWeixinAuthenticationDefaults.TokenEndpoint;
+            UserInformationEndpoint = WorkWeixinAuthenticationDefaults.UserInformationEndpoint;
+            UserIdentificationEndpoint = WorkWeixinAuthenticationDefaults.UserIdentificationEndpoint;
+
+            ClaimActions.MapJsonKey(ClaimTypes.NameIdentifier, "userid");
+            ClaimActions.MapJsonKey(ClaimTypes.Name, "name");
+            ClaimActions.MapJsonKey(ClaimTypes.Gender, "gender");
+            ClaimActions.MapJsonKey(ClaimTypes.Email, "email");
+            ClaimActions.MapJsonKey(Claims.Avatar, "avatar");
+            ClaimActions.MapJsonKey(Claims.Mobile, "mobile");
+            ClaimActions.MapJsonKey(Claims.Alias, "alias");
+        }
+
+        /// <summary>
+        /// Gets or sets the web application ID of the licensor.
+        /// </summary>
+        public string AgentId { get; set; } = default!;
+
+        /// <summary>
+        /// Gets or sets the URL of the user identification endpoint (a.k.a the "OpenID endpoint").
+        /// </summary>
+        public string UserIdentificationEndpoint { get; set; }
+    }
+}
