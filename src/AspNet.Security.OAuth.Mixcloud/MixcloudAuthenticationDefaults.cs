d6ebb28c81a2c08b2bf36fd416b4c5c2044b8047
Mike-E <michael@dragonspark.network>
2021-06-19 03:42:35 -0400

    Basic/initial Mixcloud support

---
+++ src/AspNet.Security.OAuth.Mixcloud/MixcloudAuthenticationDefaults.cs
@@ -0,0 +1,52 @@
+﻿/*
+ * Licensed under the Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
+ * See https://github.com/aspnet-contrib/AspNet.Security.OAuth.Providers
+ * for more information concerning the license and the contributors participating to this project.
+ */
+
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.OAuth;
+
+namespace AspNet.Security.OAuth.Mixcloud
+{
+    /// <summary>
+    /// Default values used by the Mixcloud authentication middleware.
+    /// </summary>
+    public static class MixcloudAuthenticationDefaults
+    {
+        /// <summary>
+        /// Default value for <see cref="Microsoft.AspNetCore.Authentication.AuthenticationScheme.Name"/>.
+        /// </summary>
+        public const string AuthenticationScheme = "Mixcloud";
+
+        /// <summary>
+        /// Default value for <see cref="Microsoft.AspNetCore.Authentication.AuthenticationScheme.DisplayName"/>.
+        /// </summary>
+        public const string DisplayName = "Mixcloud";
+
+        /// <summary>
+        /// Default value for <see cref="AuthenticationSchemeOptions.ClaimsIssuer"/>.
+        /// </summary>
+        public const string Issuer = "Mixcloud";
+
+        /// <summary>
+        /// Default value for <see cref="RemoteAuthenticationOptions.CallbackPath"/>.
+        /// </summary>
+        public const string CallbackPath = "/signin-mixcloud";
+
+        /// <summary>
+        /// Default value for <see cref="OAuthOptions.AuthorizationEndpoint"/>.
+        /// </summary>
+        public const string AuthorizationEndpoint = "https://www.mixcloud.com/oauth/authorize";
+
+        /// <summary>
+        /// Default value for <see cref="OAuthOptions.TokenEndpoint"/>.
+        /// </summary>
+        public const string TokenEndpoint = "https://www.mixcloud.com/oauth/access_token";
+
+        /// <summary>
+        /// Default value for <see cref="OAuthOptions.UserInformationEndpoint"/>.
+        /// </summary>
+        public const string UserInformationEndpoint = "https://api.mixcloud.com/me";
+    }
+}
---
+++ src/AspNet.Security.OAuth.Mixcloud/MixcloudAuthenticationExtensions.cs
@@ -0,0 +1,78 @@
+﻿/*
+ * Licensed under the Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
+ * See https://github.com/aspnet-contrib/AspNet.Security.OAuth.Providers
+ * for more information concerning the license and the contributors participating to this project.
+ */
+
+using System;
+using JetBrains.Annotations;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AspNet.Security.OAuth.Mixcloud
+{
+    /// <summary>
+    /// Extension methods to add  authentication capabilities to an HTTP application pipeline.
+    /// </summary>
+    public static class MixcloudAuthenticationExtensions
+    {
+        /// <summary>
+        /// Adds <see cref="MixcloudAuthenticationHandler"/> to the specified
+        /// <see cref="AuthenticationBuilder"/>, which enables Mixcloud authentication capabilities.
+        /// </summary>
+        /// <param name="builder">The authentication builder.</param>
+        /// <returns>The <see cref="AuthenticationBuilder"/>.</returns>
+        public static AuthenticationBuilder AddMixcloud([NotNull] this AuthenticationBuilder builder)
+        {
+            return builder.AddMixcloud(MixcloudAuthenticationDefaults.AuthenticationScheme, _ => { });
+        }
+
+        /// <summary>
+        /// Adds <see cref="MixcloudAuthenticationHandler"/> to the specified
+        /// <see cref="AuthenticationBuilder"/>, which enables Mixcloud authentication capabilities.
+        /// </summary>
+        /// <param name="builder">The authentication builder.</param>
+        /// <param name="configuration">The delegate used to configure the OpenID 2.0 options.</param>
+        /// <returns>The <see cref="AuthenticationBuilder"/>.</returns>
+        public static AuthenticationBuilder AddMixcloud(
+            [NotNull] this AuthenticationBuilder builder,
+            [NotNull] Action<MixcloudAuthenticationOptions> configuration)
+        {
+            return builder.AddMixcloud(MixcloudAuthenticationDefaults.AuthenticationScheme, configuration);
+        }
+
+        /// <summary>
+        /// Adds <see cref="MixcloudAuthenticationHandler"/> to the specified
+        /// <see cref="AuthenticationBuilder"/>, which enables Mixcloud authentication capabilities.
+        /// </summary>
+        /// <param name="builder">The authentication builder.</param>
+        /// <param name="scheme">The authentication scheme associated with this instance.</param>
+        /// <param name="configuration">The delegate used to configure the Mixcloud options.</param>
+        /// <returns>The <see cref="AuthenticationBuilder"/>.</returns>
+        public static AuthenticationBuilder AddMixcloud(
+            [NotNull] this AuthenticationBuilder builder,
+            [NotNull] string scheme,
+            [NotNull] Action<MixcloudAuthenticationOptions> configuration)
+        {
+            return builder.AddMixcloud(scheme, MixcloudAuthenticationDefaults.DisplayName, configuration);
+        }
+
+        /// <summary>
+        /// Adds <see cref="MixcloudAuthenticationHandler"/> to the specified
+        /// <see cref="AuthenticationBuilder"/>, which enables Mixcloud authentication capabilities.
+        /// </summary>
+        /// <param name="builder">The authentication builder.</param>
+        /// <param name="scheme">The authentication scheme associated with this instance.</param>
+        /// <param name="caption">The optional display name associated with this instance.</param>
+        /// <param name="configuration">The delegate used to configure the Mixcloud options.</param>
+        /// <returns>The <see cref="AuthenticationBuilder"/>.</returns>
+        public static AuthenticationBuilder AddMixcloud(
+            [NotNull] this AuthenticationBuilder builder,
+            [NotNull] string scheme,
+            [CanBeNull] string caption,
+            [NotNull] Action<MixcloudAuthenticationOptions> configuration)
+        {
+            return builder.AddOAuth<MixcloudAuthenticationOptions, MixcloudAuthenticationHandler>(scheme, caption, configuration);
+        }
+    }
+}
