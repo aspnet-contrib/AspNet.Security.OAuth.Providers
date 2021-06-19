014ef58416b9f6dca6ec88a4b7f4e5936ebb2acf
Mike-E <2931376+Mike-E-angelo@users.noreply.github.com>
2021-06-19 05:18:59 -0400

    Add Mixcloud provider (#569)
    * Basic/initial Mixcloud support
    
    * Fixed tests
    
    * Update src/AspNet.Security.OAuth.Mixcloud/MixcloudAuthenticationExtensions.cs
    
    Co-authored-by: Martin Costello <martin@martincostello.com>
    
    * Update src/AspNet.Security.OAuth.Mixcloud/MixcloudAuthenticationHandler.cs
    
    Co-authored-by: Martin Costello <martin@martincostello.com>
    
    * Update src/AspNet.Security.OAuth.Mixcloud/MixcloudAuthenticationHandler.cs
    
    Co-authored-by: Martin Costello <martin@martincostello.com>
    
    * Update src/AspNet.Security.OAuth.Mixcloud/MixcloudAuthenticationOptions.cs
    
    Co-authored-by: Martin Costello <martin@martincostello.com>
    
    * Added biography tests and comments
    
    * Fixed formatting
    
    * Removed authors
    
    * Updated README with Mixcloud listing
    
    Co-authored-by: Martin Costello <martin@martincostello.com>

---
+++ test/AspNet.Security.OAuth.Providers.Tests/Mixcloud/MixcloudTests.cs
@@ -0,0 +1,51 @@
+﻿/*
+ * Licensed under the Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
+ * See https://github.com/aspnet-contrib/AspNet.Security.OAuth.Providers
+ * for more information concerning the license and the contributors participating to this project.
+ */
+
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace AspNet.Security.OAuth.Mixcloud
+{
+    public class MixcloudTests : OAuthTests<MixcloudAuthenticationOptions>
+    {
+        public MixcloudTests(ITestOutputHelper outputHelper)
+        {
+            OutputHelper = outputHelper;
+        }
+
+        public override string DefaultScheme => MixcloudAuthenticationDefaults.AuthenticationScheme;
+
+        protected internal override void RegisterAuthentication(AuthenticationBuilder builder)
+        {
+            builder.AddMixcloud(options => ConfigureDefaults(builder, options));
+        }
+
+        [Theory]
+        [InlineData(ClaimTypes.NameIdentifier, "my-id")]
+        [InlineData(ClaimTypes.Name, "John-Smith")]
+        [InlineData(ClaimTypes.Country, "GB")]
+        [InlineData("urn:mixcloud:biography", "I am John Smith.")]
+        [InlineData("urn:mixcloud:city", "London")]
+        [InlineData("urn:mixcloud:fullname", "John Q Smith")]
+        [InlineData("urn:mixcloud:profileurl", "https://mixcloud.local/JohnSmith")]
+        [InlineData("urn:mixcloud:profileimageurl", "https://mixcloud.local/images/320wx320h")]
+        [InlineData("urn:mixcloud:profilethumbnailurl", "https://mixcloud.local/images/thumbnail")]
+        public async Task Can_Sign_In_Using_Mixcloud(string claimType, string claimValue)
+        {
+            // Arrange
+            using var server = CreateTestServer();
+
+            // Act
+            var claims = await AuthenticateUserAsync(server);
+
+            // Assert
+            AssertClaim(claims, claimType, claimValue);
+        }
+    }
+}
