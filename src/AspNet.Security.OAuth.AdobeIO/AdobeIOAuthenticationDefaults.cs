55e46dc4b7a4f073a9d12d156355456a299566ea
Logan Dam <biltongza@users.noreply.github.com>
2021-07-04 15:59:25 +0200

    Add Adobe IO provider (#573)
    * Add AdobeIO package
    
    * Complete AdobeIO authentication
    
    * Update src/AspNet.Security.OAuth.AdobeIO/AspNet.Security.OAuth.AdobeIO.csproj
    
    Co-authored-by: Martin Costello <martin@martincostello.com>
    
    * Use commas to format scope, allows adding individual scopes properly.
    
    * Remove Endpoint paths.
    
    * Remove unnecessary scope.
    
    * Update readme with AdobeIO
    
    * Use OAuth-specific page for AdobeIO
    
    Co-authored-by: Martin Costello <martin@martincostello.com>
    
    * Use DefaultNetCoreTargetFramework
    
    * Add blank lines for readability.
    
    Co-authored-by: Martin Costello <martin@martincostello.com>

---
+++ src/AspNet.Security.OAuth.AdobeIO/AdobeIOAuthenticationDefaults.cs
@@ -0,0 +1,49 @@
+﻿/*
+ * Licensed under the Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
+ * See https://github.com/aspnet-contrib/AspNet.Security.OAuth.Providers
+ * for more information concerning the license and the contributors participating to this project.
+ */
+
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.OAuth;
+
+namespace AspNet.Security.OAuth.AdobeIO
+{
+    public static class AdobeIOAuthenticationDefaults
+    {
+        /// <summary>
+        /// Default value for <see cref="AuthenticationScheme.Name"/>.
+        /// </summary>
+        public const string AuthenticationScheme = "AdobeIO";
+
+        /// <summary>
+        /// Default value for <see cref="AuthenticationScheme.DisplayName"/>.
+        /// </summary>
+        public const string DisplayName = "AdobeIO";
+
+        /// <summary>
+        /// Default value for <see cref="AuthenticationSchemeOptions.ClaimsIssuer"/>.
+        /// </summary>
+        public const string Issuer = "AdobeIO";
+
+        /// <summary>
+        /// Default value for <see cref="RemoteAuthenticationOptions.CallbackPath"/>.
+        /// </summary>
+        public const string CallbackPath = "/signin-adobeio";
+
+        /// <summary>
+        /// Default value for <see cref="OAuthOptions.AuthorizationEndpoint"/>.
+        /// </summary>
+        public const string AuthorizationEndpoint = "https://ims-na1.adobelogin.com/ims/authorize/v2";
+
+        /// <summary>
+        /// Default value for <see cref="OAuthOptions.TokenEndpoint"/>.
+        /// </summary>
+        public const string TokenEndpoint = "https://ims-na1.adobelogin.com/ims/token";
+
+        /// <summary>
+        /// Default value for <see cref="OAuthOptions.UserInformationEndpoint"/>.
+        /// </summary>
+        public const string UserInformationEndpoint = "https://ims-na1.adobelogin.com/ims/userinfo/v2";
+    }
+}
