00d2c9e7f04d689f6740244c73b5105e4654a585
Reking Shui <89603918+yongshui@users.noreply.github.com>
2021-08-31 11:59:04 -0500

    Add QuickBooks provider (#593)
    * Add the QuickBooks provider
    
    * Change the author of QuickBooks property
    
    * Change QuickBooks project property from TargetFramework to TargetFrameworks
    
    * Update AspNet.Security.OAuth.Providers.sln
    
    * Update AspNet.Security.OAuth.QuickBooks.csproj
    
    * Changed the QuickBooks authentication options.
    Add the test cases for all the mapped claims.
    Change the email to a fake email address.
    
    * Move the version from the csproj to pakcages.props.
    Clean the QuickBooksAuthenticationHandler.
    
    * Removed the unnecessary configuration and codes
    Changed the QuickBooks provider URL from test to production.
    Changed the scope implementation as recommended.

---
+++ src/AspNet.Security.OAuth.QuickBooks/QuickBooksAuthenticationConstants.cs
@@ -0,0 +1,20 @@
+﻿/*
+ * Licensed under the Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
+ * See https://github.com/aspnet-contrib/AspNet.Security.OAuth.Providers
+ * for more information concerning the license and the contributors participating to this project.
+ */
+
+namespace AspNet.Security.OAuth.QuickBooks
+{
+    /// <summary>
+    /// Contains constants specific to the <see cref="QuickBooksAuthenticationHandler"/>.
+    /// </summary>
+    public static class QuickBooksAuthenticationConstants
+    {
+        public static class Claims
+        {
+            public const string AccountType = "urn:quickbooks:appenvrionment";
+            public const string EmailVerified = "urn:quickbooks:email_verified";
+        }
+    }
+}
---
+++ test/AspNet.Security.OAuth.Providers.Tests/QuickBooks/QuickBooksTests.cs
@@ -0,0 +1,52 @@
+﻿/*
+ * Licensed under the Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
+ * See https://github.com/aspnet-contrib/AspNet.Security.OAuth.Providers
+ * for more information concerning the license and the contributors participating to this project.
+ */
+
+using System.Security.Claims;
+using System.Threading.Tasks;
+using AspNet.Security.OAuth.QuickBooks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+using Xunit.Abstractions;
+using static AspNet.Security.OAuth.QuickBooks.QuickBooksAuthenticationConstants;
+
+namespace AspNet.Security.OAuth.QuickBooksTests
+{
+    public class QuickBooksTests : OAuthTests<QuickBooksAuthenticationOptions>
+    {
+        public QuickBooksTests(ITestOutputHelper outputHelper)
+        {
+            OutputHelper = outputHelper;
+        }
+
+        public override string DefaultScheme => QuickBooksAuthenticationDefaults.AuthenticationScheme;
+
+        protected internal override void RegisterAuthentication(AuthenticationBuilder builder)
+        {
+            builder.AddQuickBooks(options =>
+            {
+                ConfigureDefaults(builder, options);
+            });
+        }
+
+        [Theory]
+        [InlineData(ClaimTypes.NameIdentifier, "2039290222")]
+        [InlineData(ClaimTypes.MobilePhone, "(314)000-0000")]
+        [InlineData(ClaimTypes.Email, "john.smith@test.local")]
+        [InlineData(Claims.EmailVerified, "true")]
+        public async Task Can_Sign_In_Using_QuickBooks(string claimType, string claimValue)
+        {
+            // Arrange
+            using var server = CreateTestServer();
+
+            // Act
+            var claims = await AuthenticateUserAsync(server);
+
+            // Assert
+            AssertClaim(claims, claimType, claimValue);
+        }
+    }
+}
