2bb98223de75766067447d0ddb76597acf6a62fe
Nicola Biancolini <nicola.biancolini@gmail.com>
2021-09-25 11:56:20 +0200

    Respect brand
    Co-authored-by: Martin Costello <martin@martincostello.com>

--- src/AspNet.Security.OAuth.Ebay/EbayAuthenticationExtensions.cs
+++ src/AspNet.Security.OAuth.Ebay/EbayAuthenticationExtensions.cs
@@ -29,7 +29,7 @@ namespace Microsoft.Extensions.DependencyInjection
 
         /// <summary>
         /// Adds <see cref="EbayAuthenticationHandler"/> to the specified
-        /// <see cref="AuthenticationBuilder"/>, which enables Ebay authentication capabilities.
+        /// <see cref="AuthenticationBuilder"/>, which enables eBay authentication capabilities.
         /// </summary>
         /// <param name="builder">The authentication builder.</param>
         /// <param name="configuration">The delegate used to configure the OpenID 2.0 options.</param>
