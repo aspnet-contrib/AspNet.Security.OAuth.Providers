adb138479e7af6b5a7a95321a41baccf514b3412
Nicola Biancolini <nicola.biancolini@gmail.com>
2021-09-25 11:52:11 +0200

    Prefer fields instead of consts
    Co-authored-by: Martin Costello <martin@martincostello.com>

--- src/AspNet.Security.OAuth.Ebay/EbayAuthenticationDefaults.cs
+++ src/AspNet.Security.OAuth.Ebay/EbayAuthenticationDefaults.cs
@@ -19,36 +19,36 @@ namespace AspNet.Security.OAuth.Ebay
         /// <summary>
         /// Default value for <see cref="AuthenticationScheme.DisplayName"/>.
         /// </summary>
-        public const string DisplayName = "Ebay";
+        public static readonly string DisplayName = "eBay";
 
         /// <summary>
         /// Default value for <see cref="AuthenticationSchemeOptions.ClaimsIssuer"/>.
         /// </summary>
-        public const string Issuer = "Ebay";
+        public static readonly string Issuer = "Ebay";
 
         /// <summary>
         /// Default value for <see cref="RemoteAuthenticationOptions.CallbackPath"/>.
         /// </summary>
-        public const string CallbackPath = "/signin-ebay";
+        public static readonly string CallbackPath = "/signin-ebay";
 
         /// <summary>
         /// Default value for <see cref="OAuthOptions.AuthorizationEndpoint"/>.
         /// </summary>
-        public const string AuthorizationEndpoint = "https://auth.ebay.com/oauth2/authorize";
+        public static readonly string AuthorizationEndpoint = "https://auth.ebay.com/oauth2/authorize";
 
         /// <summary>
         /// Default value for <see cref="OAuthOptions.TokenEndpoint"/>.
         /// </summary>
-        public const string TokenEndpoint = "https://api.ebay.com/identity/v1/oauth2/token";
+        public static readonly string TokenEndpoint = "https://api.ebay.com/identity/v1/oauth2/token";
 
         /// <summary>
         /// Default value for <see cref="OAuthOptions.UserInformationEndpoint"/>.
         /// </summary>
-        public const string UserInformationEndpoint = "https://apiz.ebay.com/commerce/identity/v1/user";
+        public static readonly string UserInformationEndpoint = "https://apiz.ebay.com/commerce/identity/v1/user";
 
         /// <summary>
         /// Default prefix for <see cref="OAuthOptions.Scope"/> value.
         /// </summary>
-        public const string ScopePrefix = "https://api.ebay.com/oauth/api_scope";
+        public static readonly string ScopePrefix = "https://api.ebay.com/oauth/api_scope";
     }
 }
