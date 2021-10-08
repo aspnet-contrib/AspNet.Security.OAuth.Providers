9eea47550f83294542c8322fd268d220d69d996b
Farid Ahamat <muhammadfarid.ahamat@valmont.com>
2021-10-08 17:03:59 +0800

    Fix Keycloak exception for Secret Key if using public access type
    Co-authored-by: FaridAhamat <farid.ahamat@gmail.com>

--- src/AspNet.Security.OAuth.Keycloak/KeycloakAuthenticationOptions.cs
+++ src/AspNet.Security.OAuth.Keycloak/KeycloakAuthenticationOptions.cs
@@ -38,6 +38,11 @@ namespace AspNet.Security.OAuth.Keycloak
         }
 
         /// <summary>
+        /// Gets or sets the value for Keycloak client's access type.
+        /// </summary>
+        public KeycloakAuthenticationAccessType AccessType { get; set; }
+
+        /// <summary>
         /// Gets or sets the base address of the Keycloak server.
         /// </summary>
         public Uri? BaseAddress { get; set; }
@@ -51,5 +56,42 @@ namespace AspNet.Security.OAuth.Keycloak
         /// Gets or sets the Keycloak realm to use for authentication.
         /// </summary>
         public string? Realm { get; set; }
+
+        /// <inheritdoc />
+        public override void Validate()
+        {
+            try
+            {
+                // HACK
+                // We want all of the base validation except for ClientSecret,
+                // so rather than re-implement it all, catch the exception thrown
+                // for that being null and only throw if we aren't using public access type.
+                // This does mean that three checks have to be re-implemented
+                // because the won't be validated if the ClientSecret validation fails.
+                base.Validate();
+            }
+            catch (ArgumentException ex) when (ex.ParamName == nameof(ClientSecret))
+            {
+                if (AccessType != KeycloakAuthenticationAccessType.Public)
+                {
+                    throw;
+                }
+            }
+
+            if (string.IsNullOrEmpty(AuthorizationEndpoint))
+            {
+                throw new ArgumentException($"The '{nameof(AuthorizationEndpoint)}' option must be provided.", nameof(AuthorizationEndpoint));
+            }
+
+            if (string.IsNullOrEmpty(TokenEndpoint))
+            {
+                throw new ArgumentException($"The '{nameof(TokenEndpoint)}' option must be provided.", nameof(TokenEndpoint));
+            }
+
+            if (!CallbackPath.HasValue)
+            {
+                throw new ArgumentException($"The '{nameof(CallbackPath)}' option must be provided.", nameof(CallbackPath));
+            }
+        }
     }
 }
