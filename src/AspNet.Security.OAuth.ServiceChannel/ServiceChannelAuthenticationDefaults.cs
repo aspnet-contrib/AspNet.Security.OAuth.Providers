d7eb9d1cddfad03dfa3b946e759417a61ac68df2
H.Yang <43012602+dev-hyang@users.noreply.github.com>
2021-10-26 11:40:49 -0500

    Add ServiceChannel provider (#616)
    * Add ServiceChannel provider

---
+++ src/AspNet.Security.OAuth.ServiceChannel/ServiceChannelAuthenticationDefaults.cs
@@ -0,0 +1,49 @@
+﻿/*
+ * Licensed under the Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
+ * See https://github.com/aspnet-contrib/AspNet.Security.OAuth.Providers
+ * for more information concerning the license and the contributors participating to this project.
+ */
+
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.OAuth;
+
+namespace AspNet.Security.OAuth.ServiceChannel
+{
+    public static class ServiceChannelAuthenticationDefaults
+    {
+        /// <summary>
+        /// Default value for <see cref="AuthenticationScheme.Name"/>.
+        /// </summary>
+        public const string AuthenticationScheme = "ServiceChannel";
+
+        /// <summary>
+        /// Default value for <see cref="AuthenticationScheme.DisplayName"/>.
+        /// </summary>
+        public static readonly string DisplayName = "ServiceChannel";
+
+        /// <summary>
+        /// Default value for <see cref="AuthenticationSchemeOptions.ClaimsIssuer"/>.
+        /// </summary>
+        public static readonly string Issuer = "ServiceChannel";
+
+        /// <summary>
+        /// Default value for <see cref="RemoteAuthenticationOptions.CallbackPath"/>.
+        /// </summary>
+        public static readonly string CallbackPath = "/signin-servicechannel";
+
+        /// <summary>
+        /// Default value for <see cref="OAuthOptions.AuthorizationEndpoint"/>.
+        /// </summary>
+        public static readonly string AuthorizationEndpoint = "https://login.servicechannel.com/oauth/authorize";
+
+        /// <summary>
+        /// Default value for <see cref="OAuthOptions.TokenEndpoint"/>.
+        /// </summary>
+        public static readonly string TokenEndpoint = "https://login.servicechannel.com/oauth/token";
+
+        /// <summary>
+        /// Default value for <see cref="OAuthOptions.UserInformationEndpoint"/>.
+        /// </summary>
+        public static readonly string UserInformationEndpoint = "https://api.servicechannel.com/v3/users/current/profile";
+    }
+}
---
+++ test/AspNet.Security.OAuth.Providers.Tests/ServiceChannel/ServiceChannelTests.cs
@@ -0,0 +1,53 @@
+﻿/*
+ * Licensed under the Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
+ * See https://github.com/aspnet-contrib/AspNet.Security.OAuth.Providers
+ * for more information concerning the license and the contributors participating to this project.
+ */
+
+using System.Security.Claims;
+using System.Threading.Tasks;
+using AspNet.Security.OAuth.ServiceChannel;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+using Xunit.Abstractions;
+using static AspNet.Security.OAuth.ServiceChannel.ServiceChannelAuthenticationConstants;
+
+namespace AspNet.Security.OAuth.ServiceChannel
+{
+    public class ServiceChannelTests : OAuthTests<ServiceChannelAuthenticationOptions>
+    {
+        public ServiceChannelTests(ITestOutputHelper outputHelper)
+        {
+            OutputHelper = outputHelper;
+        }
+
+        public override string DefaultScheme => ServiceChannelAuthenticationDefaults.AuthenticationScheme;
+
+        protected internal override void RegisterAuthentication(AuthenticationBuilder builder)
+        {
+            builder.AddServiceChannel(options =>
+            {
+                ConfigureDefaults(builder, options);
+            });
+        }
+
+        [Theory]
+        [InlineData(ClaimTypes.NameIdentifier, "4034383")]
+        [InlineData(ClaimTypes.Name, "john.smith@test.local")]
+        [InlineData(ClaimTypes.Email, "john.smith@test.local")]
+        [InlineData(Claims.ProviderId, "2000156703")]
+        [InlineData(Claims.ProviderName, "uicccf")]
+        public async Task Can_Sign_In_Using_ServiceChannel(string claimType, string claimValue)
+        {
+            // Arrange
+            using var server = CreateTestServer();
+
+            // Act
+            var claims = await AuthenticateUserAsync(server);
+
+            // Assert
+            AssertClaim(claims, claimType, claimValue);
+        }
+    }
+}
