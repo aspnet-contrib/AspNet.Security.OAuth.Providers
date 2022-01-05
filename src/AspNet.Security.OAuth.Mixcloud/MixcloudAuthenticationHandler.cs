b8540ddb6238907044d2e75ebd530c32ccf4a444
martincostello <martin@martincostello.com>
2022-01-05 18:22:56 +0000

    Refactor Mixcloud state handling
    Rather than copy-paste all of HandleRemoteAuthenticateAsync() and change one line of code in the middle, instead intercept the call to ExchangeCodeAsync() and add on the state there.

--- src/AspNet.Security.OAuth.Mixcloud/MixcloudAuthenticationHandler.cs
+++ src/AspNet.Security.OAuth.Mixcloud/MixcloudAuthenticationHandler.cs
@@ -4,7 +4,6 @@
  * for more information concerning the license and the contributors participating to this project.
  */
 
-using System.Globalization;
 using System.Net.Http.Headers;
 using System.Security.Claims;
 using System.Security.Cryptography;
@@ -14,7 +13,6 @@ using System.Text.Json;
 using Microsoft.AspNetCore.WebUtilities;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
-using Microsoft.Extensions.Primitives;
 using Base64UrlTextEncoder = Microsoft.AspNetCore.Authentication.Base64UrlTextEncoder;
 
 namespace AspNet.Security.OAuth.Mixcloud;
@@ -66,137 +64,21 @@ public partial class MixcloudAuthenticationHandler : OAuthHandler<MixcloudAuthen
         return QueryHelpers.AddQueryString(Options.AuthorizationEndpoint, parameters);
     }
 
+    protected override async Task<OAuthTokenResponse> ExchangeCodeAsync([NotNull] OAuthCodeExchangeContext context)
+    {
+        // Mixcloud does not appear to support the `state` parameter, so instead add it
+        // to the redirect URI before calling the base ExchangeCodeAsync() implementation.
+        var state = Request.Query["state"];
+
+        var redirectUri = QueryHelpers.AddQueryString(context.RedirectUri, "state", state);
+        context = new OAuthCodeExchangeContext(context.Properties, context.Code, redirectUri);
+
+        return await base.ExchangeCodeAsync(context);
+    }
+
     protected override async Task<HandleRequestResult> HandleRemoteAuthenticateAsync()
     {
-        var query = Request.Query;
-
-        var state = query["state"];
-        var properties = Options.StateDataFormat.Unprotect(state);
-
-        if (properties == null)
-        {
-            return HandleRequestResult.Fail("The oauth state was missing or invalid.");
-        }
-
-        // OAuth2 10.12 CSRF
-        if (!ValidateCorrelationId(properties))
-        {
-            return HandleRequestResult.Fail("Correlation failed.", properties);
-        }
-
-        var error = query["error"];
-        if (!StringValues.IsNullOrEmpty(error))
-        {
-            // Note: access_denied errors are special protocol errors indicating the user didn't
-            // approve the authorization demand requested by the remote authorization server.
-            // Since it's a frequent scenario (that is not caused by incorrect configuration),
-            // denied errors are handled differently using HandleAccessDeniedErrorAsync().
-            // Visit https://tools.ietf.org/html/rfc6749#section-4.1.2.1 for more information.
-            var errorDescription = query["error_description"];
-            var errorUri = query["error_uri"];
-            if (StringValues.Equals(error, "access_denied"))
-            {
-                var result = await HandleAccessDeniedErrorAsync(properties);
-                if (!result.None)
-                {
-                    return result;
-                }
-
-                var deniedEx = new Exception("Access was denied by the resource owner or by the remote server.");
-                deniedEx.Data["error"] = error.ToString();
-                deniedEx.Data["error_description"] = errorDescription.ToString();
-                deniedEx.Data["error_uri"] = errorUri.ToString();
-
-                return HandleRequestResult.Fail(deniedEx, properties);
-            }
-
-            var failureMessage = new StringBuilder();
-            failureMessage.Append(error);
-            if (!StringValues.IsNullOrEmpty(errorDescription))
-            {
-                failureMessage.Append(";Description=").Append(errorDescription);
-            }
-
-            if (!StringValues.IsNullOrEmpty(errorUri))
-            {
-                failureMessage.Append(";Uri=").Append(errorUri);
-            }
-
-            var ex = new Exception(failureMessage.ToString());
-            ex.Data["error"] = error.ToString();
-            ex.Data["error_description"] = errorDescription.ToString();
-            ex.Data["error_uri"] = errorUri.ToString();
-
-            return HandleRequestResult.Fail(ex, properties);
-        }
-
-        var code = query["code"];
-
-        if (StringValues.IsNullOrEmpty(code))
-        {
-            return HandleRequestResult.Fail("Code was not found.", properties);
-        }
-
-        // Mixcloud does not appear to support the `state` parameter, so have to unbundle it here:
-        var redirectUri = QueryHelpers.AddQueryString(BuildRedirectUri(Options.CallbackPath), "state", state);
-        var codeExchangeContext = new OAuthCodeExchangeContext(properties, code, redirectUri);
-        using var tokens = await ExchangeCodeAsync(codeExchangeContext);
-
-        if (tokens.Error != null)
-        {
-            return HandleRequestResult.Fail(tokens.Error, properties);
-        }
-
-        if (string.IsNullOrEmpty(tokens.AccessToken))
-        {
-            return HandleRequestResult.Fail("Failed to retrieve access token.", properties);
-        }
-
-        var identity = new ClaimsIdentity(ClaimsIssuer);
-
-        if (Options.SaveTokens)
-        {
-            var authTokens = new List<AuthenticationToken>();
-
-            authTokens.Add(new AuthenticationToken { Name = "access_token", Value = tokens.AccessToken });
-            if (!string.IsNullOrEmpty(tokens.RefreshToken))
-            {
-                authTokens.Add(new AuthenticationToken { Name = "refresh_token", Value = tokens.RefreshToken });
-            }
-
-            if (!string.IsNullOrEmpty(tokens.TokenType))
-            {
-                authTokens.Add(new AuthenticationToken { Name = "token_type", Value = tokens.TokenType });
-            }
-
-            if (!string.IsNullOrEmpty(tokens.ExpiresIn))
-            {
-                int value;
-                if (int.TryParse(tokens.ExpiresIn, NumberStyles.Integer, CultureInfo.InvariantCulture, out value))
-                {
-                    // https://www.w3.org/TR/xmlschema-2/#dateTime
-                    // https://msdn.microsoft.com/en-us/library/az4se3k1(v=vs.110).aspx
-                    var expiresAt = Clock.UtcNow + TimeSpan.FromSeconds(value);
-                    authTokens.Add(new AuthenticationToken
-                    {
-                        Name = "expires_at",
-                        Value = expiresAt.ToString("o", CultureInfo.InvariantCulture)
-                    });
-                }
-            }
-
-            properties.StoreTokens(authTokens);
-        }
-
-        var ticket = await CreateTicketAsync(identity, properties, tokens);
-        if (ticket != null)
-        {
-            return HandleRequestResult.Success(ticket);
-        }
-        else
-        {
-            return HandleRequestResult.Fail("Failed to retrieve user information from remote server.", properties);
-        }
+        return await base.HandleRemoteAuthenticateAsync();
     }
 
     protected override async Task<AuthenticationTicket> CreateTicketAsync(
