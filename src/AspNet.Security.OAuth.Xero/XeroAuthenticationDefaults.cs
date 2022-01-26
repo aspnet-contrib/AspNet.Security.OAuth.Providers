21bb8956a2394f2cd5a73cafd1015de9f47b91f8
Albert Zakiev <albert.zakiev@gmail.com>
2022-01-26 21:32:24 +0300

    PR suggestions

--- src/AspNet.Security.OAuth.Xero/XeroAuthenticationDefaults.cs
+++ src/AspNet.Security.OAuth.Xero/XeroAuthenticationDefaults.cs
@@ -19,30 +19,30 @@ public static class XeroAuthenticationDefaults
     /// <summary>
     /// Default value for <see cref="RemoteAuthenticationOptions.CallbackPath"/>.
     /// </summary>
-    public const string CallbackPath = "/signin-xero";
+    public static readonly string CallbackPath = "/signin-xero";
 
     /// <summary>
     /// Default value for <see cref="AuthenticationScheme.DisplayName"/>.
     /// </summary>
-    public const string DisplayName = "Xero";
+    public static readonly string DisplayName = "Xero";
 
     /// <summary>
     /// A format string used to construct <see cref="AuthenticationSchemeOptions.ClaimsIssuer"/>.
     /// </summary>
-    public const string ClaimsIssuer = "https://identity.xero.com";
+    public static readonly string ClaimsIssuer = "https://identity.xero.com";
 
     /// <summary>
     /// A format string used to populate OAuth authorize endpoint.
     /// </summary>
-    public const string AuthorizeEndpoint = "https://login.xero.com/identity/connect/authorize";
+    public static readonly string AuthorizeEndpoint = "https://login.xero.com/identity/connect/authorize";
 
     /// <summary>
     /// A format string used to populate OAuth token endpoint.
     /// </summary>
-    public const string TokenEndpoint = "https://identity.xero.com/connect/token";
+    public static readonly string TokenEndpoint = "https://identity.xero.com/connect/token";
 
     /// <summary>
     /// A format string used to construct well-known configuration endpoint.
     /// </summary>
-    public const string MetadataEndpoint = "https://identity.xero.com/.well-known/openid-configuration";
+    public static readonly string MetadataEndpoint = "https://identity.xero.com/.well-known/openid-configuration";
 }
