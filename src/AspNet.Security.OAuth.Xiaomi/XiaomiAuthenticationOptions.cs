d10cc2e096d540ff74f92b63e7f58a3e178a0594
ArcherTrister <archertrister@outlook.com>
2022-05-06 23:54:34 +0800

    Add Xiaomi provider

---
+++ src/AspNet.Security.OAuth.Xiaomi/XiaomiAuthenticationOptions.cs
@@ -0,0 +1,31 @@
+﻿/*
+ * Licensed under the Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
+ * See https://github.com/aspnet-contrib/AspNet.Security.OAuth.Providers
+ * for more information concerning the license and the contributors participating to this project.
+ */
+
+using System.Security.Claims;
+using static AspNet.Security.OAuth.Xiaomi.XiaomiAuthenticationConstants;
+
+namespace AspNet.Security.OAuth.Xiaomi;
+
+/// <summary>
+/// Defines a set of options used by <see cref="XiaomiAuthenticationHandler"/>.
+/// </summary>
+public class XiaomiAuthenticationOptions : OAuthOptions
+{
+    public XiaomiAuthenticationOptions()
+    {
+        ClaimsIssuer = XiaomiAuthenticationDefaults.Issuer;
+        CallbackPath = XiaomiAuthenticationDefaults.CallbackPath;
+
+        AuthorizationEndpoint = XiaomiAuthenticationDefaults.AuthorizationEndpoint;
+        TokenEndpoint = XiaomiAuthenticationDefaults.TokenEndpoint;
+        UserInformationEndpoint = XiaomiAuthenticationDefaults.UserInformationEndpoint;
+
+        ClaimActions.MapJsonKey(ClaimTypes.Name, "miliaoNick");
+        ClaimActions.MapJsonKey(Claims.MiliaoNick, "miliaoNick");
+        ClaimActions.MapJsonKey(Claims.UnionId, "unionId");
+        ClaimActions.MapJsonKey(Claims.MiliaoIcon, "miliaoIcon");
+    }
+}
---
+++ test/AspNet.Security.OAuth.Providers.Tests/Xiaomi/XiaomiTests.cs
@@ -0,0 +1,40 @@
+﻿/*
+ * Licensed under the Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
+ * See https://github.com/aspnet-contrib/AspNet.Security.OAuth.Providers
+ * for more information concerning the license and the contributors participating to this project.
+ */
+
+namespace AspNet.Security.OAuth.Xiaomi;
+
+public class XiaomiTests : OAuthTests<XiaomiAuthenticationOptions>
+{
+    public XiaomiTests(ITestOutputHelper outputHelper)
+    {
+        OutputHelper = outputHelper;
+    }
+
+    public override string DefaultScheme => XiaomiAuthenticationDefaults.AuthenticationScheme;
+
+    protected internal override void RegisterAuthentication(AuthenticationBuilder builder)
+    {
+        builder.AddXiaomi(options => ConfigureDefaults(builder, options));
+    }
+
+    [Theory]
+    [InlineData(ClaimTypes.NameIdentifier, "my-union-id")]
+    [InlineData(ClaimTypes.Name, "John Smith")]
+    [InlineData("urn:xiaomi:miliaoIcon", "https://xiaomi.local/image.png")]
+    [InlineData("urn:xiaomi:unionId", "my-union-id")]
+    [InlineData("urn:xiaomi:miliaoNick", "John Smith")]
+    public async Task Can_Sign_In_Using_Xiaomi(string claimType, string claimValue)
+    {
+        // Arrange
+        using var server = CreateTestServer();
+
+        // Act
+        var claims = await AuthenticateUserAsync(server);
+
+        // Assert
+        AssertClaim(claims, claimType, claimValue);
+    }
+}
