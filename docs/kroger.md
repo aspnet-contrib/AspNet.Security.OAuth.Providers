dacae7b7168fea7a1cefe77e935aae67a8fd8d12
lukemoore495 <lukemoore495@gmail.com>
2022-07-02 12:35:30 -0400

    Add Kroger provider

---
+++ docs/kroger.md
@@ -0,0 +1,20 @@
+# Integrating the Kroger Provider
+
+## Example
+
+```csharp
+services.AddAuthentication(options => /* Auth configuration */)
+        .AddKroger(options =>
+        {
+            options.ClientId = "my-client-id";
+            options.ClientSecret = "my-client-secret";
+        });
+```
+
+## Required Additional Settings
+
+_None._
+
+## Optional Settings
+
+_None._
---
+++ src/AspNet.Security.OAuth.Kroger/KrogerAuthenticationOptions.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * Licensed under the Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
+ * See https://github.com/aspnet-contrib/AspNet.Security.OAuth.Providers
+ * for more information concerning the license and the contributors participating to this project.
+ */
+
+using System.Security.Claims;
+
+namespace AspNet.Security.OAuth.Kroger;
+
+/// <summary>
+/// Defines a set of options used by <see cref="KrogerAuthenticationHandler"/>.
+/// </summary>
+public class KrogerAuthenticationOptions : OAuthOptions
+{
+    public KrogerAuthenticationOptions()
+    {
+        ClaimsIssuer = KrogerAuthenticationDefaults.Issuer;
+
+        CallbackPath = KrogerAuthenticationDefaults.CallbackPath;
+
+        AuthorizationEndpoint = KrogerAuthenticationDefaults.AuthorizationEndpoint;
+        TokenEndpoint = KrogerAuthenticationDefaults.TokenEndpoint;
+        UserInformationEndpoint = KrogerAuthenticationDefaults.UserInformationEndpoint;
+
+        Scope.Add("profile.compact");
+        Scope.Add("cart.basic:write");
+        Scope.Add("product.compact");
+
+        ClaimActions.MapJsonKey(ClaimTypes.NameIdentifier, "id");
+    }
+}
