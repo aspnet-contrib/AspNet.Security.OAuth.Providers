5c150f2d6ef35700805d9ac4e76864ec1b8e7a0c
Shayaan Ahmed Farooqi <fleshlime.1@gmail.com>
2022-08-16 13:51:11 +0500

    Add HubSpot Provider (#704)
    * - Implemented HubSpot OAuth.
    
    * Update readme and package validation baseline
    
    * Update UserInformationEndpoint to UserInformationEndpointFormat
    
    * Save user info in NameIdentifier instead of Name.
    
    * UrlEncode the accessToken before formatting it.
    
    * Move coment out.
    
    * Update base line version and add metadata
    
    * readability
    
    * - Add basic test for claim compare
    - remove whitespace from url.
    
    * reformat
    
    * Apply suggestions from code review
    
    Use literals in tests for HubSpot.
    
    Co-authored-by: Martin Costello <martin@martincostello.com>

---
+++ test/AspNet.Security.OAuth.Providers.Tests/HubSpot/HubSpotTests.cs
@@ -0,0 +1,41 @@
+﻿/*
+ * Licensed under the Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
+ * See https://github.com/aspnet-contrib/AspNet.Security.OAuth.Providers
+ * for more information concerning the license and the contributors participating to this project.
+ */
+
+namespace AspNet.Security.OAuth.HubSpot;
+
+public class HubSpotTests : OAuthTests<HubSpotAuthenticationOptions>
+{
+    public HubSpotTests(ITestOutputHelper outputHelper)
+    {
+        OutputHelper = outputHelper;
+    }
+
+    public override string DefaultScheme => HubSpotAuthenticationDefaults.AuthenticationScheme;
+
+    protected internal override void RegisterAuthentication(AuthenticationBuilder builder)
+    {
+        builder.AddHubSpot(options => ConfigureDefaults(builder, options));
+    }
+
+    [Theory]
+    [InlineData(ClaimTypes.NameIdentifier, "dummy@dummymail.com")]
+    [InlineData(ClaimTypes.Email, "dummy@dummymail.com")]
+    [InlineData("urn:HubSpot:hub_id", "13371337")]
+    [InlineData("urn:HubSpot:user_id", "123123")]
+    [InlineData("urn:HubSpot:app_id", "696969")]
+    [InlineData("urn:HubSpot:hub_domain", "dev-13371337.com")]
+    public async Task Can_Sign_In_Using_HubSpot(string claimType, string claimValue)
+    {
+        // Arrange
+        using var server = CreateTestServer();
+
+        // Act
+        var claims = await AuthenticateUserAsync(server);
+
+        // Assert
+        AssertClaim(claims, claimType, claimValue);
+    }
+}
