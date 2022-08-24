f1e2f76c5425dcd69467eb7da523c616ffe837d9
vicenteyu <yxr@live.cn>
2022-08-24 15:23:47 +0800

    Add Feishu provider (#709)
    * Add Feishu provider
    
    Signed-off-by: Vicente Yu <yxr@live.cn>
    
    * Add Feishu provider.
    
    Signed-off-by: Vicente Yu <yxr@live.cn>

---
+++ src/AspNet.Security.OAuth.Feishu/FeishuAuthenticationDefaults.cs
@@ -0,0 +1,48 @@
+﻿/*
+ * Licensed under the Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
+ * See https://github.com/aspnet-contrib/AspNet.Security.OAuth.Providers
+ * for more information concerning the license and the contributors participating to this project.
+ */
+
+namespace AspNet.Security.OAuth.Feishu;
+
+/// <summary>
+/// Default values for Feishu authentication.
+/// </summary>
+public static class FeishuAuthenticationDefaults
+{
+    /// <summary>
+    /// Default value for <see cref="AuthenticationScheme.Name"/>.
+    /// </summary>
+    public const string AuthenticationScheme = "Feishu";
+
+    /// <summary>
+    /// Default value for <see cref="AuthenticationScheme.DisplayName"/>.
+    /// </summary>
+    public static readonly string DisplayName = "Feishu";
+
+    /// <summary>
+    /// Default value for <see cref="RemoteAuthenticationOptions.CallbackPath"/>.
+    /// </summary>
+    public static readonly string CallbackPath = "/signin-feishu";
+
+    /// <summary>
+    /// Default value for <see cref="AuthenticationSchemeOptions.ClaimsIssuer"/>.
+    /// </summary>
+    public static readonly string Issuer = "Feishu";
+
+    /// <summary>
+    /// Default value for <see cref="OAuthOptions.AuthorizationEndpoint"/>.
+    /// </summary>
+    public static readonly string AuthorizationEndpoint = "https://passport.feishu.cn/suite/passport/oauth/authorize";
+
+    /// <summary>
+    /// Default value for <see cref="OAuthOptions.TokenEndpoint"/>.
+    /// </summary>
+    public static readonly string TokenEndpoint = "https://passport.feishu.cn/suite/passport/oauth/token";
+
+    /// <summary>
+    /// Default value for <see cref="OAuthOptions.UserInformationEndpoint"/>.
+    /// </summary>
+    public static readonly string UserInformationEndpoint = "https://passport.feishu.cn/suite/passport/oauth/userinfo";
+}
