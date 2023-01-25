95c17c2f8adf1b9f99ac3a77c95b4cd50f2e3137
Andrew Killion <andrew_killion@steris.com>
2023-01-25 18:25:11 -0500

    adding test with custom domain

--- test/AspNet.Security.OAuth.Providers.Tests/PingOne/PingOnePostConfigureOptionsTests.cs
+++ test/AspNet.Security.OAuth.Providers.Tests/PingOne/PingOnePostConfigureOptionsTests.cs
@@ -34,6 +34,33 @@ public static class PingOnePostConfigureOptionsTests
         Uri.TryCreate(options.UserInformationEndpoint, UriKind.Absolute, out _).ShouldBeTrue();
     }
 
+    [Fact]
+    public static void PostConfigure_Configures_Valid_Endpoints_With_Custom_Domain()
+    {
+        // Arrange
+        string name = "PingOne";
+        var target = new PingOnePostConfigureOptions();
+
+        var options = new PingOneAuthenticationOptions()
+        {
+            EnvironmentId = "b775aadd-a2f3-4d88-a768-b7c85dd1af47",
+            Domain = "auth.pingone.local"
+        };
+
+        // Act
+        target.PostConfigure(name, options);
+
+        // Assert
+        options.AuthorizationEndpoint.ShouldStartWith("https://auth.pingone.local/b775aadd-a2f3-4d88-a768-b7c85dd1af47/");
+        Uri.TryCreate(options.AuthorizationEndpoint, UriKind.Absolute, out _).ShouldBeTrue();
+
+        options.TokenEndpoint.ShouldStartWith("https://auth.pingone.local/b775aadd-a2f3-4d88-a768-b7c85dd1af47/");
+        Uri.TryCreate(options.TokenEndpoint, UriKind.Absolute, out _).ShouldBeTrue();
+
+        options.UserInformationEndpoint.ShouldStartWith("https://auth.pingone.local/b775aadd-a2f3-4d88-a768-b7c85dd1af47/");
+        Uri.TryCreate(options.UserInformationEndpoint, UriKind.Absolute, out _).ShouldBeTrue();
+    }
+
     [Theory]
     [InlineData(null)]
     [InlineData("")]
