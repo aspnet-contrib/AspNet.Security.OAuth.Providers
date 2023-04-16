29137f71f01fe58ea96dc7e4672c5e61cb67de7d
Ge <gehongyan1996@126.com>
2023-04-16 23:43:02 +0800

    Add KOOK provider (#754)
    Add KOOK provider.

---
+++ src/AspNet.Security.OAuth.Kook/KookAuthenticationDefaults.cs
@@ -0,0 +1,48 @@
+﻿/*
+ * Licensed under the Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
+ * See https://github.com/aspnet-contrib/AspNet.Security.OAuth.Providers
+ * for more information concerning the license and the contributors participating to this project.
+ */
+
+namespace AspNet.Security.OAuth.Kook;
+
+/// <summary>
+/// Default values used by the Kook authentication middleware.
+/// </summary>
+public static class KookAuthenticationDefaults
+{
+    /// <summary>
+    /// Default value for <see cref="Microsoft.AspNetCore.Authentication.AuthenticationScheme.Name"/>.
+    /// </summary>
+    public const string AuthenticationScheme = "Kook";
+
+    /// <summary>
+    /// Default value for <see cref="Microsoft.AspNetCore.Authentication.AuthenticationScheme.DisplayName"/>.
+    /// </summary>
+    public static readonly string DisplayName = "Kook";
+
+    /// <summary>
+    /// Default value for <see cref="AuthenticationSchemeOptions.ClaimsIssuer"/>.
+    /// </summary>
+    public static readonly string Issuer = "Kook";
+
+    /// <summary>
+    /// Default value for <see cref="RemoteAuthenticationOptions.CallbackPath"/>.
+    /// </summary>
+    public static readonly string CallbackPath = "/signin-kook";
+
+    /// <summary>
+    /// Default value for <see cref="OAuthOptions.AuthorizationEndpoint"/>.
+    /// </summary>
+    public static readonly string AuthorizationEndpoint = "https://www.kookapp.cn/app/oauth2/authorize";
+
+    /// <summary>
+    /// Default value for <see cref="OAuthOptions.TokenEndpoint"/>.
+    /// </summary>
+    public static readonly string TokenEndpoint = "https://www.kookapp.cn/api/oauth2/token";
+
+    /// <summary>
+    /// Default value for <see cref="OAuthOptions.UserInformationEndpoint"/>.
+    /// </summary>
+    public static readonly string UserInformationEndpoint = "https://www.kookapp.cn/api/v3/user/me";
+}
---
+++ src/AspNet.Security.OAuth.Kook/KookAuthenticationOptions.cs
@@ -0,0 +1,35 @@
+﻿/*
+ * Licensed under the Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
+ * See https://github.com/aspnet-contrib/AspNet.Security.OAuth.Providers
+ * for more information concerning the license and the contributors participating to this project.
+ */
+
+using System.Security.Claims;
+
+namespace AspNet.Security.OAuth.Kook;
+
+/// <summary>
+/// Defines a set of options used by <see cref="KookAuthenticationHandler"/>.
+/// </summary>
+public class KookAuthenticationOptions : OAuthOptions
+{
+    public KookAuthenticationOptions()
+    {
+        ClaimsIssuer = KookAuthenticationDefaults.Issuer;
+        CallbackPath = KookAuthenticationDefaults.CallbackPath;
+        AuthorizationEndpoint = KookAuthenticationDefaults.AuthorizationEndpoint;
+        TokenEndpoint = KookAuthenticationDefaults.TokenEndpoint;
+        UserInformationEndpoint = KookAuthenticationDefaults.UserInformationEndpoint;
+
+        ClaimActions.MapJsonKey(ClaimTypes.NameIdentifier, "id");
+        ClaimActions.MapJsonKey(ClaimTypes.Name, "username");
+        ClaimActions.MapJsonKey(ClaimTypes.MobilePhone, "mobile");
+        ClaimActions.MapJsonKey(KookAuthenticationConstants.Claims.IdentifyNumber, "identify_num");
+        ClaimActions.MapJsonKey(KookAuthenticationConstants.Claims.OperatingSystem, "os");
+        ClaimActions.MapJsonKey(KookAuthenticationConstants.Claims.AvatarUrl, "avatar");
+        ClaimActions.MapJsonKey(KookAuthenticationConstants.Claims.BannerUrl, "banner");
+        ClaimActions.MapJsonKey(KookAuthenticationConstants.Claims.IsMobileVerified, "mobile_verified");
+
+        Scope.Add("get_user_info");
+    }
+}
