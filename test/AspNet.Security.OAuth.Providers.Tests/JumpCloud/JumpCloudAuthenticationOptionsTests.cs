e31910337de24a6c3e45cf6072f962af1d717246
Aaron <aaron@umbhost.net>
2023-08-15 12:08:02 +0100

    Added unit tests

---
+++ test/AspNet.Security.OAuth.Providers.Tests/JumpCloud/JumpCloudAuthenticationOptionsTests.cs
@@ -0,0 +1,77 @@
+﻿/*
+ * Licensed under the Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
+ * See https://github.com/aspnet-contrib/AspNet.Security.OAuth.Providers
+ * for more information concerning the license and the contributors participating to this project.
+ */
+
+using AspNet.Security.OAuth.Okta;
+
+namespace AspNet.Security.OAuth.JumpCloud;
+
+public static class JumpCloudAuthenticationOptionsTests
+{
+    [Fact]
+    public static void Validate_Throws_If_AuthorizationEndpoint_Not_Set()
+    {
+        // Arrange
+        var options = new OktaAuthenticationOptions()
+        {
+            ClientId = "ClientId",
+            ClientSecret = "ClientSecret",
+            TokenEndpoint = "https://jumpcloud.local",
+            UserInformationEndpoint = "https://jumpcloud.local",
+        };
+
+        // Act and Assert
+        Assert.Throws<ArgumentException>("AuthorizationEndpoint", () => options.Validate());
+    }
+
+    [Fact]
+    public static void Validate_Throws_If_TokenEndpoint_Not_Set()
+    {
+        // Arrange
+        var options = new OktaAuthenticationOptions()
+        {
+            AuthorizationEndpoint = "https://jumpcloud.local",
+            ClientId = "ClientId",
+            ClientSecret = "ClientSecret",
+            UserInformationEndpoint = "https://jumpcloud.local",
+        };
+
+        // Act and Assert
+        Assert.Throws<ArgumentException>("TokenEndpoint", () => options.Validate());
+    }
+
+    [Fact]
+    public static void Validate_Throws_If_UserInformationEndpoint_Not_Set()
+    {
+        // Arrange
+        var options = new OktaAuthenticationOptions()
+        {
+            AuthorizationEndpoint = "https://okta.local",
+            ClientId = "ClientId",
+            ClientSecret = "ClientSecret",
+            TokenEndpoint = "https://okta.local",
+        };
+
+        // Act and Assert
+        Assert.Throws<ArgumentException>("UserInformationEndpoint", () => options.Validate());
+    }
+
+    [Fact]
+    public static void Validate_Does_Not_Throw_If_Uris_Are_Valid()
+    {
+        // Arrange
+        var options = new OktaAuthenticationOptions()
+        {
+            AuthorizationEndpoint = "https://okta.local",
+            ClientId = "ClientId",
+            ClientSecret = "ClientSecret",
+            TokenEndpoint = "https://okta.local",
+            UserInformationEndpoint = "https://okta.local",
+        };
+
+        // Act (no Assert)
+        options.Validate();
+    }
+}
---
+++ test/AspNet.Security.OAuth.Providers.Tests/JumpCloud/JumpCloudPostConfigureOptionsTests.cs
@@ -0,0 +1,60 @@
+﻿/*
+ * Licensed under the Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
+ * See https://github.com/aspnet-contrib/AspNet.Security.OAuth.Providers
+ * for more information concerning the license and the contributors participating to this project.
+ */
+
+namespace AspNet.Security.OAuth.JumpCloud;
+
+public static class JumpCloudPostConfigureOptionsTests
+{
+    [Theory]
+    [InlineData("jumpcloud.local")]
+    [InlineData("http://jumpcloud.local")]
+    [InlineData("http://jumpcloud.local/")]
+    [InlineData("https://jumpcloud.local")]
+    [InlineData("https://jumpcloud.local/")]
+    public static void PostConfigure_Configures_Valid_Endpoints(string domain)
+    {
+        // Arrange
+        string name = "JumpCloud";
+        var target = new JumpCloudPostConfigureOptions();
+
+        var options = new JumpCloudAuthenticationOptions()
+        {
+            Domain = domain,
+        };
+
+        // Act
+        target.PostConfigure(name, options);
+
+        // Assert
+        options.AuthorizationEndpoint.ShouldStartWith("https://jumpcloud.local/oauth2/auth");
+        Uri.TryCreate(options.AuthorizationEndpoint, UriKind.Absolute, out _).ShouldBeTrue();
+
+        options.TokenEndpoint.ShouldStartWith("https://jumpcloud.local/oauth2/token");
+        Uri.TryCreate(options.TokenEndpoint, UriKind.Absolute, out _).ShouldBeTrue();
+
+        options.UserInformationEndpoint.ShouldStartWith("https://jumpcloud.local/userinfo");
+        Uri.TryCreate(options.UserInformationEndpoint, UriKind.Absolute, out _).ShouldBeTrue();
+    }
+
+    [Theory]
+    [InlineData(null)]
+    [InlineData("")]
+    [InlineData(" ")]
+    public static void PostConfigure_Throws_If_Domain_Is_Invalid(string value)
+    {
+        // Arrange
+        string name = "JumpCloud";
+        var target = new JumpCloudPostConfigureOptions();
+
+        var options = new JumpCloudAuthenticationOptions()
+        {
+            Domain = value,
+        };
+
+        // Act and Assert
+        Assert.Throws<ArgumentException>("options", () => target.PostConfigure(name, options));
+    }
+}
