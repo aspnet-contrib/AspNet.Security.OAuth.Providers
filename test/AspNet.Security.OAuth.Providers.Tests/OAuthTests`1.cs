5a3a7762e8eb38f0ddc46be84195e048bf2c2829
softwareolatomiwa <softwareolatomiwa@gmail.com>
2024-03-01 21:49:15 +0100

    Added OpenID support. Updated Userdetails  and scope according to this linkhttps://www.linkedin.com/developers/news/featured-updates/openid-connect-authentication

--- test/AspNet.Security.OAuth.Providers.Tests/OAuthTests`1.cs
+++ test/AspNet.Security.OAuth.Providers.Tests/OAuthTests`1.cs
@@ -253,7 +253,7 @@ public abstract class OAuthTests<TOptions> : ITestOutputHelperAccessor
         using (var client = server.CreateDefaultClient(LoopbackRedirectHandler))
         {
             // Act
-            using var result = await client.GetAsync("/me");
+            using var result = await client.GetAsync("/userinfo");
 
             // Assert
             result.StatusCode.ShouldBe(HttpStatusCode.Found);
@@ -269,7 +269,7 @@ public abstract class OAuthTests<TOptions> : ITestOutputHelperAccessor
             client.DefaultRequestHeaders.Add("Cookie", cookies);
 
             // Act
-            using var result = await client.GetAsync("/me");
+            using var result = await client.GetAsync("/userinfo");
 
             // Assert
             result.StatusCode.ShouldBe(HttpStatusCode.OK);
