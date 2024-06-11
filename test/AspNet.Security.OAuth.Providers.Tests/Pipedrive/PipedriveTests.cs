a55269a2896df6f6200d237f3f244cb881869673
denis-goncharenko <52198869+denis-goncharenko@users.noreply.github.com>
2024-06-11 19:01:23 +0300

    Implement Pipedrive OAuth provider

---
+++ test/AspNet.Security.OAuth.Providers.Tests/Pipedrive/PipedriveTests.cs
@@ -0,0 +1,24 @@
+﻿/*
+ * Licensed under the Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
+ * See https://github.com/aspnet-contrib/AspNet.Security.OAuth.Providers
+ * for more information concerning the license and the contributors participating to this project.
+ */
+
+namespace AspNet.Security.OAuth.Pipedrive;
+
+public class PipedriveTests(ITestOutputHelper outputHelper) : OAuthTests<PipedriveAuthenticationOptions>(outputHelper)
+{
+    public override string DefaultScheme => PipedriveAuthenticationDefaults.AuthenticationScheme;
+
+    protected internal override void RegisterAuthentication(AuthenticationBuilder builder)
+    {
+        builder.AddPipedrive(options => ConfigureDefaults(builder, options));
+    }
+
+    [Theory]
+    [InlineData(ClaimTypes.NameIdentifier, "12345")]
+    [InlineData(ClaimTypes.Name, "Test User")]
+    [InlineData(ClaimTypes.Email, "testuser@example.com")]
+    public async Task Can_Sign_In_Using_Pipedrive(string claimType, string claimValue)
+        => await AuthenticateUserAndAssertClaimValue(claimType, claimValue);
+}
