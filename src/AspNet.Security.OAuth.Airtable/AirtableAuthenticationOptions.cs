b6c69ecf49d11029159659da36238f0a26a00d11
Denys Goncharenko <52198869+denis-goncharenko@users.noreply.github.com>
2024-06-14 15:38:13 +0300

    Implement Airtable OAuth provider (#895)
    Implement Airtable OAuth provider.

---
+++ src/AspNet.Security.OAuth.Airtable/AirtableAuthenticationOptions.cs
@@ -0,0 +1,30 @@
+﻿/*
+ * Licensed under the Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
+ * See https://github.com/aspnet-contrib/AspNet.Security.OAuth.Providers
+ * for more information concerning the license and the contributors participating to this project.
+ */
+
+using System.Security.Claims;
+
+namespace AspNet.Security.OAuth.Airtable;
+
+/// <summary>
+/// Defines a set of options used by <see cref="AirtableAuthenticationHandler"/>.
+/// </summary>
+public class AirtableAuthenticationOptions : OAuthOptions
+{
+    public AirtableAuthenticationOptions()
+    {
+        ClaimsIssuer = AirtableAuthenticationDefaults.Issuer;
+        CallbackPath = AirtableAuthenticationDefaults.CallbackPath;
+
+        AuthorizationEndpoint = AirtableAuthenticationDefaults.AuthorizationEndpoint;
+        TokenEndpoint = AirtableAuthenticationDefaults.TokenEndpoint;
+        UserInformationEndpoint = AirtableAuthenticationDefaults.UserInformationEndpoint;
+
+        Scope.Add("user.email:read");
+
+        ClaimActions.MapCustomJson(ClaimTypes.NameIdentifier, user => user.GetString("id"));
+        ClaimActions.MapCustomJson(ClaimTypes.Email, user => user.GetString("email"));
+    }
+}
