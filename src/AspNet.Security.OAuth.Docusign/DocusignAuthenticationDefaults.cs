218e4e8b9627ba58f385e98ed500f6074b6e4888
denis-goncharenko <52198869+denis-goncharenko@users.noreply.github.com>
2024-06-15 11:41:18 +0300

    Implement Docusign OAuth provider

---
+++ src/AspNet.Security.OAuth.Docusign/DocusignAuthenticationDefaults.cs
@@ -0,0 +1,48 @@
+﻿/*
+ * Licensed under the Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
+ * See https://github.com/aspnet-contrib/AspNet.Security.OAuth.Providers
+ * for more information concerning the license and the contributors participating to this project.
+ */
+
+namespace AspNet.Security.OAuth.Docusign;
+
+/// <summary>
+/// Default values used by the Docusign authentication middleware.
+/// </summary>
+public static class DocusignAuthenticationDefaults
+{
+    /// <summary>
+    /// Default value for <see cref="AuthenticationScheme.Name"/>.
+    /// </summary>
+    public const string AuthenticationScheme = "Docusign";
+
+    /// <summary>
+    /// Default value for <see cref="AuthenticationScheme.DisplayName"/>.
+    /// </summary>
+    public static readonly string DisplayName = "Docusign";
+
+    /// <summary>
+    /// Default value for <see cref="AuthenticationSchemeOptions.ClaimsIssuer"/>.
+    /// </summary>
+    public static readonly string Issuer = "Docusign";
+
+    /// <summary>
+    /// Default value for <see cref="RemoteAuthenticationOptions.CallbackPath"/>.
+    /// </summary>
+    public static readonly string CallbackPath = "/signin-docusign";
+
+    /// <summary>
+    /// Default value for <see cref="OAuthOptions.AuthorizationEndpoint"/>.
+    /// </summary>
+    public static readonly string AuthorizationEndpoint = "https://account-d.docusign.com/oauth/auth";
+
+    /// <summary>
+    /// Default value for <see cref="OAuthOptions.TokenEndpoint"/>.
+    /// </summary>
+    public static readonly string TokenEndpoint = "https://account-d.docusign.com/oauth/token";
+
+    /// <summary>
+    /// Default value for <see cref="OAuthOptions.UserInformationEndpoint"/>.
+    /// </summary>
+    public static readonly string UserInformationEndpoint = "https://account-d.docusign.com/oauth/userinfo";
+}
---
+++ src/AspNet.Security.OAuth.Docusign/DocusignAuthenticationOptions.cs
@@ -0,0 +1,30 @@
+﻿/*
+ * Licensed under the Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
+ * See https://github.com/aspnet-contrib/AspNet.Security.OAuth.Providers
+ * for more information concerning the license and the contributors participating to this project.
+ */
+
+using System.Security.Claims;
+
+namespace AspNet.Security.OAuth.Docusign;
+
+/// <summary>
+/// Defines a set of options used by <see cref="DocusignAuthenticationHandler"/>.
+/// </summary>
+public class DocusignAuthenticationOptions : OAuthOptions
+{
+    public DocusignAuthenticationOptions()
+    {
+        ClaimsIssuer = DocusignAuthenticationDefaults.Issuer;
+        CallbackPath = DocusignAuthenticationDefaults.CallbackPath;
+
+        AuthorizationEndpoint = DocusignAuthenticationDefaults.AuthorizationEndpoint;
+        TokenEndpoint = DocusignAuthenticationDefaults.TokenEndpoint;
+        UserInformationEndpoint = DocusignAuthenticationDefaults.UserInformationEndpoint;
+
+        ClaimActions.MapCustomJson(ClaimTypes.Name, user => user.GetString("name"));
+        ClaimActions.MapCustomJson(ClaimTypes.Email, user => user.GetString("email"));
+        ClaimActions.MapCustomJson(ClaimTypes.GivenName, user => user.GetString("given_name"));
+        ClaimActions.MapCustomJson(ClaimTypes.Surname, user => user.GetString("family_name"));
+    }
+}
