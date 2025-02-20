139fa75b7df39c28f5b7dc3457d8de2959929db4
Loongle <taitanwl@qq.com>
2025-02-20 15:52:27 +0800

    Add BuildChallengeUrl test

--- test/AspNet.Security.OAuth.Providers.Tests/Douyin/DouyinTests.cs
+++ test/AspNet.Security.OAuth.Providers.Tests/Douyin/DouyinTests.cs
@@ -4,6 +4,7 @@
  * for more information concerning the license and the contributors participating to this project.
  */
 
+using AspNet.Security.OAuth.Xero;
 using Microsoft.AspNetCore.WebUtilities;
 
 namespace AspNet.Security.OAuth.Douyin;
@@ -27,4 +28,34 @@ public class DouyinTests(ITestOutputHelper outputHelper) : OAuthTests<DouyinAuth
     [InlineData("urn:douyin:nickname", "TestAccount")]
     public async Task Can_Sign_In_Using_Douyin(string claimType, string claimValue)
         => await AuthenticateUserAndAssertClaimValue(claimType, claimValue);
+
+    [Fact]
+    public async Task BuildChallengeUrl_Generates_Correct_Url()
+    {
+        // Arrange
+        var options = new DouyinAuthenticationOptions();
+
+        var redirectUrl = "https://my-site.local/signin-douyin";
+
+        // Act
+        Uri actual = await BuildChallengeUriAsync(
+            options,
+            redirectUrl,
+            (options, loggerFactory, encoder) => new DouyinAuthenticationHandler(options, loggerFactory, encoder));
+
+        // Assert
+        actual.ShouldNotBeNull();
+        actual.ToString().ShouldStartWith("https://open.douyin.com/platform/oauth/connect/");
+
+        var query = QueryHelpers.ParseQuery(actual.Query);
+
+        query.ShouldContainKey("state");
+        query.ShouldContainKeyAndValue("client_key", options.ClientId);
+        query.ShouldContainKeyAndValue("redirect_uri", redirectUrl);
+        query.ShouldContainKeyAndValue("response_type", "code");
+        query.ShouldContainKeyAndValue("scope", "user_info");
+
+        query.ShouldNotContainKey(OAuthConstants.CodeChallengeKey);
+        query.ShouldNotContainKey(OAuthConstants.CodeChallengeMethodKey);
+    }
 }
