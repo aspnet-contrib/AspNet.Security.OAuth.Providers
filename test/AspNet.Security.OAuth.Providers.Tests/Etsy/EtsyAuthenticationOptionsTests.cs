84929154748297d45d2585b34739212c57a8f3a7
DevTKSS <info@technische-konstruktion.com>
2025-11-06 20:39:02 +0100

    test(EtsyProvider): Added unit tests for EtsyAuthenticationOptions and IncludeDetailedUserInfo Handler

---
+++ test/AspNet.Security.OAuth.Providers.Tests/Etsy/EtsyAuthenticationOptionsTests.cs
@@ -0,0 +1,161 @@
+﻿/*
+ * Licensed under the Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
+ * See https://github.com/aspnet-contrib/AspNet.Security.OAuth.Providers
+ * for more information concerning the license and the contributors participating to this project.
+ */
+
+namespace AspNet.Security.OAuth.Etsy;
+
+public static class EtsyAuthenticationOptionsTests
+{
+    public static TheoryData<EtsyAuthenticationAccessType> AccessTypes => new()
+    {
+        { EtsyAuthenticationAccessType.Public }, // Private Etsy API access does not use client secret (aka 'shared secret' in Etsy App Registration) https://developers.etsy.com/documentation/essentials/authentication
+
+        // { EtsyAuthenticationAccessType.Confidential } // TODO: Verify commercial access app registration Authentication does support confidential clients. Etsy docs do not indicate this to be used at all but support stated this would be required for token refresh
+    };
+
+    [Theory]
+    [InlineData(null, EtsyAuthenticationAccessType.Public)]
+    [InlineData("", EtsyAuthenticationAccessType.Public)]
+    public static void Validate_Does_Not_Throw_If_ClientSecret_Is_Not_Provided_For_Public_Access_Type(string? clientSecret, EtsyAuthenticationAccessType accessType)
+    {
+        // Arrange
+        var options = new EtsyAuthenticationOptions()
+        {
+            AccessType = accessType,
+            ClientId = "my-client-id",
+            ClientSecret = clientSecret!,
+        };
+
+        // Act (no Assert)
+        options.Validate();
+    }
+
+    // Leaving this test commented out to for case that ClientSecret is used with commercial access type - so this can be reactivated
+    // [Theory]
+    // [InlineData(EtsyAuthenticationAccessType.Commercial)]
+    // public static void Validate_Throws_If_ClientSecret_Is_Null(EtsyAuthenticationAccessType accessType)
+    // {
+    //    // Arrange
+    //    var options = new EtsyAuthenticationOptions()
+    //    {
+    //        AccessType = accessType,
+    //        ClientId = "my-client-id",
+    //        ClientSecret = null!,
+    //    };
+    //
+    //    // Act and Assert
+    //    _ = Assert.Throws<ArgumentNullException>("ClientSecret", options.Validate);
+    // }
+    [Theory]
+    [InlineData(EtsyAuthenticationAccessType.Public, true, false)]
+    [InlineData(EtsyAuthenticationAccessType.Public, false, false)]
+    [InlineData(EtsyAuthenticationAccessType.Public, false, true)]
+    public static void Validate_Throws_If_SaveTokens_Or_Pkce_Is_Disabled(EtsyAuthenticationAccessType accessType, bool usePkce, bool saveTokens)
+    {
+        // Arrange
+        var options = new EtsyAuthenticationOptions()
+        {
+            AccessType = accessType,
+            ClientId = "my-client-id",
+            ClientSecret = "my-client-secret",
+            SaveTokens = saveTokens,
+            UsePkce = usePkce,
+        };
+
+        // Act and Assert
+        _ = Assert.Throws<ArgumentException>(options.Validate);
+    }
+
+    [Theory]
+    [MemberData(nameof(AccessTypes))]
+    public static void Validate_Throws_If_Scope_Is_Empty(EtsyAuthenticationAccessType accessType)
+    {
+        // Arrange
+        var options = new EtsyAuthenticationOptions()
+        {
+            AccessType = accessType,
+            ClientId = "my-client-id",
+            ClientSecret = "my-client-secret",
+            Scope = { },
+        };
+
+        // Act and Assert
+        _ = Assert.Throws<ArgumentOutOfRangeException>(options.Validate);
+    }
+
+    [Theory]
+    [MemberData(nameof(AccessTypes))]
+    public static void Validate_Throws_If_Scope_Does_Not_Contain_Scope_shop_r(EtsyAuthenticationAccessType accessType)
+    {
+        // Arrange
+        var options = new EtsyAuthenticationOptions()
+        {
+            AccessType = accessType,
+            ClientId = "my-client-id",
+            ClientSecret = "my-client-secret",
+        };
+        options.Scope.Clear();
+        options.Scope.Add(ClaimTypes.Email);
+
+        // Act and Assert
+        _ = Assert.Throws<ArgumentOutOfRangeException>(options.Validate);
+    }
+
+    [Theory]
+    [MemberData(nameof(AccessTypes))]
+    public static void Validate_Throws_If_IncludeDetailedUserInfo_Is_True_But_Does_Not_Contain_Scope_email_r(EtsyAuthenticationAccessType accessType)
+    {
+        // Arrange
+        var options = new EtsyAuthenticationOptions()
+        {
+            AccessType = accessType,
+            ClientId = "my-client-id",
+            ClientSecret = "my-client-secret",
+            IncludeDetailedUserInfo = true,
+        };
+
+        // Not Adding email scope, shop scope is already added by default
+
+        // Act and Assert
+        _ = Assert.Throws<ArgumentOutOfRangeException>(options.Validate);
+    }
+
+    [Theory]
+    [MemberData(nameof(AccessTypes))]
+    public static void Validate_Does_Not_Throw_When_IncludeDetailedUserInfo_Is_False_And_Contains_Scope_email_r(EtsyAuthenticationAccessType accessType)
+    {
+        // Arrange
+        var options = new EtsyAuthenticationOptions()
+        {
+            AccessType = accessType,
+            ClientId = "my-client-id",
+            ClientSecret = "my-client-secret",
+            IncludeDetailedUserInfo = false,
+        };
+
+        // Adding email scope
+        options.Scope.Add(ClaimTypes.Email);
+
+        // Act (no Assert)
+        options.Validate();
+    }
+
+    [Theory]
+    [MemberData(nameof(AccessTypes))]
+    public static void Validate_Throws_If_CallbackPath_Is_Null(EtsyAuthenticationAccessType accessType)
+    {
+        // Arrange
+        var options = new EtsyAuthenticationOptions()
+        {
+            AccessType = accessType,
+            CallbackPath = null,
+            ClientId = "my-client-id",
+            ClientSecret = "my-client-secret",
+        };
+
+        // Act and Assert
+        _ = Assert.Throws<ArgumentException>(nameof(EtsyAuthenticationOptions.CallbackPath), options.Validate);
+    }
+}
--- test/AspNet.Security.OAuth.Providers.Tests/Etsy/EtsyTests.cs
+++ test/AspNet.Security.OAuth.Providers.Tests/Etsy/EtsyTests.cs
@@ -1,14 +1,10 @@
-/*
+﻿/*
  * Licensed under the Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
  * See https://github.com/aspnet-contrib/AspNet.Security.OAuth.Providers
  * for more information concerning the license and the contributors participating to this project.
  */
 
-using System.Security.Claims;
 using AspNet.Security.OAuth.Etsy;
-using Microsoft.AspNetCore.Authentication;
-using Xunit;
-using Xunit.Abstractions;
 
 namespace AspNet.Security.OAuth.Providers.Tests.Etsy;
 
@@ -27,7 +23,7 @@ public class EtsyTests : OAuthTests<EtsyAuthenticationOptions>
     }
 
     [Theory]
-    [InlineData(ClaimTypes.NameIdentifier, "789012")] // shop_id and user_id is used as primary identifier!
+    [InlineData(ClaimTypes.NameIdentifier, "789012")]
     [InlineData(ClaimTypes.Email, "test@example.com")]
     [InlineData(ClaimTypes.GivenName, "Test")]
     [InlineData(ClaimTypes.Surname, "User")]
@@ -39,4 +35,50 @@ public class EtsyTests : OAuthTests<EtsyAuthenticationOptions>
     [InlineData("urn:etsy:image_url", "https://i.etsystatic.com/test/test_75x75.jpg")]
     public async Task Can_Sign_In_Using_Etsy(string claimType, string claimValue)
         => await AuthenticateUserAndAssertClaimValue(claimType, claimValue);
+
+    [Fact]
+    public async Task Does_Not_Include_Detailed_Claims_When_IncludeDetailedUserInfo_Is_False()
+    {
+        // Arrange: disable detailed user info enrichment
+        void ConfigureServices(IServiceCollection services) => services.PostConfigureAll<EtsyAuthenticationOptions>(o => o.IncludeDetailedUserInfo = false);
+
+        using var server = CreateTestServer(ConfigureServices);
+
+        // Act
+        var claims = await AuthenticateUserAsync(server);
+
+        // Assert basic claims are present
+        claims.ShouldContainKey("urn:etsy:user_id");
+        claims.ShouldContainKey("urn:etsy:shop_id");
+
+        // Detailed claims should be absent when flag is false
+        claims.Keys.ShouldNotContain(ClaimTypes.Email);
+        claims.Keys.ShouldNotContain(ClaimTypes.GivenName);
+        claims.Keys.ShouldNotContain(ClaimTypes.Surname);
+        claims.Keys.ShouldNotContain("urn:etsy:primary_email");
+        claims.Keys.ShouldNotContain("urn:etsy:first_name");
+        claims.Keys.ShouldNotContain("urn:etsy:last_name");
+        claims.Keys.ShouldNotContain("urn:etsy:image_url");
+    }
+
+    [Fact]
+    public async Task Includes_Detailed_Claims_When_IncludeDetailedUserInfo_Is_True()
+    {
+        // Arrange: explicitly enable detailed user info enrichment (default may already be true, set explicitly for clarity)
+        void ConfigureServices(IServiceCollection services) => services.PostConfigureAll<EtsyAuthenticationOptions>(o => o.IncludeDetailedUserInfo = true);
+
+        using var server = CreateTestServer(ConfigureServices);
+
+        // Act
+        var claims = await AuthenticateUserAsync(server);
+
+        // Assert detailed claims are present
+        claims.ShouldContainKey(ClaimTypes.Email);
+        claims.ShouldContainKey(ClaimTypes.GivenName);
+        claims.ShouldContainKey(ClaimTypes.Surname);
+        claims.ShouldContainKey("urn:etsy:primary_email");
+        claims.ShouldContainKey("urn:etsy:first_name");
+        claims.ShouldContainKey("urn:etsy:last_name");
+        claims.ShouldContainKey("urn:etsy:image_url");
+    }
 }
