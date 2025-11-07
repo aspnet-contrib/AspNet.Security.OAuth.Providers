5374092cd14d85fa10c9cf7ee92b651aa8e70922
DevTKSS <info@technische-konstruktion.com>
2025-11-07 16:21:42 +0100

    chore(Etsy): align oauth scopes with the docs table
    there is no endpoint this could get from and as its in the table format in etsy api, it gets also not inserted into the api spec .json for CLI tools like Kiota

--- src/AspNet.Security.OAuth.Etsy/EtsyAuthenticationConstants.cs
+++ src/AspNet.Security.OAuth.Etsy/EtsyAuthenticationConstants.cs
@@ -30,64 +30,64 @@ public static class EtsyAuthenticationConstants
     /// </summary>
     public static class Scopes
     {
-        /// <summary>Read billing and shipping addresses.</summary>
+        /// <summary>See billing and shipping addresses</summary>
         public static readonly string AddressRead = "address_r";
 
-        /// <summary>Update billing and shipping addresses.</summary>
+        /// <summary>Update billing and shipping addresses</summary>
         public static readonly string AddressWrite = "address_w";
 
-        /// <summary>Read all billing statement data.</summary>
+        /// <summary>See all billing statement data</summary>
         public static readonly string BillingRead = "billing_r";
 
-        /// <summary>Read shopping carts.</summary>
+        /// <summary>Read shopping carts</summary>
         public static readonly string CartRead = "cart_r";
 
-        /// <summary>Add and remove items from shopping carts.</summary>
+        /// <summary>Add/Remove from shopping carts</summary>
         public static readonly string CartWrite = "cart_w";
 
-        /// <summary>Read user profile and email address.</summary>
+        /// <summary>Read a user profile</summary>
         public static readonly string EmailRead = "email_r";
 
-        /// <summary>Read private favorites.</summary>
+        /// <summary>See private favorites</summary>
         public static readonly string FavoritesRead = "favorites_r";
 
-        /// <summary>Add and remove favorites.</summary>
+        /// <summary>Add/Remove favorites</summary>
         public static readonly string FavoritesWrite = "favorites_w";
 
-        /// <summary>Read purchase information in feedback.</summary>
+        /// <summary>See purchase info in feedback</summary>
         public static readonly string FeedbackRead = "feedback_r";
 
-        /// <summary>Delete listings.</summary>
+        /// <summary>Delete listings</summary>
         public static readonly string ListingsDelete = "listings_d";
 
-        /// <summary>Read all listings, including expired listings.</summary>
+        /// <summary>See all listings (including expired etc)</summary>
         public static readonly string ListingsRead = "listings_r";
 
-        /// <summary>Create and edit listings.</summary>
+        /// <summary>Create/Edit listings</summary>
         public static readonly string ListingsWrite = "listings_w";
 
-        /// <summary>Read all profile data.</summary>
+        /// <summary>See all profile data</summary>
         public static readonly string ProfileRead = "profile_r";
 
-        /// <summary>Update user profile, avatar, and related data.</summary>
+        /// <summary>Update user profile, avatar, etc</summary>
         public static readonly string ProfileWrite = "profile_w";
 
-        /// <summary>Read recommended listings.</summary>
+        /// <summary>See recommended listings</summary>
         public static readonly string RecommendRead = "recommend_r";
 
-        /// <summary>Accept and reject recommended listings.</summary>
+        /// <summary>Accept/Reject recommended listings</summary>
         public static readonly string RecommendWrite = "recommend_w";
 
-        /// <summary>Read private shop information.</summary>
+        /// <summary>See private shop info</summary>
         public static readonly string ShopsRead = "shops_r";
 
-        /// <summary>Update shop information.</summary>
+        /// <summary>Update shop</summary>
         public static readonly string ShopsWrite = "shops_w";
 
-        /// <summary>Read all checkout and payment data.</summary>
+        /// <summary>See all checkout/payment data</summary>
         public static readonly string TransactionsRead = "transactions_r";
 
-        /// <summary>Update receipts.</summary>
+        /// <summary>Update receipts</summary>
         public static readonly string TransactionsWrite = "transactions_w";
     }
 }
